e49e20a4df17ba6fd9f7c7dbeb7c062d29217e26
Joe Futrelle <joefutrelle@gmail.com>
2011-08-07 18:31:02 +0000

    parsing arguments the pythonic way
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@12 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 96565744182b05d10f7f0c3660f14948905c11c0

--- IFCB_tools/roi2images.py
+++ IFCB_tools/roi2images.py
@@ -7,19 +7,5 @@ if __name__ == '__main__':
     if(len(argv) < 2):
         print 'usage: roi2images [id] [dir: .] [outdir: .] [format: png]'
     else:
-        # allow id to be the ROI file name, strip off the .roi extension if it's there
-        id = re.sub('(.*)\\.[a-z]+','\\1',argv[1])
-        dir = '.'
-        outdir = '.'
-        format = 'PNG'
-        arg = 2
-        if(len(argv) > arg):
-            dir = argv[arg]
-            arg = arg + 1
-        if(len(argv) > arg):
-            outdir = argv[arg]
-            arg = arg + 1
-        if(len(argv) > arg):
-            format = string.upper(argv[arg])
-        bin = Bin(id,dir)
-        bin.save_images(outdir, format)
+        bin = Bin(*argv[1:3]) # id, dir
+        bin.save_images(*argv[3:5]) # outdir, format
--- IFCB_tools/roi2xml.py
+++ IFCB_tools/roi2xml.py
@@ -18,6 +18,7 @@ def rois2xml(bin,out):
     for roi_info in bin.all_rois():
         roi = SubElement(root,QName(ifcb.namespace, 'roi'))
         SubElement(roi,QName(dc,'identifier')).text = roi_info[PID]
+        # each roi is a dict. prepend the ifcb namespace to each dict key and make subtags
         for tag, value in roi_info.items():
             if tag != PID:
                 property = SubElement(roi,QName(ifcb.namespace,tag))
@@ -28,12 +29,5 @@ if __name__ == '__main__':
     if(len(argv) < 2):
         print 'usage: roi2xml [id] [dir: .]'
     else:
-        # allow id to be the ROI file name, strip off the .roi extension if it's there
-        id = re.sub('(.*)\\.[a-z]+','\\1',argv[1])
-        dir = '.'
-        arg = 2
-        if(len(argv) > arg):
-            dir = argv[arg]
-            arg = arg + 1
-        rois2xml(Bin(id,dir),stdout)
+        rois2xml(Bin(*argv[1:]),stdout)
         
