4d0bb482bbf1535fc79a8529e23b61ad38320199
Joe Futrelle <joefutrelle@gmail.com>
2011-08-25 20:53:15 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@74 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 63f9964c0ff0482e11153e0e6089668bd2f36fbb

--- IFCB_tools/rss.py
+++ IFCB_tools/rss.py
@@ -3,11 +3,12 @@ from sys import argv, stdout
 import cgi
 import cgitb
 from ifcb.io.convert import fs2atom, fs2json_feed, fs2html_feed
-from config import FS, ATOM
+from ifcb.io.path import Filesystem
+from config import FS_ROOTS, ATOM
 
 if __name__ == '__main__':
     cgitb.enable()
     format = cgi.FieldStorage().getvalue('format','atom')
     mime = dict(atom='application/atom+xml', json='application/json', html='text/html')[format]
     print 'Content-type: %s\n' % (mime)
-    dict(atom=fs2atom, json=fs2json_feed, html=fs2html_feed)[format](FS,ATOM)
+    dict(atom=fs2atom, json=fs2json_feed, html=fs2html_feed)[format](Filesystem(FS_ROOTS),ATOM)
