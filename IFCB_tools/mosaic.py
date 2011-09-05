549542a430d116c6deb1ff46e4713b40647bf791
Joe Futrelle <joefutrelle@gmail.com>
2011-09-05 20:23:27 +0000

    attempt at CGI
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@91 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 208efea51b99da11acb8f5b25135666f2b1d6157

--- IFCB_tools/mosaic.py
+++ IFCB_tools/mosaic.py
@@ -1,30 +1,53 @@
 # create a mosaic image containing multiple ROI's from the given bin
 from PIL import Image
-from ifcb.binpacking import JimScottRectanglePacker, CygonRectanglePacker
+from ifcb.binpacking import JimScottRectanglePacker
 from ifcb.io import HEIGHT, WIDTH, TARGET_NUMBER
 from ifcb.io.file import BinFile
 import math
+
+from config import FS_ROOTS
+import cgitb
+import ifcb.io.cache
 
 def mosaic(bin, width, height, size=0):
     mosaic = Image.new('L', (width, height))
     mosaic.paste(160,(0,0,width,height))
     packer = JimScottRectanglePacker(width, height)
     targets = sorted(bin.all_targets(), key=lambda t: 0 - (t.info[HEIGHT] * t.info[WIDTH]))
+    good = 0
+    bad = 0
     for target in targets:
         h = target.info[WIDTH] # rotate 90 degrees
         w = target.info[HEIGHT] # rotate 90 degrees
         if w * h > size:
             p = packer.TryPack(w, h)
             if p is not None:
+                good = good + 1
                 mosaic.paste(target.image(), (p.x, p.y))
+            else:
+                bad = bad + 1
     return mosaic
-            
-if __name__=='__main__':
-    width = 1920
-    height = 1080
-    bin = BinFile('../exampleData/IFCB1_2011_231/IFCB1_2011_231_182610.roi')
+           
+def thumbnail(image, width, height):
+    image.thumbnail((width, height))
+    return image
+
+def test():
+    width = 2000
+    height = 1500
+    #bin = BinFile('../exampleData/IFCB1_2011_231/IFCB1_2011_231_182610.roi')
     #bin = BinFile('../exampleData/IFCB1_2009_216/IFCB1_2009_216_075913.roi')
-    m = mosaic(bin, width, height)
+    #bin = BinFile('/Users/jfutrelle/Downloads/IFCB1_2011_248_160637.roi')
+    m = mosaic(bin, width, height, 2500)
     m.thumbnail((800,600))
     with open('/Users/jfutrelle/Pictures/bar.png','wb') as f:
         m.save(f,'png')
+
+if __name__=='__main__':
+    cgitb.enable()
+    (pid, ext) = os.path.splitext(cgi.FieldStorage().getvalue('pid'))
+    print 'Content-type: image/png\n'
+    bin = Filesystem(FS_ROOTS).resolve(pid)
+    cache_key = ifcb.lid(pid) + '_thumb.png'
+    cache.cache_io(cache_key, lambda o: thumbnail(mosaic(bin, 2000, 1500, 2500),800,600).save(o,format), sys.stdout)
+    
