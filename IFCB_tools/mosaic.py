5dcb3b3e56748dce01079f624d29a5f88a84d966
Joe Futrelle <joefutrelle@gmail.com>
2011-09-05 13:55:39 +0000

    mosaic summary of bin (prototype)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@89 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 88f522d83834aa40dcf749640a0785e63af504db

---
+++ IFCB_tools/mosaic.py
@@ -0,0 +1,27 @@
+# create a mosaic image containing multiple ROI's from the given bin
+from PIL import Image
+from ifcb.binpacking import JimScottRectanglePacker, CygonRectanglePacker
+from ifcb.io import HEIGHT, WIDTH, TARGET_NUMBER
+from ifcb.io.file import BinFile
+import math
+
+def mosaic(bin, width, height, size=0):
+    mosaic = Image.new('L', (width, height))
+    packer = JimScottRectanglePacker(width, height)
+    targets = sorted(bin.all_targets(), key=lambda t: 0 - (t.info[HEIGHT] * t.info[WIDTH]))
+    for target in targets:
+        h = target.info[WIDTH] # rotate 90 degrees
+        w = target.info[HEIGHT] # rotate 90 degrees
+        if w * h > size:
+            p = packer.TryPack(w, h)
+            if p is not None:
+                mosaic.paste(target.image(), (p.x, p.y))
+    return mosaic
+            
+if __name__=='__main__':
+    width = 1920
+    height = 1080
+    bin = BinFile('../exampleData/IFCB1_2011_231/IFCB1_2011_231_182610.roi')
+    m = mosaic(bin, width, height)
+    with open('/Users/jfutrelle/Pictures/bar.png','wb') as f:
+        m.save(f,'png')
