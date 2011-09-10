e483e56215e32f60ce7f5f7ec7cfd604e32794b5
Joe Futrelle <joefutrelle@gmail.com>
2011-09-10 14:10:13 +0000

    skips files that have already been fetched; also, curls into "file.part" and then moves to "file" so that it doesn't think it's finished with a download until it's finished with a download
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@120 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 530986651c494318b2a983ccd6f5f270eb461b13

--- IFCB_tools/spider/spider.sh
+++ IFCB_tools/spider/spider.sh
@@ -12,10 +12,16 @@ while true; do
 	lid="$(echo $pid | sed -e 's/.*IFCB/IFCB/')"
 	dir=${DEST}/"$(echo $lid | sed -e 's/_......$//')"
 	for ext in adc roi hdr; do
-            fn=${pid}.${ext}
-	    echo Fetching ${fn}...
-	    mkdir -p ${dir}
-	    curl -s ${fn} -o ${dir}/${lid}.${ext}
+            url=${pid}.${ext}
+	    file=${dir}/${lid}.${ext}
+	    if [ -e ${file} ]; then
+		echo Already fetched ${url}.
+	    else
+		echo Fetching ${url}...
+		mkdir -p ${dir}
+		curl -s ${url} -o ${file}.part
+		mv ${file}.part ${file}
+	    fi
 	done
     done
     DATE=${updated}
