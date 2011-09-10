37eaa5d322ebede45e0f97f8902e6e9fe3a69c32
Joe Futrelle <joefutrelle@gmail.com>
2011-09-10 14:42:04 +0000

    fixed irritating tabs
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@121 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: d72330f053edcf36e1f70569aa1b288272882ed7

--- IFCB_tools/spider/spider.sh
+++ IFCB_tools/spider/spider.sh
@@ -7,25 +7,22 @@ while true; do
     FEED="http://ifcb-data.whoi.edu/rss.py?format=atom&date=${DATE}"
     echo "Fetching feed @ ${FEED}..."
     for line in `curl -s $FEED | xsltproc atom2csv.xsl - | tail -n +2`; do
-	pid="$(echo $line | cut -d, -f1)"
-	updated="$(echo $line | cut -d, -f2)"
-	lid="$(echo $pid | sed -e 's/.*IFCB/IFCB/')"
-	dir=${DEST}/"$(echo $lid | sed -e 's/_......$//')"
-	for ext in adc roi hdr; do
+        pid="$(echo $line | cut -d, -f1)"
+        updated="$(echo $line | cut -d, -f2)"
+        lid="$(echo $pid | sed -e 's/.*IFCB/IFCB/')"
+        dir=${DEST}/"$(echo $lid | sed -e 's/_......$//')"
+        for ext in adc roi hdr; do
             url=${pid}.${ext}
-	    file=${dir}/${lid}.${ext}
-	    if [ -e ${file} ]; then
-		echo Already fetched ${url}.
-	    else
-		echo Fetching ${url}...
-		mkdir -p ${dir}
-		curl -s ${url} -o ${file}.part
-		mv ${file}.part ${file}
-	    fi
-	done
+            file=${dir}/${lid}.${ext}
+            if [ -e ${file} ]; then
+                echo Already fetched ${url}.
+            else
+                echo Fetching ${url}...
+                mkdir -p ${dir}
+                curl -s ${url} -o ${file}.part
+                mv ${file}.part ${file}
+            fi
+        done
     done
     DATE=${updated}
 done
-
-
-
