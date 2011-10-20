c6f5843398e20c30e1f5180c606a724de0f086df
Joe Futrelle <joefutrelle@gmail.com>
2011-10-20 17:08:49 +0000

    now adds median-filtered noise with correct mean and variance.
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@174 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 923cfaf612982bf93b11a86380e6b710d18d4964

--- IFCB_tools/stitch.py
+++ IFCB_tools/stitch.py
@@ -1,8 +1,10 @@
 from urllib2 import urlopen, Request
 import json
 from StringIO import StringIO
-from PIL import Image, ImageDraw, ImageChops
+from PIL import Image, ImageDraw, ImageChops, ImageFilter
 from collections import namedtuple
+import math
+from numpy import random
 
 Roi = namedtuple('Roi', ['image','x','y','w','h'])
 
@@ -27,20 +29,46 @@ def stitch(pids):
     y = min([roi.y for roi in rois.values()])
     w = max([roi.x + roi.w for roi in rois.values()]) - x
     h = max([roi.y + roi.h for roi in rois.values()]) - y
-    s = Image.new('L',(h,w)) # rotated 90
-    mask = Image.new('L',(h,w), 0)
-    draw = ImageDraw.Draw(mask)
+    s = Image.new('L',(h,w)) # the stitched image with a missing region
+    mask = Image.new('L',(h,w), 255) # a mask of the missing region
+    edges = Image.new('L',(h,w), 0) # just the edges of the missing region
+    draw = ImageDraw.Draw(edges)
     for roi in rois.values():
         rx = roi.x - x
         ry = roi.y - y
         s.paste(roi.image, (ry, rx))
-        mask.paste(255, (ry, rx, ry + roi.h, rx + roi.w))
+        mask.paste(0, (ry, rx, ry + roi.h, rx + roi.w))
+        edges.paste(255, (ry, rx, ry + roi.h, rx + roi.w))
     for roi in rois.values():
         rx = roi.x - x
         ry = roi.y - y
         draw.rectangle((ry + 1, rx + 1, ry + roi.h - 2, rx + roi.w - 2), fill=0)
     draw.rectangle((0,0,h-1,w-1), outline=0)
-    s = ImageChops.lighter(s,mask) # FIXME debug
+    # OK. Now the stitched image contains both ROIs and black gap
+    # The mask contains just the gap, in white
+    # The edges is a mask just for the pixels bordering on the edge of the gap
+    # compute the mean and variance of all of those pixels that aren't too dark (>150)
+    sum = 0
+    n = 0
+    histogram = s.histogram(edges)
+    low = 120 # arbitrary darkness cutoff
+    for color in range(low,256):
+        count = histogram[color]
+        n += count
+        sum += count * color
+    mean = sum / n
+    sum2 = 0
+    for color in range(low,256):
+        sum2 += ((color - mean) ** 2) * histogram[color]
+    variance = sum2 / n
+    pix = mask.load()
+    noise = random.normal(math.sqrt(variance), size=(h,w))
+    for x in xrange(h):
+        for y in xrange(w):
+            if pix[x,y] == 255:
+                pix[x,y] = mean + noise[x,y]
+    mask.filter(ImageFilter.MedianFilter(3))
+    s = ImageChops.add(mask, s)
     return s
     
 if __name__=='__main__':
