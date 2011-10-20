7378b7155acc9aa89f0058300f1b58b394da91b7
Heidi Sosik <hsosik@whoi.edu>
2011-10-20 17:08:26 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@173 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 5fd546bebdeb8fe250d4cadac6486ee68c517c1f

--- feature_extraction/sandbox.m
+++ feature_extraction/sandbox.m
@@ -9,8 +9,8 @@ targets = get_targets(bin);
 for tix = 1:length(targets.pid),
     img = get_image(targets.pid(tix));
     img_blob = blob(img);
-    subplot(1,2,1), imshow(img);
-    subplot(1,2,2), imshow(img_blob);
+    %subplot(1,2,1), imshow(img);
+    %subplot(1,2,2), imshow(img_blob);
     disp(['area = ' num2str(blob_area(img_blob))]);
     pause
 end;
