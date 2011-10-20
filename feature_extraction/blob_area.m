9323dea7b03ae30e00132c282211283346db57b3
Heidi Sosik <hsosik@whoi.edu>
2011-10-20 17:08:17 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@172 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 233e4c396a14a17fbd366baf8a38549e83cf6c13

--- feature_extraction/blob_area.m
+++ feature_extraction/blob_area.m
@@ -4,14 +4,21 @@ function [ area ] = blob_area( blob_image )
 
 blob_min = 200; % FIXME magic number
 
+%cc = bwconncomp(blob_image);
+%t = regionprops(cc, 'Area');
 t = regionprops(logical(blob_image), 'Area');
-if ~isempty(t),  %this is case with a detectable blob
-    s = cat(1, t.Area);
-    temp = s > blob_min;
-    area = sum(s(temp));
-else    
-    area = NaN;  %case where no blobs
-end;
+idx = find([t.Area] > blob_min);
+%BW2 = ismember(labelmatrix(cc), idx); %need this? use for next call to region props or better to do all at once?
+area = sum([t(idx).Area]);
+
+% t = regionprops(logical(blob_image), 'Area');
+% if ~isempty(t),  %this is case with a detectable blob
+%     s = cat(1, t.Area);
+%     temp = s > blob_min;
+%    area = sum(s(temp)); 
+% else    
+%     area = NaN;  %case where no blobs
+% end;
 
 end
 
