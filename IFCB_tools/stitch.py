61946e7869fdc21317e9c20b904d863154c7af0a
Joe Futrelle <joefutrelle@gmail.com>
2011-10-21 13:43:51 +0000

    finds overlapping pairs of rois in a bin and stitches them
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@175 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 10a8de35c1870b32a26423d6aaf57f3484f63fd6

--- IFCB_tools/stitch.py
+++ IFCB_tools/stitch.py
@@ -5,8 +5,9 @@ from PIL import Image, ImageDraw, ImageChops, ImageFilter
 from collections import namedtuple
 import math
 from numpy import random
+import ifcb
 
-Roi = namedtuple('Roi', ['image','x','y','w','h'])
+Roi = namedtuple('Roi', ['image','x','y','w','h', 'trigger'])
 
 def get_image(pid):
     u = urlopen(pid + '.png')
@@ -17,14 +18,34 @@ def get_image(pid):
 def get_image_properties(pid):
     return json.loads(''.join(urlopen(Request(pid + '.json'))))
 
+def overlaps(t1, t2):
+    if t1['trigger'] == t2['trigger']:
+        (x11, y11) = (t1['left'], t1['bottom'])
+        (x12, y12) = (x11 + t1['width'], y11 + t1['height'])
+        (x21, y21) = (t2['left'], t2['bottom'])
+        (x22, y22) = (x21 + t2['width'], y21 + t2['height'])
+        # if (RectA.X1 < RectB.X2 && RectA.X2 > RectB.X1 &&
+        #RectA.Y1 < RectB.Y2 && RectA.Y2 > RectB.Y1)
+        if x11 < x22 and x12 > x21 and y11 < y22 and y12 > y21:
+            return True
+    return False
+
+def find_pairs(bin_pid):
+    bin = json.loads(''.join(urlopen(Request(bin_pid + '/full.json'))))
+    prev = None
+    for target in bin['targets']:
+        if prev is not None and overlaps(target, prev):
+            yield (prev['pid'], target['pid'])
+        prev = target
+            
 def stitch(pids):
     rois = {}
     for pid in pids:
         p = get_image_properties(pid)
         print p
         i = get_image(pid)
-        xywh = [p[x] for x in ['left','bottom','width','height']]
-        rois[pid] = Roi._make([i] + xywh)
+        xywht = [p[x] for x in ['left','bottom','width','height','trigger']]
+        rois[pid] = Roi._make([i] + xywht)
     x = min([roi.x for roi in rois.values()])
     y = min([roi.y for roi in rois.values()])
     w = max([roi.x + roi.w for roi in rois.values()]) - x
@@ -50,37 +71,52 @@ def stitch(pids):
     # compute the mean and variance of all of those pixels that aren't too dark (>150)
     sum = 0
     n = 0
-    histogram = s.histogram(edges)
-    low = 120 # arbitrary darkness cutoff
+    histogram = s.histogram(edges) # image pixel value, just along the edges
+    low = 120 # arbitrary darkness cutoff: don't count edge pixels under this value
     for color in range(low,256):
         count = histogram[color]
         n += count
         sum += count * color
-    mean = sum / n
+    mean = sum / n # mean pixel value along the edges (excluding dark pixels)
     sum2 = 0
     for color in range(low,256):
         sum2 += ((color - mean) ** 2) * histogram[color]
-    variance = sum2 / n
+    variance = sum2 / n # corresponding variance
+    # now fill the mask with the mean value, plus Gaussian noise
     pix = mask.load()
     noise = random.normal(math.sqrt(variance), size=(h,w))
     for x in xrange(h):
         for y in xrange(w):
-            if pix[x,y] == 255:
+            if pix[x,y] == 255: # only for pixels in the mask
                 pix[x,y] = mean + noise[x,y]
+    # apply a median filter to the noise
     mask.filter(ImageFilter.MedianFilter(3))
-    s = ImageChops.add(mask, s)
+    s = ImageChops.add(mask, s) # final composite
     return s
-    
-if __name__=='__main__':
+
+def test_stitch():
     pids = [
             #'http://ifcb-data.whoi.edu/IFCB1_2011_290_060847_00008',
             #'http://ifcb-data.whoi.edu/IFCB1_2011_290_060847_00009'
-            'http://ifcb-data.whoi.edu/IFCB1_2011_292_022614_02752.html',
-            'http://ifcb-data.whoi.edu/IFCB1_2011_292_022614_02753.html'
+            #'http://ifcb-data.whoi.edu/IFCB1_2011_292_022614_02752.html',
+            #'http://ifcb-data.whoi.edu/IFCB1_2011_292_022614_02753.html'
             #'http://ifcb-data.whoi.edu/IFCB1_2011_293_025945_01223',
             #'http://ifcb-data.whoi.edu/IFCB1_2011_293_025945_01224'
+            'http://ifcb-data.whoi.edu/IFCB1_2011_292_211737_00524',
+            'http://ifcb-data.whoi.edu/IFCB1_2011_292_211737_00525'
     ]
     s = stitch(pids)
     pathname = 'stitch.png'
     s.save(pathname,'png')
-    
+
+def test_bin(pid):
+    for pid1,pid2 in find_pairs(pid):
+        try:
+            s = stitch([pid1, pid2])
+            pathname = 'stitch/' + ifcb.lid(pid1) + '.png'
+            s.save(pathname,'png')
+        except:
+            print 'Error stitching ' + pid1 + ' and ' + pid2
+        
+if __name__=='__main__':
+    test_bin('http://ifcb-data.whoi.edu/IFCB1_2011_294_114650')
