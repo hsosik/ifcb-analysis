0f1a9a455a433139beb15b0514b8918faa908ab1
Joe Futrelle <joefutrelle@gmail.com>
2011-11-07 21:53:17 +0000

    improved code to read IFCB manual annotation files and insert into a database.
    the new database schema is normalized so that there is no assumption built in about what the manual_subXX columns are.
    
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@213 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: c3db901e9d271614c95eb15c4456785980291f37

--- IFCB_tools/manual2csv.py
+++ IFCB_tools/manual2csv.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
+import psycopg2 as pg
 from scipy.io import loadmat
 import numpy
-from numpy import squeeze
+from numpy import squeeze, isnan
 from os import path
+import os
 import math
 import sys
+import re
 
 def sq(thang):
     return map(squeeze, thang)
@@ -29,21 +32,69 @@ def dump_mat_field(file,field):
     for row in l[field]:
         print ','.join([csvrep(squeeze(c)) for c in row])
 
-def manual2csv(path):
+def manual2tuples(path):
+    lid = re.sub('.*/(.*).mat','\\1',path)
     l = loadmat(path)
-    annotations = sq(l['classlist'])
-    base_classes = [None] + sq(l['class2use_manual'][0])
-    sub_classes = [None] + sq(l['class2use_sub4'][0])
+    try:
+        cols = map(str,sq(l['list_titles'][0]))
+        annotations = sq(l['classlist'])
+        manual_classes = [None] + sq(l['class2use_manual'][0])
+        auto_classes = [None] + sq(l['class2use_auto'][0])
+        sub_classes = {}
+        for n,sub_col in zip(range(4,len(cols)+1),cols[3:]):
+            field = 'class2use_sub%d' % n
+            if field in l:
+                sub_classes[sub_col] = sq(l[field][0])
 
-    print 'roi_number,manual,SVM-auto,ciliate'    
-    for annotation in annotations:
-        (roi, manual, svm_auto, ciliate) = map(intNan, annotation)
-        print '%d,%s,%s,%s' % (roi, base_classes[manual] if manual is not None else '', base_classes[svm_auto] if svm_auto is not None else '', sub_classes[ciliate] if ciliate is not None else '')
+        for annotation in annotations:
+            (roi, manual, svm_auto) = map(intNan, annotation[:3])
+            subs = {}
+            for n,sub in zip(range(3,len(annotation)),annotation[3:]):
+                if not isnan(sub):
+                    col = cols[n]
+                    subs[cols[n]] = str(sub_classes[cols[n]][int(sub)-1])
+            yield (lid, '%s_%05d' % (lid, roi), str(manual_classes[manual-1]) if manual is not None else None, str(auto_classes[svm_auto-1]) if svm_auto is not None else None, subs)
+    except:
+        print 'error on %s' % lid
+        raise
     
 #dump_mat_field('manual_list.mat','manual_list')
 #dump_mat_field('IFCB1_2006_158_000036.mat','classlist')
 #manual2csv('../exampleData/annotation/IFCB1_2006_158_000036.mat')
 
+def sqlize_str(value):
+    if value is None:
+        return None
+    else:
+        return str(value)
+    
 if __name__=='__main__':
-    manual2csv(sys.argv[1])
+    PSQL_DB = 'ifcb'
+    PSQL_USER = 'jfutrelle'
+    PSQL_PASSWORD = 'vNLH814i'
+
+    conn = pg.connect('dbname=%s user=%s password=%s' % (PSQL_DB,PSQL_USER,PSQL_PASSWORD))
+
+    db = conn.cursor()
+    
+    #manual2csv(sys.argv[1])
+    #manual2csv('../exampleData/annotation/IFCB1_2006_158_000036.mat')
+    dir = '/Users/jfutrelle/dev/ifcb/manual'
+    for f in os.listdir(dir):
+        if re.match('IFCB._[\d_]+\\.mat',f):
+            bin_lid = re.sub('\\.mat','',f)
+            db.execute("select count(*) from manual where bin_lid = %s",(bin_lid,))
+            count = db.fetchone()[0]
+            if count > 0:
+                #print 'skipping %s...' % f
+                pass
+            else:
+                p = os.path.join(dir,f)
+                print 'processing %s...' % f
+                for (bin_lid, roi_lid, manual, svm_auto, subs) in manual2tuples(p):
+                    (bin_lid, roi_lid, manual, svm_auto) = map(sqlize_str,(bin_lid, roi_lid, manual, svm_auto))
+                    db.execute("INSERT INTO manual (bin_lid, roi_lid, manual, svm_auto) VALUES (%s,%s,%s,%s)",(bin_lid,roi_lid,manual,svm_auto))
+                    for key,value in subs.items():
+                        db.execute("INSERT INTO manual_sub (bin_lid, roi_lid, sub_column, sub_class) VALUES (%s,%s,%s,%s)",(bin_lid,roi_lid,key,value))
+                conn.commit()
 
