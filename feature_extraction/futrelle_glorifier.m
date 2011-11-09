d828b03d49dba33bd688cb690fd5e57c81568cae
Joe Futrelle <joefutrelle@gmail.com>
2011-11-09 20:37:42 +0000

    generalized stitch eval code so it can be a contest between any two stitching algorithms working on generalizing code to fetch data from xml endpoints, see #639
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@215 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 9eea056b07fd9fc1ed9862c88392e0bea9d92af6

--- feature_extraction/futrelle_glorifier.m
+++ feature_extraction/futrelle_glorifier.m
@@ -4,8 +4,9 @@ clear;
 dr = '/Users/jfutrelle/dev/workspace/ifcb/IFCB_tools/stitch/IFCB5_2010_264_102403';
 d = '/Users/jfutrelle/dev/workspace/ifcb/IFCB_tools/stitch/IFCB5_2010_264_102403_orig';
 d2 = '/Users/jfutrelle/dev/workspace/ifcb/IFCB_tools/stitch/IFCB5_2010_264_102403_r212';
-disp(d);
 rfs = dir([d '/*.png']);
+c1 = 'Heidi';
+c2 = 'Joe';
 for i = 1:length(rfs),
     name = char(rfs(i).name);
     if regexp(name,'.*_[a-z]+.png'),
@@ -23,26 +24,28 @@ for i = 1:length(rfs),
         pc = M + m;
         [M m] = phasecong3(img2, 4, 6, 2, 2.5, 0.55, 2.0, 0.3, 5,-1);
         pc2 = M + m;
-        pc_diff = pc .* pcr;
-        pc2_diff = pc2 .* pcr;
+        pc_diff = (pcr .* pc);
+        pc2_diff = (pcr .* pc2);
         pc_sum = sum(sum(pc_diff));
         pc2_sum = sum(sum(pc2_diff));
-        if pc2_sum < pc_sum,
-            label = 'Joe wins';
-        else
-            label = 'Heidi wins';
-        end;
-        if pc_sum > 0 & pc2_sum > 0,
+        if pc_sum > 0 && pc2_sum > 0,
+         if pc2_sum < pc_sum,
+             winner = c2;
+         else
+             winner = c1;
+         end;
+         label = [winner ' wins'];
          figure(98), clf;
          subplot(3,3,1), imshow(imgr), title('mask');
          subplot(3,3,4), imshow(pcr), title('pc3(mask)');
-         subplot(3,3,2), imshow(img), title('Heidi stitch');
-         subplot(3,3,5), imshow(pc), title('pc3(Heidi)');
-         subplot(3,3,8), imshow(pc_diff * 10), title('pc3(Heidi) * pc3(mask)');
-         subplot(3,3,3), imshow(img2), title('Joe stitch');
-         subplot(3,3,6), imshow(pc2), title('pc3(Joe)');
-         subplot(3,3,9), imshow(pc2_diff * 10), title('pc3(Joe) * pc3(mask)');
+         subplot(3,3,2), imshow(img), title([c1 ' stitch']);
+         subplot(3,3,5), imshow(pc), title(['pc3(' c1 ')']);
+         subplot(3,3,8), imshow(pc_diff * 10), title(['pc3(mask) * pc3(' c1 ')']);
+         subplot(3,3,3), imshow(img2), title([c2 ' stitch']);
+         subplot(3,3,6), imshow(pc2), title(['pc3(' c2 ')']);
+         subplot(3,3,9), imshow(pc2_diff * 10), title(['pc3(mask) * pc3(' c2 ')']);
          subplot(3,3,7), bar([pc_sum, pc2_sum]), title(label);
+         disp([name ',' winner]);
          pause
         end;
     end
--- feature_extraction/parse_target_xml.m
+++ feature_extraction/parse_target_xml.m
@@ -1,29 +1,11 @@
 function [ metadata ] = parse_target_xml( element )
 % Given an XML element describing a target, return it as a struct
 
-import org.w3c.dom.Node;
+metadata = parse_xml_metadata(element);
 
-metadata_nodes = element.getChildNodes;
-n = metadata_nodes.getLength;
-keys = {};
-values = {};
-for count = 1:n
-    node = metadata_nodes.item(count-1);
-    if node.getNodeType == Node.ELEMENT_NODE
-        k = char(node.getLocalName);
-        if strcmp(k,'identifier') % downcovert from Dublin Core
-            k = 'pid';
-        end
-        v = char(node.getTextContent);
-        if regexp(v,'^-?\d+\.?\d*$') % is it a number?
-            v = str2double(v);
-        end
-        keys{end+1} = k;
-        values{end+1} = v;
-    end
-end
-
-metadata = cell2struct(values, keys, 2);
+% change "identifier" (dc:identifier) to "pid"
+metadata.pid = metadata.identifier;
+rmfield(metadata,'identifier');
 
 end
 
