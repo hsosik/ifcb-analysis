f49619482db1afac61129b6ffada705a2883b5fa
Heidi Sosik <hsosik@whoi.edu>
2011-12-05 14:03:56 +0000

    partially complete code to generate blob image rotated to align major axis horizontally
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@237 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: c54d05cde0a197a42f3ceb332c9580eae9bf6ea5

---
+++ feature_extraction/blob_rotate.m
@@ -0,0 +1,31 @@
+function [ target ] = blob_rotate( target )
+%function [ target ] = blob_rotate( target )
+% take an input b&w blob image and rotate it to make the major axis of the
+% largest blob horizontal
+%Heidi M. Sosik, Woods Hole Oceanographic Institution
+%November 2011, IFCB processing
+
+persistent se2 se3; %structuring elemements for morphological processing
+
+if isempty(se2)
+    se2 = strel('disk',2);
+end;
+
+if isempty(se3)
+    se3 = strel('disk',3);
+end;
+
+geomprops = regionprops(logical(target.blob_image), 'Orientation');
+[~,ind] = sort([target.blob_props.Area], 'descend'); ind = ind(1);
+theta = -1*geomprops(ind).Orientation;
+    %    img3 = imrotate(img2, theta, 'bilinear', 'crop');
+img = imrotate(target.blob_image, theta, 'bilinear');
+img2 = imclose(img, se3);
+%img2 = imdilate(img2, se2);
+%img2 = bwmorph(img2, 'thin', 3);
+figure(1), subplot(2,2,1), imshow(target.blob_image)
+subplot(2,2,2), imshow(img), subplot(2,2,4), imshow(img2); subplot(2,2,3), imshow(target.image)
+pause
+target.blob_image_rotated = img;
+end
+
