b3374f9cd1446e0fdb406262a9fe7fcff067bbf3
Joe Futrelle <joefutrelle@gmail.com>
2011-12-21 21:27:23 +0000

    brought into line with new CSV columns (that include "stitched")
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@260 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 9d9afb5efa060cabe23613bad9c8deb1313d5358

--- feature_extraction/csv2targets.m
+++ feature_extraction/csv2targets.m
@@ -1,8 +1,8 @@
 function [ targets ] = csv2targets ( csv_data )
 % Parse the .csv format bin and return a struct representing targets
 
-[headers, pos] = textscan(csv_data,'%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s',1,'Delimiter',',');
-cols = textscan(csv_data(pos+1:end), '%d %n %n %n %n %n %n %n %n %d %d %d %d %d %n %q %q %d','Delimiter',',');
+[headers, pos] = textscan(csv_data,'%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s',1,'Delimiter',',');
+cols = textscan(csv_data(pos+1:end), '%d %n %n %n %n %n %n %n %n %d %d %d %d %d %n %q %q %d %d','Delimiter',',');
 
 % wonky format conversion necessary.
 keys = {};
