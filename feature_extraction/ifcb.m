3f6f54caa64736d1a2917420e75f073b3a399a7e
Joe Futrelle <joefutrelle@gmail.com>
2011-12-21 21:04:24 +0000

    using new namespace http://ifcb-data.whoi.edu/mvco/
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@258 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 0599d6965b8801d38b536c0ec39cd589d4d8f9ea

--- feature_extraction/ifcb.m
+++ feature_extraction/ifcb.m
@@ -1,7 +1,7 @@
 classdef ifcb
     properties (Constant)
         TERM_NAMESPACE = 'http://ifcb.whoi.edu/terms#';
-        DATA_NAMESPACE = 'http://ifcb-data.whoi.edu/';
+        DATA_NAMESPACE = 'http://ifcb-data.whoi.edu/mvco/';
         DC_NAMESPACE = 'http://purl.org/dc/elements/1.1/';
         ATOM_NAMESPACE = 'http://www.w3.org/2005/Atom';
     end
