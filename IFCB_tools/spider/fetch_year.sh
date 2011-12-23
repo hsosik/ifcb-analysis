fddd4172576b5620f5d7a1c9d5836b0691f72329
Joe Futrelle <joefutrelle@gmail.com>
2011-12-23 21:47:13 +0000

    creates day directories
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@269 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 4118701e32f9650fc9d07df7ce5b663895077f8e

--- IFCB_tools/spider/fetch_year.sh
+++ IFCB_tools/spider/fetch_year.sh
@@ -4,11 +4,13 @@ DATA_NAMESPACE='http://ifcb-data.whoi.edu/mvco/'
 DEST=/scratch/ifcb/2010
 for i in $(seq 1 366); do
     day=${YEAR}-`printf '%03d' $i`
+    day_dir=${DEST}/${YEAR}_`printf '%03d' $i`
+    mkdir -p ${day_dir}
     feed="${DATA_NAMESPACE}rss.py?day=$day"
     for url in `curl -s $feed | xsltproc atom2csv.xsl - | cut -d, -f 1 | sed -e 's/$/.zip/'`; do
 	fn=`echo $url | sed -e 's#.*/##'`
 	curl -s -O $url
-	mv $fn $DEST
+	mv $fn ${day_dir}
 	echo `date +%Y-%m-%dT%H:%M:%S%Z` $url
     done
 done
