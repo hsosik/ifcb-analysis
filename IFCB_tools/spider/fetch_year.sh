077c7b6457403749fdd6ebe802202dbc1e4ac7fc
Joe Futrelle <joefutrelle@gmail.com>
2011-12-23 21:24:32 +0000

    curl -s
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@267 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 15133c6c7df43b001bc0c4952016b03276174a35

--- IFCB_tools/spider/fetch_year.sh
+++ IFCB_tools/spider/fetch_year.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 YEAR=2010
 DATA_NAMESPACE='http://ifcb-data.whoi.edu/mvco/'
-DEST=scratch
+DEST=/scratch/ifcb/2010
 for i in $(seq 1 366); do
     day=${YEAR}-`printf '%03d' $i`
     feed="${DATA_NAMESPACE}rss.py?day=$day"
     for url in `curl -s $feed | xsltproc atom2csv.xsl - | cut -d, -f 1 | sed -e 's/$/.zip/'`; do
 	fn=`echo $url | sed -e 's#.*/##'`
-	curl -O $url
+	curl -s -O $url
 	mv $fn $DEST
 	echo `date +%Y-%m-%dT%H:%M:%S%Z` $url
     done
