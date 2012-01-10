f31de4acec188b4c42e90b1ac6a8e6e6c444436b
Joe Futrelle <joefutrelle@gmail.com>
2012-01-10 19:55:53 +0000

    looks for truly completed days!
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@311 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 64b50ec819edc3c6710303049a01677308dc08ed

--- feature_extraction/blob_extraction/show_progress.sh
+++ feature_extraction/blob_extraction/show_progress.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-nproc=`find /scratch/ifcb/blobs -name "20*_*" | wc | awk '{print $1}'`
+nproc=`find /scratch/ifcb/blobs/2* -name "log.txt" | wc -l`
 ndays=`find /scratch/ifcb -wholename "/scratch/ifcb/20*/20*_*" -type d | wc | awk '{print $1}'`
 echo Processing day ${nproc} of ${ndays} on ${HOSTNAME}
 top -b | head -19
