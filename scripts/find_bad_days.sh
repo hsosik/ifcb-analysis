189eeea0a32d5a599b7e426b182df6502674d3de
Joe Futrelle <joefutrelle@gmail.com>
2012-01-10 21:21:16 +0000

    fixed
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@317 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 1ebcc3f11c71a2307eec675bcb91d6891b6fd27a

--- scripts/find_bad_days.sh
+++ scripts/find_bad_days.sh
@@ -3,5 +3,10 @@ for day in /scratch/ifcb/blobs/2*/2*; do
     n=`ls $day | grep -v .zip | wc -l`
     if [ ! $n -eq 1 ]; then
 	echo FAIL $day
+    else
+	n=`ls $day | grep .zip | wc -l`
+	if [ $n -eq 0 ]; then
+	    echo FAIL $day
+	fi
     fi
 done
