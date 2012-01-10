ad9a59fcf339f814e3c7cb1acc1c0e9d886c3748
Heidi Sosik <hsosik@whoi.edu>
2012-01-10 20:23:04 +0000

    forgot to add new files before last commit
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@313 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 68edc065d4e34fa6e86ce996a591f12cc226bbc3

---
+++ analyze_by_class/make_indexseries_figs.m
@@ -0,0 +1,26 @@
+function [ ] = make_indexseries_figs(class2use,ymat,maxsubplots)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+numfig = ceil(length(class2use)/maxsubplots);
+indcount = 1;
+for figcount = 1:numfig,
+    figure
+    orient tall
+    fpos = get(gcf, 'position');
+    set(gcf, 'position', [fpos(1) fpos(2)/10 fpos(3) fpos(4)*2])
+    if figcount < numfig,
+        subplots = maxsubplots;
+    else
+        subplots = length(class2use)-indcount;
+    end;
+    for subcount = 1:subplots,
+        subplot(maxsubplots,1,subcount)
+        plot(ymat(:,indcount), '.-')
+        yl = ylim;
+        text(200,.9*yl(2), class2use(indcount), 'interpreter', 'none', 'verticalalignment', 'top')
+        indcount = indcount + 1;
+    end;
+end
+end
+
---
+++ analyze_by_class/plot_indexseries_manual.m
@@ -0,0 +1,11 @@
+function [ ] = plot_indexseries_manual( countfile )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+load(countfile)
+%[matdate_bin, classcount_bin, ml_analyzed_mat_bin] = make_day_bins(matdate,classcount, ml_analyzed_mat);
+%save([countfile '_day'], 'matdate_bin', 'classcount_bin', 'ml_analyzed_mat_bin', 'class2use')
+maxsubplots = 5;
+classind = find(nansum(classcount));
+make_indexseries_figs(class2use(classind),classcount(:,classind)./ml_analyzed_mat(:,classind),maxsubplots); 
+%make_time_figs(class2use(classind),matdate_bin, classcount_bin(:,classind)./ml_analyzed_mat_bin(:,classind),maxsubplots); 
+
