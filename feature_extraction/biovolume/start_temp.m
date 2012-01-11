0c7b026bed51a4ae5a90ed64bd7c4cbc7cce2815
Heidi Sosik <hsosik@whoi.edu>
2012-01-11 00:19:10 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@320 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 781821330cf50637470d880f3cc8fcc1cce1b2e8

--- feature_extraction/biovolume/start_temp.m
+++ feature_extraction/biovolume/start_temp.m
@@ -2,6 +2,10 @@ resultpath = '\\raspberry\d_work\IFCB1\ifcb_data_mvco_jun06\Manual_fromClass\';
 load([resultpath 'manual_list']) %load the manual list detailing annotate mode for each sample file
 filelist = manual_list(2:end,1);
 filelist = regexprep(filelist,'.mat','');
-out_dir = 'C:\work\IFCB\ifcb_data_MVCO_jun06\biovolume2\';
+out_dir = '\\queenrose\ifcb_data_mvco_jun06\biovolume\';
 in_dir = 'http://ifcb-data.whoi.edu/mvco/';
-batch_volume( in_dir, filelist, out_dir );
+files_done = dir([out_dir 'IFCB*.mat']);
+files_done = char(files_done.name)
+files_done = cellstr(files_done(:,1:end-4));
+filelist2 = setdiff(filelist, files_done);
+batch_volume( in_dir, filelist2, out_dir );
