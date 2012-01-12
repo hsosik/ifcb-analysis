2a4939cbb2a81070c4d0eeef67deac55813bd734
Heidi Sosik <hsosik@whoi.edu>
2012-01-12 15:12:37 +0000

    forgot to save last change with previous commit?
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@331 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 3154e41ffd6c80834edda525698f65247148d8c7

--- analyze_by_class/plot_timeseries_manual.m
+++ analyze_by_class/plot_timeseries_manual.m
@@ -6,7 +6,6 @@ maxsubplots = 5;
 classind = find(nansum(classcount));
 if daybin_flag,
     [matdate_bin, classcount_bin, ml_analyzed_mat_bin] = make_day_bins(matdate,classcount, ml_analyzed_mat);
-    %save([countfile '_day'], 'matdate_bin', 'classcount_bin', 'ml_analyzed_mat_bin', 'class2use')
     make_time_figs(class2use(classind),matdate_bin, classcount_bin(:,classind)./ml_analyzed_mat_bin(:,classind),maxsubplots); 
 else
     make_time_figs(class2use(classind),matdate, classcount(:,classind)./ml_analyzed_mat(:,classind),maxsubplots);    
