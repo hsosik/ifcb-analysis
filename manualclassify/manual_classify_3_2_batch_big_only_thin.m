d1885229ee5912aed474c15d4a38aa268476bf1b
Emily Peacock <epeacock@whoi.edu>
2012-03-14 16:46:16 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@396 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 1a58da56797b7f030f1ebf6752c8bb27fbd5eb62

--- manualclassify/manual_classify_3_2_batch_big_only_thin.m
+++ manualclassify/manual_classify_3_2_batch_big_only_thin.m
@@ -33,8 +33,8 @@
 
 close all; clear all;
 
-filenum2start = 159;  %USER select file number to begin (within the chose day)
-batch_classnum = [1:54]; %USER which class do you want to view in batch mode, Heidi 10/7/09, only works for correct_or_subdivide for now
+filenum2start = 1;  %USER select file number to begin (within the chose day)
+batch_classnum = [25]; %USER which class do you want to view in batch mode, Heidi 10/7/09, only works for correct_or_subdivide for now
 
 pick_mode = 'correct_or_subdivide'; %USER choose one from case list below
 big_only = 1; %case for picking Laboea and tintinnids only
@@ -46,8 +46,10 @@ stitchpath = '\\queenrose\ifcb_data_mvco_jun06\stitchxxxx\';  %%USER set, roi st
 class_filestr = '_class_24May07_revDec11'; %USER set, string appended on roi name for class files
 
 %filelist = dir([resultpath 'IFCB1_2009_???_00*']);
-filelist = get_filelist_manual([resultpath 'manual_list'],7,[2011], 'only'); %manual_list, column to use, year to find
+filelist = get_filelist_manual([resultpath 'manual_list'],7,[2006:2011], 'only'); %manual_list, column to use, year to find
 %load Ditylum_ciliate_files; filelist = Ditylum_ciliate_files; clear Ditylum_ciliate_files
+%load checksmall_filelist; filelist = cellstr([char(filelist) repmat('.mat', length(filelist),1)]);
+%filelist = cell2struct(filelist,{'name'},2);
 
 if ~exist(resultpath, 'dir'),
     dos(['mkdir ' resultpath]);
@@ -99,9 +101,9 @@ switch pick_mode
         %[junk, class2view1] = setdiff(class2use_pick1, {'bad', 'mix'});  %use this to exclude some classes
         class2view1 = sort(class2view1);
         class2view1 = class2view1(batch_classnum); %set category(ies) to view, needed for batch mode
-        %class2view1 = [];  %use this to skip all original auto categories
+        class2view1 = [];  %use this to skip all original auto categories
         class2view2 = 1:length(class2use_sub);
-        class2view2 = []; %Comment out if using a subdivided class
+        %class2view2 = []; %Comment out if using a subdivided class
     otherwise
         disp('Invalid pick_mode. Check setting in m-file.')
         return
