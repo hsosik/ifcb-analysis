b60eefc70f49c40828798bdf8a6f3316f20122b4
Heidi Sosik <hsosik@whoi.edu>
2012-03-28 14:43:57 +0000

    add preliminary code to apply random forest classifier to selected IFCB data bins
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@410 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: e57f38dcebe1b9b142b917b7348eb6b450cf7514

---
+++ feature_extraction/batch_features_bins/add_derived_features.m
@@ -0,0 +1,22 @@
+function [ feature_mat, featitles ] = add_derived_features( feature_mat, featitles)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+a = strmatch('Area', featitles, 'exact'); b = strmatch('Perimeter', featitles, 'exact'); 
+feature_mat = [feature_mat; feature_mat(a,:)./feature_mat(b,:).^2; feature_mat(a,:)./feature_mat(b,:)]; %A/P^2 compactness or circularity index; A/P roundness index
+featitles = [featitles; 'Area/Perimeter^2'; 'Area/Perimeter'];
+
+a = strmatch('Hflip', featitles, 'exact'); b = strmatch('H90', featitles, 'exact'); c = strmatch('H180', featitles, 'exact'); 
+feature_mat = [feature_mat; feature_mat(b,:)./feature_mat(a,:); feature_mat(b,:)./feature_mat(c,:); feature_mat(a,:)./feature_mat(c,:)]; %A/P^2 compactness or circularity index; A/P roundness index
+featitles = [featitles; 'H90/Hflip'; 'H90/H180'; 'Hflip/H180'];
+
+a = strmatch('summedConvexPerimeter', featitles, 'exact'); b = strmatch('summedPerimeter', featitles, 'exact'); 
+feature_mat = [feature_mat; feature_mat(a,:)./feature_mat(b,:).^2]; %A/P^2 compactness or circularity index; A/P roundness index
+featitles = [featitles; 'summedConvexPerimeter/Perimeter'];
+
+a = strmatch('RotatedBoundingBox_xwidth', featitles, 'exact'); b = strmatch('RotatedBoundingBox_ywidth', featitles, 'exact'); c = strmatch('RotatedArea', featitles, 'exact'); 
+feature_mat = [feature_mat; feature_mat(c,:)./(feature_mat(a,:).*feature_mat(b,:))]; %solidity of rotated bounding box
+featitles = [featitles; 'rotated BoundingBox solidity'];
+
+end
+
---
+++ feature_extraction/batch_features_bins/format_features.m
@@ -0,0 +1,56 @@
+function [ feature_mat, featitles ] = format_features( bin )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+s_temp = bin.features;
+Rings = [s_temp.Rings];
+Wedges = [s_temp.Wedges];
+HOG = [s_temp.HOG];
+s_temp = rmfield(s_temp, {'Rings' 'Wedges' 'HOG'});
+%n = [s_temp.numBlobs];
+n = zeros(1,length(s_temp));
+for i = 1:length(s_temp),
+    n(i) = size(s_temp(i).Area,2);
+end;
+largest_ind = [1 cumsum(n(1:end-1))+1];
+%keyboard
+
+fields = fieldnames(s_temp);
+fieldstemp = setdiff(fields, {'RotatedArea' 'RotatedBoundingBox_xwidth' 'RotatedBoundingBox_ywidth'})';
+for i = 1:length(fieldstemp),
+    field = char(fieldstemp(i));
+    list = [s_temp.(field)];
+    if length(list) > length(n),
+        s_largest.(field) = list(largest_ind);
+    else
+        s_largest.(field) = list;
+    end;
+end;
+%special case for Rotated features since sometimes fewer blobs
+fieldstemp = {'RotatedArea' 'RotatedBoundingBox_xwidth' 'RotatedBoundingBox_ywidth'};
+n_rot = zeros(1,length(s_temp));
+for i = 1:length(s_temp),
+    n_rot(i) = size(s_temp(i).RotatedArea,2);
+end;
+largest_ind = [1 cumsum(n_rot(1:end-1))+1];
+for i = 1:length(fieldstemp),
+    field = char(fieldstemp(i));
+    list = [s_temp.(field)];
+    if length(list) > length(n_rot),
+        s_largest.(field) = list(largest_ind);
+    else
+        s_largest.(field) = list;
+    end;
+end;
+
+output_largest.features = s_largest;
+temp = struct2cell(s_largest);    
+feature_mat = [cell2mat(temp); Wedges; Rings; HOG];
+feature_mat(isnan(feature_mat)) = 0;
+  
+featitles = fieldnames(output_largest.features);
+nring = size(Rings,1); nwedge = size(Wedges,1); nhog = size(HOG,1);
+featitles = [featitles; cellstr([repmat('Wedge:',nwedge,1) num2str((1:nwedge)')]); cellstr([repmat('Ring:',nring,1) num2str((1:nring)')]); cellstr([repmat('HOG:',nhog,1) num2str((1:nhog)')])];
+
+end
+
