48ee03f59cdd3682a21f858901660ee5914b37c6
Heidi Sosik <hsosik@whoi.edu>
2012-03-28 14:55:25 +0000

    revise to skip files for which classifier output already exists
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@411 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 02ee12302760d2f89cf396a7e246328d17d83541

--- feature_extraction/batch_features_bins/batch_classify.m
+++ feature_extraction/batch_features_bins/batch_classify.m
@@ -15,34 +15,38 @@ ciliate_classnum = strmatch('ciliate', class2use, 'exact');
 for filecount = 1:length(filelist),
     filename = filelist(filecount).name;
     disp(['load ' filename])
-    load([feature_path filename])
-    t = char(out.pid);
-    roi_num = str2num(t(:,end-4:end));
-    disp('format features')
-    [ feature_mat, featitles ] = format_features( out );
-    [ feature_mat, featitles ] = add_derived_features( feature_mat, featitles);
-    if filecount == 1,
-        [~,i] = setdiff(featitles, {'FilledArea' 'summedFilledArea' 'Area' 'ConvexArea' 'MajorAxisLength' 'MinorAxisLength' 'Perimeter'}');
-        featitles = featitles(i);
+    if ~exist([class_path filename], 'file'), 
+        load([feature_path filename])
+        t = char(out.pid);
+        roi_num = str2num(t(:,end-4:end));
+        disp('format features')
+        [ feature_mat, featitles ] = format_features( out );
+        [ feature_mat, featitles ] = add_derived_features( feature_mat, featitles);
+        if filecount == 1,
+            [~,i] = setdiff(featitles, {'FilledArea' 'summedFilledArea' 'Area' 'ConvexArea' 'MajorAxisLength' 'MinorAxisLength' 'Perimeter'}');
+            featitles = featitles(i);
+        end;
+        feature_mat = feature_mat(i,:)';
+        disp('classifying...')
+        Y = predict(tb_24Mar2012,feature_mat);
+        numrois = roi_num(end);
+        classlist = NaN(numrois,4);
+        classlist(:,1) = 1:numrois;
+        for ii = 1:nclass,
+            %need to use roi_num convention to match expected input for
+            %previous manual_classify etc.
+            classlist(roi_num(strmatch(class2use(ii), Y, 'exact')),3) = ii; 
+        end;
+        for ii = 1:length(class2use_sub4),
+            %need to use roi_num convention to match expected input for
+            %previous manual_classify etc.
+            ind = strmatch(class2use_sub4(ii), Y, 'exact');
+            classlist(roi_num(ind),4) = ii; 
+            classlist(roi_num(ind),3) = ciliate_classnum;  
+        end;
+        disp('save results')
+        save([class_path filename], 'classlist', 'class2use_auto', 'class2use_manual', 'list_titles') 
+    else
+        disp('already done')
     end;
-    feature_mat = feature_mat(i,:)';
-    disp('classifying...')
-    Y = predict(tb_24Mar2012,feature_mat);
-    numrois = roi_num(end);
-    classlist = NaN(numrois,4);
-    classlist(:,1) = 1:numrois;
-    for ii = 1:nclass,
-        %need to use roi_num convention to match expected input for
-        %previous manual_classify etc.
-        classlist(roi_num(strmatch(class2use(ii), Y, 'exact')),3) = ii; 
-    end;
-    for ii = 1:length(class2use_sub4),
-        %need to use roi_num convention to match expected input for
-        %previous manual_classify etc.
-        ind = strmatch(class2use_sub4(ii), Y, 'exact');
-        classlist(roi_num(ind),4) = ii; 
-        classlist(roi_num(ind),3) = ciliate_classnum;  
-    end;
-    disp('save results')
-    save([class_path filename], 'classlist', 'class2use_auto', 'class2use_manual', 'list_titles') 
 end;
