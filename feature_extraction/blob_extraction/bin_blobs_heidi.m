dffb745a81529fa33d912a9f71d6a6335c9793ff
Heidi Sosik <hsosik@whoi.edu>
2012-03-28 18:27:48 +0000

    short-term kludge to make 2012 blob zips on queenrose
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@416 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: d9479eb0d98c112393226331632a1cb0e54c9937

---
+++ feature_extraction/blob_extraction/bin_blobs_heidi.m
@@ -0,0 +1,50 @@
+function [ ] = bin_blobs( in_dir, file, out_dir )
+%BIN_BLOBS Summary of this function goes here
+
+debug = false;
+
+function log(msg) % not to be confused with logarithm function
+    logmsg(['bin_blobs ' msg],debug);
+end
+
+archive = [out_dir filesep regexprep(file,'.zip','_blobs.zip')];
+if exist(archive,'file'),
+    log(['SKIPPING ' file]);
+    return
+end
+
+% load the zip file
+log(['LOAD ' file]);
+targets = get_bin_file([in_dir  file]);
+nt = length(targets.targetNumber);
+log(['PROCESSING ' num2str(nt) ' target(s) from ' file]);
+
+png_dir = [out_dir filesep regexprep(file,'.zip','')];
+mkdir(png_dir);
+
+png_paths = {};
+% for each target
+for i = 1:nt,
+    target = {};
+    % configure feature extraction
+    target.config = configure();
+    % get the image
+    target.image = cell2mat(targets.image(i));
+    % compute the blob mask (result in target.blob_image)
+    target = blob(target);
+    % now output the blob image as a 1-bit png
+    png_path = [png_dir filesep regexprep(file,'.zip',sprintf('_%05d.png',targets.targetNumber(i)))];
+    imwrite(target.blob_image,png_path,'bitdepth',1);
+    png_paths = [png_paths; {png_path}];
+end
+
+zip(archive, png_paths, png_dir);
+
+log(['SAVING ' archive]);
+
+rmdir(png_dir,'s');
+
+log(['DONE ' file]);
+
+end
+
---
+++ feature_extraction/blob_extraction/day_blobs_heidi.m
@@ -0,0 +1,62 @@
+function [ ] = day_blobs_heidi( in_dir, out_dir )
+% Accept parameters specifying a directory full of .zip files (one for each bin) and a directory in which to place the output .zip files
+% Start a matlab pool
+% Process the bins in parallel.
+% Log progress and errors (so the calling script can monitor progress and detect failures)
+% Clean up all temporary storage
+% Shut down the pool
+% Report completion status
+
+debug = false;
+
+function log(msg) % not to be confused with logarithm function
+    logmsg(['day_blobs ' msg],debug);
+end
+
+if not(debug),
+    try
+        matlabpool;
+        log('POOL - started');
+    catch e %#ok<NASGU>
+        log('WARNING - workers cannot start, or already active');
+    end;
+end
+
+%daydir = dir([in_dir filesep '*.zip']);
+%daydir = dir(['\\demi\ifcbnew\ifcb5_2012_009\*.roi']);
+days = dir(['\\demi\ifcbnew\ifcb5_2012*']);
+daydir = [];
+for ii = 1:length(days),
+    daydir = [daydir; dir(['\\demi\ifcbnew\' days(ii).name '\*.roi'])];
+end;
+
+if not(debug),
+    parfor daycount = 1:length(daydir)
+        try
+            %bin_blobs_heidi(in_dir, daydir(daycount).name, out_dir);
+            zipname = regexprep(daydir(daycount).name,'roi','zip');
+            bin_blobs_heidi(in_dir, zipname, out_dir);
+        catch e
+   	    logmsg(['day_blobs FAIL ' daydir(daycount).name],debug);
+        end
+    end
+else
+    for daycount = 1:length(daydir)
+        %bin_blobs_heidi(in_dir, daydir(daycount).name, out_dir);
+        zipname = regexprep(daydir(daycount).name,'roi','zip');
+        bin_blobs_heidi(in_dir, zipname, out_dir);
+    end
+end
+
+
+if not(debug),
+    try
+        matlabpool close;
+        log('POOL - stopped');
+    catch e %#ok<NASGU>
+        log('WARNING - workers cannot stop, or already stopped');
+    end;
+end
+
+end
+
