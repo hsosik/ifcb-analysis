b53296034519eadc71090e6a44a97d7f7e79b66f
Heidi Sosik <hsosik@whoi.edu>
2012-04-01 13:47:39 +0000

    variations on export_png
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@420 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 2dcd9b1c519379c79c3240fb95a2db2efdfa43c2

--- IFCB_tools/export_png_manual_only_fromWebServices_newonly.m
+++ IFCB_tools/export_png_manual_only_fromWebServices_newonly.m
@@ -1,13 +1,13 @@
 %USER SET PATHS
 %where are your manual classification results? same resultpath as for manual_classify_stream2b.m
 resultpath = '\\raspberry\d_work\IFCB1\ifcb_data_mvco_jun06\Manual_fromClass\';
-outputpath = '\\queenrose\ifcb_data_mvco_jun06\train_new_only\'; %USER where to write out pngs
+outputpath = '\\queenrose\ifcb_data_mvco_jun06\train_new_only2\'; %USER where to write out pngs
 urlbase = 'http://ifcb-data.whoi.edu/mvco/';
 
 set1path = '\\queenrose\ifcb_data_mvco_jun06\train_04Nov2011_fromWebServices\';
 
-%resultfilelist = get_filelist_manual([resultpath 'manual_list'],3,[2006:2011], 'all'); %manual_list, column to use, year to find, Copied here by Emily P. from manual_classify_batch_3_1
-resultfilelist = dir([resultpath 'IFCB*.mat']);
+resultfilelist = get_filelist_manual([resultpath 'manual_list'],7,[2006:2011], 'all'); %manual_list, column to use, year to find, Copied here by Emily P. from manual_classify_batch_3_1
+%resultfilelist = dir([resultpath 'IFCB*.mat']);
 %resultfilelist = [dir([resultpath 'IFCB1_2006_???_00*.mat']); dir([resultpath 'IFCB1_2007_???_00*.mat'])];
 %case 3 for ciliate, big ciliate, diatoms, ditylum ONLY
 %load([resultpath 'manual_list.mat']) %col 2-7: {'all categories','ciliates','ditylum','diatoms','big ciliates','special big only'}
@@ -22,7 +22,9 @@ resultfilelist = cellstr(resultfilelist(:,1:end-4));
 %category = setdiff(class2use_manual, {'bad' 'ciliate' 'crypto' 'dino30' 'roundCell' 'clusterflagellate' 'other' 'mix' 'detritus' 'mix_elongated' 'flagellate' });  %use this syntax to export all EXCEPT the listed categories
 %keyboard
 %category = class2use_manual; %use this syntax to export ALL categories
-category = {'clusterflagellate' 'Coscinodiscus' 'Dinophysis' 'Euglena' 'Gyrodinium' 'kiteflagellates' 'Lauderia' 'Licmophora' 'Odontella' 'Pyramimonas' 'Stephanopyxis'}; %use this syntax to export ONLY the listed categories
+%category = {'clusterflagellate' 'Coscinodiscus' 'Dinophysis' 'Euglena' 'Gyrodinium' 'kiteflagellates' 'Lauderia' 'Licmophora' 'Odontella' 'Pyramimonas' 'Stephanopyxis'}; %use this syntax to export ONLY the listed categories
+category = {'Ceratium' 'Coscinodiscus' 'Dinophysis' 'Ephemera' 'Eucampia' 'Euglena' 'Guinardia_flaccida' 'Guinardia_striata' 'Hemiaulus' 'Lauderia' 'Licmophora'...
+    'Odontella' 'Paralia' 'Phaeocystis' 'Pyramimonas' 'Stephanopyxis' 'bad' 'clusterflagellate' 'kiteflagellates'};
 
 %make subdirs for tiffs
 for count = 1:length(category),
@@ -42,6 +44,7 @@ for count = 1:length(resultfilelist),
     load([resultpath resultfile])
     
     disp(resultfile)
+    adc_struct = get_targets([urlbase resultfile]);
     
     %loop over classes and save pngs to subdirs
     for count2 = 1:length(category);
@@ -54,9 +57,12 @@ for count = 1:length(resultfilelist),
             end;
             %list2 = [repmat([resultfile '_'], length(ind),1) num2str(ind) repmat('.png', length(ind),1)];
             [list_merge,idx] = setdiff(list2, {list1{count2}.name}');
-            ind = ind(idx);
+            ind = ind(idx); %disp(length(ind))
+            %ind = intersect(ind,adc_struct.targetNumber); %skip the missing ones, e.g., 0-sized ROIs
+            %disp(length(ind))
             for count = 1:length(ind),
-                num = classlist(ind(count),1);
+                %num = classlist(ind(count),1); %%This is same as ind(count)!!!!!
+                num = ind(count);
                 pngname = [resultfile '_' num2str(num,'%05.0f')];
                 image = get_image([urlbase pngname]);
                 if length(image) > 0,
