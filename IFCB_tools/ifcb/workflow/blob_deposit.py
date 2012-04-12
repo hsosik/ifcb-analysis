d91edc7b6114d18976ad6eac211ce00b6a89ef66
Joe Futrelle <joefutrelle@gmail.com>
2012-04-12 20:05:25 +0000

    blob deposit script is working
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@431 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: e2cda86dcabbb8ec246a02eec58f73c1da4a96f9

--- IFCB_tools/ifcb/workflow/blob_deposit.py
+++ IFCB_tools/ifcb/workflow/blob_deposit.py
@@ -6,12 +6,16 @@ from ifcb.util import iso8601utcnow
 import sys
 import os
 import json
+import urllib2 as urllib
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/deposit/<path:pid>',methods=['POST'])
-def deposit(pid):
+def jsonr(s):
+    return Response(json.dumps(s), mimetype='application/json')
+
+@app.route('/deposit/blobs/<path:pid>',methods=['POST'])
+def deposit_impl(pid):
     zipdata = request.data
     destpath = dest(pid)
     try:
@@ -22,14 +26,49 @@ def deposit(pid):
         shutil.copyfileobj(StringIO(zipdata), out)
     utcnow = iso8601utcnow()
     message = '%s wrote %d bytes to %s' % (utcnow, len(zipdata), destpath)
-    receipt = dict(
+    return jsonr(dict(
         status='OK',
         time=utcnow,
         message=message,
         pid=pid,
         path=destpath
-    )
-    return Response(json.dumps(receipt), mimetype='application/json')
+    ))
+                     
+@app.route('/exists/blobs/<path:pid>')
+def exists_impl(pid):
+    destpath = dest(pid)
+    exists = os.path.exists(destpath)
+    if exists:
+        message = '%s %s FOUND at %s' % (iso8601utcnow(), pid, destpath)
+    else:
+        message = '%s %s NOT FOUND at %s' % (iso8601utcnow(), pid, destpath)
+    utcnow = iso8601utcnow()
+    return jsonr(dict(
+        exists=exists,
+        time=utcnow,
+        message=message,
+        pid=pid,
+        path=destpath
+    ))
+
+# client code
+
+class BlobDeposit(object):
+    def __init__(self,url_base='http://localhost:5000'):
+        self.url_base = url_base
+
+    def exists(self,pid):
+        req = urllib.Request('%s/exists/blobs/%s' % (self.url_base, pid))
+        resp = json.loads(urllib.urlopen(req).read())
+        return resp['exists']
+
+    def deposit(self,pid,zipfile):
+        with open(zipfile,'r') as inzip:
+            zipdata = inzip.read()
+            req = urllib.Request('%s/deposit/blobs/%s' % (self.url_base, pid), zipdata)
+            req.add_header('Content-type','application/x-ifcb-blobs')
+            resp = json.loads(urllib.urlopen(req).read())
+            return resp
 
 if __name__=='__main__':
     try:
