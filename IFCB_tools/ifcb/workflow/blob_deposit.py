5978e7223b223ff25624a2c3d4a913c52f0498a3
Joe Futrelle <joefutrelle@gmail.com>
2012-04-12 16:53:56 +0000

    completely untested web service for depositing blob data
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@429 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 9d7e70991412fb01018d1ec205f73e4e88f00a4f

---
+++ IFCB_tools/ifcb/workflow/blob_deposit.py
@@ -0,0 +1,35 @@
+from flask import Flask, url_for
+from blob_extraction import dest 
+import shutil
+from StringIO import StringIO
+from ifcb.util import iso8601utcnow
+import sys
+import os
+
+app = Flask(__name__)
+
+@app.route('/deposit/<path:pid>',methods=['POST'])
+def deposit(pid):
+    zipdata = request.data
+    destpath = dest(pid)
+    try:
+        os.makedirs(os.path.dirname(destpath))
+    except:
+        pass
+    with open(destpath,'w') as out:
+        shutil.copyfileobj(StringIO(zipdata), out)
+    utcnow = iso8601utcnow()
+    message = '%s wrote %d bytes to %s' % (utcnow, len(zipdata), destpath)
+    receipt = dict(
+        status='OK',
+        time=utcnow,
+        message=message,
+        pid=pid,
+        path=destpath
+    )
+
+if __name__=='__main__':
+    try:
+        app.run(host=sys.argv[1])
+    except:
+        app.run(host='0.0.0.0')
