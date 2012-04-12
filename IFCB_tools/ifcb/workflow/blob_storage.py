460772afa8c827a66c49e632c32183ff10dcdd67
Joe Futrelle <joefutrelle@gmail.com>
2012-04-12 20:34:19 +0000

    oops need shared storage module
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@433 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: d85d0066b1f2324c850ea3dad353b8301af077b1

---
+++ IFCB_tools/ifcb/workflow/blob_storage.py
@@ -0,0 +1,31 @@
+import re
+import os
+from ifcb.io.pids import parse_id
+
+blob_years='/data/vol4/blobs'
+
+def lid(pid):
+    return parse_id(pid).as_lid
+
+def year(pid):
+    return parse_id(pid).year
+
+def year_day(pid):
+    return parse_id(pid).yearday
+
+def zipname(pid):
+    return lid(pid)+'_blobs_v2.zip'
+
+def dest(pid):
+    return os.path.join(blob_years,year(pid),year_day(pid),zipname(pid))
+
+def local_exists(bin_pid):
+    return os.path.exists(dest(bin_pid))
+
+def local_deposit(bin_pid,zipfile):
+    dest_file = dest(bin_pid)
+    try:
+        os.makedirs(os.path.dirname(dest_file))
+    except:
+        pass
+    shutil.move(zipfile,dest_file)
