17e0946402822a31d60f485b0d59e0a3e382ec0d
Heidi Sosik <hsosik@whoi.edu>
2012-04-13 00:39:32 +0000

    preliminary script to estimate ml_analyzed for healy 2010 data; needs attention to decide what value to use for min_proc_time (see issue #1196 in redmine)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@435 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 2fbb74f77cdb48b36c6ae3938d4d2764e321c01a

---
+++ analyze_by_class/save_ml_analyzed_icescape.m
@@ -0,0 +1,44 @@
+roipath = '\\floatcoat\laneylab\projects\HLY1001\data\imager\underway\';
+
+%syringe and pump settings
+steps_per_sec = 40;
+ml_per_step = 5/48000;
+flowrate = ml_per_step * steps_per_sec;  %ml/sec
+minproctime = 0.14;
+adcpath = roipath;
+filelist = dir([roipath '*.adc']);
+runtime = NaN(length(filelist),1);  %initialize large to avoid growing vectors
+looktime = runtime;
+%looktime2 = runtime;
+numtriggers = runtime;
+numrois = runtime;   
+for count = 1:length(filelist),
+    filename = filelist(count).name;
+    filename = filename(1:end-4);
+    temp = dir([roipath filename '.roi']);
+    disp([num2str(count) ' of ' num2str(length(filelist)) ': ' filename])
+    adcdata = importdata([adcpath filename '.adc'], ',');  %robust to last line having a few missing values...
+    %rob - fix time stamps for files that span midnight
+    negind = find(adcdata(:,8)<0);
+    adcdata(negind,8) = adcdata(negind,8) + 24*60*60;
+    negind = find(adcdata(:,9)<0);
+    adcdata(negind,9) = adcdata(negind,9) + 24*60*60;
+    adctime = adcdata(:,8:9)';
+    runtime(count) = adcdata(end,9); %Heidi, added in place of earlier version after Rob fixed midnight crossing problem above
+
+    longproc_ind = find(adcdata(2:end,8)-adcdata(1:end-1,9)>minproctime);
+    %only use adcdata line once in cases with 2 or more rois each with new line
+    [junk,i]= unique(adcdata(longproc_ind,1));
+    longproc_ind = longproc_ind(i);
+    looktime(count) = adcdata(end,9)- adcdata(end,1)*minproctime - sum(adcdata(longproc_ind+1,8)-adcdata(longproc_ind,9)) + length(longproc_ind)*minproctime;
+    longproc_ind = find(adcdata(2:end,8)-adcdata(1:end-1,9)>minproctime*2);
+%        %only use adcdata line once in cases with 2 or more rois each with new line
+%        [junk,i]= unique(adcdata(longproc_ind,1));
+%        longproc_ind = longproc_ind(i);
+%        looktime2(count) = adcdata(end,9)- adcdata(end,1)*minproctime*2 - sum(adcdata(longproc_ind+1,8)-adcdata(longproc_ind,9)) + length(longproc_ind)*minproctime*2;
+
+    numtriggers(count) = adcdata(end,1);
+    numrois(count) = size(adcdata,1);
+end;
+ml_analyzed = flowrate.*looktime;
+save([roipath 'ml_analyzed'], 'ml_analyzed', 'runtime', 'looktime', 'numrois', 'numtriggers', 'filelist', 'minproctime') %, 'looktime2')
