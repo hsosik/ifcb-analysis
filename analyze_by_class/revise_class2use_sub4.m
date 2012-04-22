77b8bf2997b7159082477fbf88d902c9685fab4e
Heidi Sosik <hsosik@whoi.edu>
2012-04-22 19:13:46 +0000

    quick and dirty to correct new class2use_sub4 entries that have unallowed characters and spaces
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@449 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 959dd0826a9b6f22985bcdce6438ddf6a7f3fcae

---
+++ analyze_by_class/revise_class2use_sub4.m
@@ -0,0 +1,19 @@
+resultpath = '\\raspberry\d_work\IFCB1\ifcb_data_mvco_jun06\Manual_fromClass\';
+load([resultpath 'manual_list']) %load the manual list detailing annotate mode for each sample file
+filelist = manual_list(2:end,1);
+
+class2use_sub4new = {'not_ciliate'    'ciliate_mix'    'tintinnid'    'Myrionecta'    'Laboea'    'S_conicum'...
+    'tiarina'    'strombidium1'    'Scaudatum'    'Tontonia' 'strombidium2'    'S_wulffi'    'S_inclinatum'...
+    'Euplotes'    'Didinium'    'Leegaardiella'    'Sol'    'strawberry'    'S_capitatum'};
+
+for ii = 1:length(filelist),
+    disp(filelist{ii})
+    clear class2use_sub4
+    load([resultpath filelist{ii}], 'class2use_sub4');
+    if exist('class2use_sub4', 'var'),
+        class2use_sub4 = class2use_sub4new;
+        save([resultpath filelist{ii}], 'class2use_sub4', '-append');
+    else
+        keyboard
+    end;
+end;
