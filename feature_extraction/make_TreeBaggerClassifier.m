3369cb78829958d11da39d1a5540abcf52a41a92
Heidi Sosik <hsosik@whoi.edu>
2012-05-03 17:12:45 +0000

    temporary script to make a random forest classifier
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@477 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 5aa1314856db3db0c7082cfa2db383569ad7e089

--- feature_extraction/make_TreeBaggerClassifier.m
+++ feature_extraction/make_TreeBaggerClassifier.m
@@ -1,15 +1,18 @@
 %run compile_train_features3.m to generate train and class_vector
+load compiled_train
+datestring = datestr(now, 'ddmmmyyyy');
 
+class_vector = classes(class_vector);
+disp('Growing trees...please be patient')
 b = TreeBagger(300,train,class_vector,'Method','c','OOBVarImp','on','MinLeaf',1);
 figure, hold on
 plot(oobError(b), 'b-');
 xlabel('Number of Grown Trees');
 ylabel('Out-of-Bag Classification Error');
 
-b = growTrees(b,250); %specify how many to add
-plot(oobError(b3), 'g');
-
-
+%b = growTrees(b,250); %specify how many to add
+%plot(oobError(b3), 'g');
+save(['SaltPond_trees_' datestring],'b', 'targets', 'featitles', 'classes')
 [Yfit,Sfit] = oobPredict(b);  
 c1 = confusionmat(b.Y,Yfit); %transposed from mine
 
@@ -19,7 +22,7 @@ Sp = 1-(sum(c1)-diag(c1)')./total';
 sum(sum(c1)-diag(c1)')/sum(total)
 text_offset = .1;
 figure, bar([total diag(c1) sum(c1)'-diag(c1)])
-legend('total in set', 'detected', 'missed')
+legend('total in set', 'detected', 'wrong')
 set(gca, 'xtick', 1:length(classes), 'xticklabel', [])
 text(1:length(classes), -text_offset.*ones(size(classes)), classes, 'interpreter', 'none', 'horizontalalignment', 'right', 'rotation', 45) 
 set(gca, 'position', [ 0.13 0.35 0.8 0.6])
@@ -27,3 +30,4 @@ figure, bar([Pd Sp'])
 set(gca, 'xtick', 1:length(classes), 'xticklabel', [])
 text(1:length(classes), -text_offset.*ones(size(classes)), classes, 'interpreter', 'none', 'horizontalalignment', 'right', 'rotation', 45) 
 set(gca, 'position', [ 0.13 0.35 0.8 0.6])
+legend('Pd', 'Sp')
