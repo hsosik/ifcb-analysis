b85b11d5a0a620a695e4c3a1ec05958b573ccfc9
Heidi Sosik <hsosik@whoi.edu>
2012-05-04 16:32:12 +0000

    special case for healy1001
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@480 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 218be75f66f188798038c58dc39143b3ca818f95

---
+++ feature_extraction/ifcb_healy1001asb.m
@@ -0,0 +1,9 @@
+classdef ifcb_healy1001asb
+    properties (Constant)
+        TERM_NAMESPACE = 'http://ifcb.whoi.edu/terms#';
+        DATA_NAMESPACE = 'http://ifcb-data.whoi.edu/Healy1001asb/';
+        DC_NAMESPACE = 'http://purl.org/dc/elements/1.1/';
+        ATOM_NAMESPACE = 'http://www.w3.org/2005/Atom';
+    end
+end
+
