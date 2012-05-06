7d88d2fdd521d770c90f8c35a871a74f36e06735
Heidi Sosik <hsosik@whoi.edu>
2012-05-06 00:52:05 +0000

    make weekly bins from yearday matrix
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@488 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 29a0137aa98104a7fb1f613ef172dde194d783c6

---
+++ analyze_by_class/ydmat2weeklymat.m
@@ -0,0 +1,22 @@
+function [  y_wkmat, mdate_wkmat, yd_wk ] = ydmat2weeklymat( y_ydmat, yearlist )
+%function [ y_wkmat, mdate_wkmat, yd_wk ] = ydmat2weeklymat( y_ydmat )
+%accept an input matrix of mean values for each yearday by year (y_ydmat)
+%output a matrix of mean values for each week by year (y_wkmat) 
+%Heidi M. Sosik, Woods Hole Oceanographic Institution, May 2012
+
+numyrs = length(yearlist);
+mdate_year = datenum(yearlist,0,0);
+yd_wk = (1:7:364)';
+mdate_wkmat = repmat(yd_wk,1,numyrs)+repmat(mdate_year,length(yd_wk),1);
+%yearday_all = mdate-datenum(dv(:,1),0,0);
+y_wkmat = NaN(52,numyrs);
+for count = 1:52-1,
+    iii = count*7-6:count*7;
+    y_wkmat(count,:) = nanmean(y_ydmat(iii,:));
+end;
+%include last day (or two if leap year) in final week
+iii = yd_wk(end):366;
+y_wkmat(count+1,:) = nanmean(y_ydmat(iii,:));
+end
+
+
