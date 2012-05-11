690752b59d5f7d4a36ddb3608c34d96e0b75bc02
Heidi Sosik <hsosik@whoi.edu>
2012-05-11 20:52:45 +0000

    revise for pre-computed blobs
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@516 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: d03e81dce98350b8a64e933ef875b8f91273fde0

--- feature_extraction/configure.m
+++ feature_extraction/configure.m
@@ -26,7 +26,7 @@ config.blob_min = 150; % minimum area of blob
 % list of region props to compute for blob; AREA is done no matter what so
 % omit from this list
 % add Area back if blobs are pre-computed
-config.blob_props = {'Area' 'BoundingBox', 'ConvexArea', 'Eccentricity', 'EquivDiameter', 'Extent', 'MajorAxisLength', ...
+config.blob_props = {'Area', 'BoundingBox', 'ConvexArea', 'Eccentricity', 'EquivDiameter', 'Extent', 'MajorAxisLength', ...
     'MinorAxisLength', 'Orientation', 'Perimeter', 'Solidity', 'ConvexHull'};
 config.props2sum = {'Area' 'ConvexArea', 'MajorAxisLength', 'MinorAxisLength', 'Perimeter', 'ConvexPerimeter', 'FeretDiameter'};
 
