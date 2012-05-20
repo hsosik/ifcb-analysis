34477f205010d0ee424a916fb006c5662e1c03f5
Heidi Sosik <hsosik@whoi.edu>
2012-05-20 12:52:51 +0000

    maxfig.m from mathworks fileexchange ydmat2monthlymat.m makes month bins from a day-by-year matrix summary_plots3.m minor updates to plotting
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@537 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 253e85cefd383f9eaade8604ed01153505865a4a

---
+++ analyze_by_class/ydmat2monthlymat.m
@@ -0,0 +1,21 @@
+function [  y_monthmat, mdate_monthmat] = ydmat2monthlymat( y_ydmat, yearlist )
+%function [ y_monthmat, mdate_monthmat ] = ydmat2monthlymat( y_ydmat )
+%accept an input matrix of mean values for each yearday by year (y_ydmat)
+%output a matrix of mean values for each month by year (y_monthmat) 
+%Heidi M. Sosik, Woods Hole Oceanographic Institution, May 2012
+
+numyrs = length(yearlist);
+mdate_year = datenum(yearlist,0,0);
+yd = (1:366)';
+mdate = repmat(yd,1,numyrs)+repmat(mdate_year,length(yd),1);
+[~,month,~,~,~,~] = datevec(mdate);
+mdate_monthmat = datenum(repmat(yearlist,12,1),repmat((1:12)',1,length(yearlist)),1);
+y_monthmat = NaN(12,numyrs);
+for count = 1:12,
+    mask = ones(size(month));
+    mask(month~=count) = NaN;
+    y_monthmat(count,:) = nanmean(y_ydmat.*mask);
+end;
+end
+
+
