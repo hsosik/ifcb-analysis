52a580ea201cd5ca0ecec4ab9695bf344be1c4b9
Heidi Sosik <hsosik@whoi.edu>
2012-05-20 12:44:03 +0000

    moved to classification folder
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@532 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 1212caae25d4c383b0bb4aefe4ef6a1774fcbda3

--- classification/compile_train_features3.m
+++ classification/compile_train_features3.m
@@ -1,5 +1,7 @@
 %load output %load result file produced by batch_features_train.m
 outdir = 'C:\work\IFCB\ifcb_data_MVCO_jun06\train_04Nov2011_fromWebServices\';
+%outdir = 'C:\work\IFCB3LisaCampbell\Training_images_PNG\';
+maxn = 300;
 temp = dir([outdir '*.mat']);
 classes = {temp.name};
 for i = 1:length(classes),
@@ -8,9 +10,7 @@ end;
 
 %classes = fieldnames(output);
 %skip some classes that are empty or not properly annotated
-classes = setdiff(classes, {'config' 'Eucampia_groenlandica' 'Tropidoneis' 'dino10' 'roundCell' 'other' 'flagellate' 'crypto'});
-%classes = setdiff(classes, {'config' 'Eucampia_groenlandica' 'Tropidoneis' 'dino10' 'roundCell' 'other' 'flagellate' 'crypto' 'Lauderia' 'Odontella' 'Stephanopyxis'});
-maxn = 300;
+classes = setdiff(classes, {'Eucampia_groenlandica' 'Tropidoneis' 'dino10' 'roundCell' 'other' 'flagellate' 'crypto' 'ciliate'});
 class_vector = [];
 targets = [];
 for classcount = 1:length(classes),
@@ -40,14 +40,7 @@ end;
 
 train = cell2mat(feamat)';
 
-%featitles = fieldnames(output_largest.(class).features);
-%nring = size(Rings,1); nwedge = size(Wedges,1); nhog = size(HOG,1);
-%featitles = [featitles; cellstr([repmat('Wedge:',nwedge,1) num2str((1:nwedge)')]); cellstr([repmat('Ring:',nring,1) num2str((1:nring)')]); cellstr([repmat('HOG:',nhog,1) num2str((1:nhog)')])];
-%train(isnan(train)) = 0;
-
-%clear temp s_largest classcount i largest_ind field s_temp n temp list fields class ind nhog nring nwedge 
-%clear outdir ans HOG Rings Wedges maxn out n_class t_temp
-%clear output output_largest n_rot fieldstemp
+save compiled_train_mvco train class_vector classes targets featitles
 
 clear t_temp classcount i n maxn temp output outdir out class ind feature_mat
 
