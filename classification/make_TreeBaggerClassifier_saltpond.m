60da4d0dcf5e89084fb0e21aa87e31d583cd4829
Heidi Sosik <hsosik@whoi.edu>
2012-05-20 12:38:00 +0000

    revise to compute optimized score thresholds
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@531 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: d1a686219aa5034c185585923df0b294d33f4965

--- classification/make_TreeBaggerClassifier_saltpond.m
+++ classification/make_TreeBaggerClassifier_saltpond.m
@@ -1,31 +1,38 @@
 %run compile_train_features3.m to generate train and class_vector
-load compiled_train_saltpond
-datestring = datestr(now, 'ddmmmyyyy');
+%load compiled_train_saltpond
+%datestring = datestr(now, 'ddmmmyyyy');
 
 %class_vector = classes(class_vector);
-disp('Growing trees...please be patient')
-b = TreeBagger(50,train,class_vector,'Method','c','OOBVarImp','on','MinLeaf',1);
-figure, hold on
-plot(oobError(b), 'b-');
-xlabel('Number of Grown Trees');
-ylabel('Out-of-Bag Classification Error');
+%disp('Growing trees...please be patient')
+%b = TreeBagger(50,train,class_vector,'Method','c','OOBVarImp','on','MinLeaf',1);
+%figure, hold on
+%plot(oobError(b), 'b-');
+%xlabel('Number of Grown Trees');
+%ylabel('Out-of-Bag Classification Error');
 
 %b = growTrees(b,250); %specify how many to add
 %plot(oobError(b), 'g');
-save(['SaltPond_trees_' datestring],'b', 'targets', 'featitles', 'classes')
-
-return
 
 [Yfit,Sfit] = oobPredict(b);  
 c1 = confusionmat(b.Y,Yfit); %transposed from mine
 
+for count = 1:length(classes),
+ [fpr,accu,thr] = perfcurve(b.Y,Sfit(:,count), num2str(count),'ycrit','accu');
+ [maxaccu(count),iaccu] = max(accu);
+ maxthre(count) = thr(iaccu);
+end;
+clear count fpr tpr thr iaccu accu
+
+%save(['SaltPond_trees_' datestring],'b', 'targets', 'featitles', 'classes', 'maxthre')
+
 total = sum(c1')';
 Pd = diag(c1)./total;
 Sp = 1-(sum(c1)-diag(c1)')./total';
-sum(sum(c1)-diag(c1)')/sum(total)
+disp('overall error rate:')
+disp(sum(sum(c1)-diag(c1)')/sum(total))
 text_offset = .1;
 figure, bar([total diag(c1) sum(c1)'-diag(c1)])
-legend('total in set', 'detected', 'wrong')
+legend('total in set', 'true pos', 'false pos')
 set(gca, 'xtick', 1:length(classes), 'xticklabel', [])
 text(1:length(classes), -text_offset.*ones(size(classes)), classes, 'interpreter', 'none', 'horizontalalignment', 'right', 'rotation', 45) 
 set(gca, 'position', [ 0.13 0.35 0.8 0.6])
@@ -34,31 +41,12 @@ set(gca, 'xtick', 1:length(classes), 'xticklabel', [])
 text(1:length(classes), -text_offset.*ones(size(classes)), classes, 'interpreter', 'none', 'horizontalalignment', 'right', 'rotation', 45) 
 set(gca, 'position', [ 0.13 0.35 0.8 0.6])
 legend('Pd', 'Sp')
-
-figure
-h = histc(Pd, [0:.1:1]); b = 0:10:100;
-bar(fliplr(b)+5,cumsum(flipud(h)/length(Pd)),'width', 1)
-set(gca, 'xtick', 0:10:100, 'xlim', [-5 105], 'xdir', 'rev')
-cumsum(flipud(h./length(Pd)))'
-fliplr(b)
-
-figure, hold on
-for count = 1:length(classes),
- [fpr,tpr,thr] = perfcurve(b.Y,Sfit(:,count), classes{count});
-  subplot(2,1,1), ph = plot(fpr,tpr, 'r');
-  title(classes(count)), xlabel('False pos rate'), ylabel('True pos rate')
-  [fpr,accu,thr] = perfcurve(b.Y,Sfit(:,count), classes{count},'ycrit','accu');
-  subplot(2,1,2), hold off, ph2 = plot(thr,accu, 'g'); 
-  hold on, ph3 = plot(thr, 1-fpr, 'r'); legend('accuracy', '1-false pos rate')
-  set(ph, 'color', 'k'), set(gca, 'ylim', [max(accu)*.99 1])
-  [maxaccu(count),iaccu] = max(accu);
-  maxthre(count) = thr(iaccu);
-  line(maxthre(count)*[1 1], ylim, 'linestyle', ':')
-  xlabel('Threshold for ''good'' Returns');
-  subplot(2,1,1), hold on, plot(fpr(iaccu), tpr(iaccu), 'g*')
-  pause
-end;
-
+figure, boxplot(max(Sfit'),str2num(char(b.Y)))
+ylabel('Out-of-bag winning scores')
+set(gca, 'xtick', 1:length(classes), 'xticklabel', [])
+text(1:length(classes), -text_offset.*ones(size(classes)), classes, 'interpreter', 'none', 'horizontalalignment', 'right', 'rotation', 45) 
+set(gca, 'position', [ 0.13 0.35 0.8 0.6])
+hold on, plot(1:length(classes), maxthre, '*g')
 
 t = repmat(maxthre,length(Yfit),1);
 win = (Sfit > t);
@@ -71,14 +59,43 @@ for count = 1:length(ind),
     [~,Yfit_max(ind(count))] = max(Sfit(ind(count),:));
 end;
 
-ind = find(~isnan(Yfit_max));
-c2 = confusionmat(b.Y(ind),classes(Yfit_max(ind)));
+ind = find(isnan(Yfit_max));
+Yfit_max(ind) = 0; %unclassified
+%c2 = confusionmat(b.Y(ind),classes(Yfit_max(ind)));
+[c2all, gord] = confusionmat(str2num(char(b.Y)),Yfit_max);
+c2 = c2all(2:end,2:end); %skip the unclassified row/col
 total = sum(c2')';
-Pd2 = diag(c2)./total;
-Sp2 = 1-(sum(c2)-diag(c2)')./total';
+Pd2 = diag(c2)./total; %true pos rate among accepted classifications
+Sp2 = 1-(sum(c2)-diag(c2)')./total'; %specificity of accepted classifications
 sum(sum(c2)-diag(c2)')/sum(total)
-Pm2 = (sum(c1')-sum(c2'))./sum(c1');
+Pm2 = (sum(c1')-sum(c2'))./sum(c1'); %miss rate (true pos in unclassified)
 figure, bar([Pd2 Sp2' Pm2'])
+set(gca, 'xtick', 1:length(classes), 'xticklabel', [])
+text(1:length(classes), -text_offset.*ones(size(classes)), classes, 'interpreter', 'none', 'horizontalalignment', 'right', 'rotation', 45) 
+set(gca, 'position', [ 0.13 0.35 0.8 0.6])
+legend('true pos rate', 'Sp', 'Pmissed')
+set(gca, 'position', [ 0.13 0.35 0.8 0.6])
+
+return
+
+figure, hold on
+for count = 1:length(classes),
+ [fpr,tpr,thr] = perfcurve(b.Y,Sfit(:,count), num2str(count));
+  subplot(2,1,1), ph = plot(fpr,tpr, 'r');
+  title(classes(count)), xlabel('False pos rate'), ylabel('True pos rate')
+  [fpr,accu,thr] = perfcurve(b.Y,Sfit(:,count), num2str(count),'ycrit','accu');
+  subplot(2,1,2), hold off, ph2 = plot(thr,accu, 'g'); 
+  hold on, ph3 = plot(thr, 1-fpr, 'r'); legend('accuracy', '1-false pos rate')
+  set(ph, 'color', 'k'), set(gca, 'ylim', [max(accu)*.99 1])
+  [maxaccu(count),iaccu] = max(accu);
+  maxthre(count) = thr(iaccu);
+  line(maxthre(count)*[1 1], ylim, 'linestyle', ':')
+  xlabel('Threshold for ''good'' Returns');
+  subplot(2,1,1), hold on, plot(fpr(iaccu), tpr(iaccu), 'g*')
+  disp([fpr(iaccu), tpr(iaccu), maxthre(count)])
+  pause
+end;
+
 
 %num_correct = length(find(b.Y==Yfit_max))
 %num_nanincorrect = length(find(b.Y~=Yfit_max))
@@ -88,3 +105,9 @@ acc = num_correct./(num_correct+num_wrong)
 unk_rate = num_nan./(num_correct+num_nanincorrect)
 sum(c')-sum(c2') %num unknown per 
 
+figure
+h = histc(Pd, [0:.1:1]); b = 0:10:100;
+bar(fliplr(b)+5,cumsum(flipud(h)/length(Pd)),'width', 1)
+set(gca, 'xtick', 0:10:100, 'xlim', [-5 105], 'xdir', 'rev')
+cumsum(flipud(h./length(Pd)))'
+fliplr(b)
