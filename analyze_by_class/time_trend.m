c6d4279a0d0fbfba57e8188921d692a743a25b83
Heidi Sosik <hsosik@whoi.edu>
2012-05-26 16:14:12 +0000

    function for trend stats on time series
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@549 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 71fa08b02b7c4129ca116d69a74d40b00b40d6b1

---
+++ analyze_by_class/time_trend.m
@@ -0,0 +1,46 @@
+function [] = time_trend(mdate_mat, y_mat, yearlist, labelstr)
+%function [] = time_trend(mdate_mat, y_mat, yearlist)
+
+next_year = yearlist(end)+1;
+[ y_mat, mdate_mat, yd_wk ] = ydmat2weeklymat( y_mat, yearlist );
+[ ymean, ystd ] = smoothed_climatology( y_mat , 3);
+%yanom = y_mat-repmat(ymeansm',1,length(yearlist));
+yanom = y_mat-repmat(ymean',1,length(yearlist));
+
+figure
+set(gcf, 'position', [350 600 720 200])
+plot(mdate_mat(:), y_mat(:), '.'), hold on
+xlim(datenum([yearlist(1) next_year],1,1))
+set(gca, 'xtick', datenum([yearlist next_year],1,1))
+datetick('x', 'keeplimits', 'keepticks')
+[s,b,r,p]=fit_line(mdate_mat(:), y_mat(:));
+fplot(['x*' num2str(s) '+' num2str(b)], xlim,'k')
+title(['r = ' num2str(r(1), '%.2f') '; \tau = '  num2str(r(2), '%.2f') '; y = ' num2str(s) '*x+' num2str(b) '; p = ' num2str(p, '%.3f ')])
+ylabel(labelstr)
+
+figure
+set(gcf, 'position', [350 600 720 200])
+plot(mdate_mat(:), yanom(:), '.'), hold on
+xlim(datenum([yearlist(1) next_year],1,1))
+set(gca, 'xtick', datenum([yearlist next_year],1,1))
+datetick('x', 'keeplimits', 'keepticks')
+[s,b,r,p]=fit_line(mdate_mat(:), yanom(:));
+fplot(['x*' num2str(s) '+' num2str(b)], xlim,'k')
+title(['r = ' num2str(r(1), '%.2f') '; \tau = '  num2str(r(2), '%.2f') '; y = ' num2str(s) '*x+' num2str(b) '; p = ' num2str(p, '%.3f ')])
+line(xlim, [0 0], 'linestyle',  '--')
+ylabel([labelstr ', anom'])
+
+figure
+set(gcf, 'position', [350 600 720 200])
+ipos = find(yanom(:)>0);
+bar(mdate_mat(ipos), yanom(ipos), 'r', 'edgecolor', 'r')
+hold on 
+ineg = find(yanom(:)<0);
+bar(mdate_mat(ineg), yanom(ineg), 'b', 'edgecolor', 'b')
+xlim(datenum([yearlist(1) next_year],1,1))
+set(gca, 'xtick', datenum([yearlist next_year],1,1))
+datetick('x', 'keeplimits', 'keepticks')
+%[s,b,r,p]=fit_line(mdate_mat(:), yanom(:));
+fplot(['x*' num2str(s) '+' num2str(b)], xlim,'k')
+title(['r = ' num2str(r(1), '%.2f') '; \tau = '  num2str(r(2), '%.2f') '; y = ' num2str(s) '*x+' num2str(b) '; p = ' num2str(p, '%.3f ')])
+ylabel([labelstr ', anom'])
