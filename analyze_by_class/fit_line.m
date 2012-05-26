b513f108e7f68f3a3ae0f92176886524e72cf93c
Heidi Sosik <hsosik@whoi.edu>
2012-05-26 16:12:41 +0000

    revise to do Kendall's tau stats and handle row v. column vector
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@546 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 662c521f5a3dbe0ef41d8f8b55e6d42360424cf6

--- analyze_by_class/fit_line.m
+++ analyze_by_class/fit_line.m
@@ -3,20 +3,26 @@ function [slope, b, r, p] = fit_line(x,y,plotflag);
 if ~exist('plotflag', 'var') %default no plot
     plotflag = 0;
 end;
+temp = size(x);
+if temp(2)>temp(1), x = x'; y = y'; end;
 ii = find(~isnan(x) & ~isnan(y));
 %[slope,b,r,sm] = lsqfitgm(x(ii),y(ii)); %model-2
 fit = polyfit(x(ii), y(ii), 1);
 slope = fit(1); b = fit(2);
 r = corrcoef(x(ii),y(ii));
+[tau,p_rho] = corr(x(ii),y(ii), 'type', 'kendall'); %Kendall's rank
 r = r(2);
+keyboard
+r(2) = tau; % same value as returned by corrcoef
 n = length(ii);
 t = abs(r).*sqrt((n-2)./(1-r.^2));
 p = 1-tcdf(t,n-2);
+p(2) = p_rho;
 if plotflag,
     figure
     plot(x,y, '.')
     hold on
     fplot(['x*' num2str(slope) '+' num2str(b)], xlim)
-    title(['r^2 = ' num2str(r^2) '; ' 'y = ' num2str(slope) '*x+' num2str(b) '; p = ' num2str(p)])
+    title(['r = ' num2str(r, '%.2f ') '; ' 'y = ' num2str(slope) '*x+' num2str(b) '; p = ' num2str(p,'%.3f')])
 end;
 end
