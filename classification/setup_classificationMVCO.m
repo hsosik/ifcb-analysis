a843afd75f964be3a16af9346515becb58803613
Heidi Sosik <hsosik@whoi.edu>
2012-06-27 01:47:56 +0000

    revise to loop over whole year
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@578 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 54f9b69c7c6adb302f3d18353d5adca7ebecb0e3

--- classification/setup_classificationMVCO.m
+++ classification/setup_classificationMVCO.m
@@ -1,7 +1,17 @@
-resultpath = '\\raspberry\d_work\IFCB1\ifcb_data_mvco_jun06\Manual_fromClass\';
-filelist = get_filelist_manual([resultpath 'manual_list'],2,[2006:2012], 'all'); %manual_list, column to use, year to find
-feafilestr = '_fea_v1';
-[ filelist, feafiles ] = resolve_MVCOfeafiles( filelist, feafilestr );
+%resultpath = '\\raspberry\d_work\IFCB1\ifcb_data_mvco_jun06\Manual_fromClass\';
+%filelist = get_filelist_manual([resultpath 'manual_list'],2,[2006:2012], 'all'); %manual_list, column to use, year to find
+%feafilestr = '_fea_v1';
+%[ filelist, feafiles ] = resolve_MVCOfeafiles( filelist, feafilestr );
+
+filelist = dir('\\queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\features2006_v1\*.csv');
+filelist = {filelist.name}';
+filesdone = dir('\\queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\class2006_v1\*.mat');
+filesdone = {filesdone.name}';
+f1 = regexprep(filelist, '_fea_v1.csv', '');
+f2 = regexprep(filesdone, '_class_v1.mat', '');
+[~,i1] = setdiff(f1,f2);
+filelist = filelist(i1); clear f1 f2 i1 filesdone
+
 classifierName = 'MVCO_trees_25Jun2012';
 
 header = textread(feafiles{1}, '%s',235, 'delimiter', ',');
@@ -9,12 +19,10 @@ header = textread(feafiles{1}, '%s',235, 'delimiter', ',');
 featitles_file = header(feaorder);
 if true, %isequal(featitles, featitles_file), 
     num2dostr = num2str(length(filelist))
-    tic
     for filecount = 1:length(filelist),
         disp(['classifying ' num2str(filecount) ' of ' num2dostr])
         apply_TBclassifierMVCO(feafiles{filecount}, feaorder, classifierName);
     end;
-    toc
 else, 
     disp('Classification incomplete - feature titles do not match with classifier.')
 end;
