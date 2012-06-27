57fdc05fa4ec4756f50ab3efc52a3f9a07be7ea6
Heidi Sosik <hsosik@whoi.edu>
2012-06-27 01:55:44 +0000

    fix filelist error
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@579 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 85c94b491ab8f75e0c8dd684aba6677fd167eb4f

--- classification/setup_classificationMVCO.m
+++ classification/setup_classificationMVCO.m
@@ -3,26 +3,23 @@
 %feafilestr = '_fea_v1';
 %[ filelist, feafiles ] = resolve_MVCOfeafiles( filelist, feafilestr );
 
-filelist = dir('\\queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\features2006_v1\*.csv');
+feapath = '\\queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\features2006_v1\';
+filelist = dir([feapath '*.csv');
 filelist = {filelist.name}';
 filesdone = dir('\\queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\class2006_v1\*.mat');
 filesdone = {filesdone.name}';
 f1 = regexprep(filelist, '_fea_v1.csv', '');
 f2 = regexprep(filesdone, '_class_v1.mat', '');
 [~,i1] = setdiff(f1,f2);
-filelist = filelist(i1); clear f1 f2 i1 filesdone
+feafiles = filelist(i1); clear f1 f2 i1 filesdone
 
 classifierName = 'MVCO_trees_25Jun2012';
 
-header = textread(feafiles{1}, '%s',235, 'delimiter', ',');
+header = textread([feapath feafiles{1}], '%s',235, 'delimiter', ',');
 [~,feaorder] = setdiff(header, {'FilledArea' 'summedFilledArea' 'Area' 'ConvexArea' 'MajorAxisLength' 'MinorAxisLength' 'Perimeter', 'roi_number'});
 featitles_file = header(feaorder);
-if true, %isequal(featitles, featitles_file), 
-    num2dostr = num2str(length(filelist))
-    for filecount = 1:length(filelist),
-        disp(['classifying ' num2str(filecount) ' of ' num2dostr])
-        apply_TBclassifierMVCO(feafiles{filecount}, feaorder, classifierName);
-    end;
-else, 
-    disp('Classification incomplete - feature titles do not match with classifier.')
+num2dostr = num2str(length(filelist))
+for filecount = 1:length(filelist),
+    disp(['classifying ' num2str(filecount) ' of ' num2dostr])
+    apply_TBclassifierMVCO([feapath feafiles{filecount}], feaorder, classifierName);
 end;
