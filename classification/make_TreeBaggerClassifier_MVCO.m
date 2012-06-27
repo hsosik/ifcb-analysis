f8dcd153353d06bd3e67e4b5686dc9862b9575e6
Heidi Sosik <hsosik@whoi.edu>
2012-06-27 01:18:51 +0000

    update to output results as class strings
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@575 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: d7fe5cba4bdef6dc09748da914d42121de00c914

--- classification/make_TreeBaggerClassifier_MVCO.m
+++ classification/make_TreeBaggerClassifier_MVCO.m
@@ -4,7 +4,13 @@ datestring = datestr(now, 'ddmmmyyyy');
 
 %%class_vector = classes(class_vector);
 disp('Growing trees...please be patient')
-b = TreeBagger(300,train,class_vector,'Method','c','OOBVarImp','on','MinLeaf',1);
+%b = TreeBagger(300,train,class_vector,'Method','c','OOBVarImp','on','MinLeaf',1);
+
+matlabpool
+paroptions = statset('UseParallel','always');
+%tic, b = TreeBagger(300,train,class_vector,'Method','c','OOBVarImp','on','MinLeaf',1,'Options',paroptions); toc
+tic, b = TreeBagger(300,train,classes(class_vector),'Method','c','OOBVarImp','on','MinLeaf',1,'Options',paroptions); toc
+matlabpool close
 figure, hold on
 plot(oobError(b), 'b-');
 xlabel('Number of Grown Trees');
@@ -13,11 +19,15 @@ ylabel('Out-of-Bag Classification Error');
 %b = growTrees(b,250); %specify how many to add
 %plot(oobError(b), 'g');
 
-[Yfit,Sfit] = oobPredict(b);  
+[Yfit,Sfit,Sstdfit] = oobPredict(b);
+[mSfit, ii] = max(Sfit');
+for count = 1:length(mSfit), mSstdfit(count) = Sstdfit(count,ii(count)); t(count)= Sfit(count,ii(count)); end; 
+if isempty(find(mSfit-t)), clear t, else disp('check for error...'); end;
 c1 = confusionmat(b.Y,Yfit); %transposed from mine
 
 for count = 1:length(classes),
- [fpr,accu,thr] = perfcurve(b.Y,Sfit(:,count), num2str(count),'ycrit','accu');
+ %[fpr,accu,thr] = perfcurve(b.Y,Sfit(:,count), num2str(count),'ycrit','accu');
+  [fpr,accu,thr] = perfcurve(b.Y,Sfit(:,count), classes{count},'ycrit','accu');
  [maxaccu(count),iaccu] = max(accu);
  maxthre(count) = thr(iaccu);
 end;
@@ -27,7 +37,7 @@ save(['MVCO_trees_' datestring],'b', 'targets', 'featitles', 'classes', 'maxthre
 
 total = sum(c1')';
 Pd = diag(c1)./total;
-Sp = 1-(sum(c1)-diag(c1)')./total';
+Pr = 1-(sum(c1)-diag(c1)')./total'; %precision
 disp('overall error rate:')
 disp(sum(sum(c1)-diag(c1)')/sum(total))
 text_offset = .1;
@@ -36,11 +46,11 @@ legend('total in set', 'true pos', 'false pos')
 set(gca, 'xtick', 1:length(classes), 'xticklabel', [])
 text(1:length(classes), -text_offset.*ones(size(classes)), classes, 'interpreter', 'none', 'horizontalalignment', 'right', 'rotation', 45) 
 set(gca, 'position', [ 0.13 0.35 0.8 0.6])
-figure, bar([Pd Sp'])
+figure, bar([Pd Pr'])
 set(gca, 'xtick', 1:length(classes), 'xticklabel', [])
 text(1:length(classes), -text_offset.*ones(size(classes)), classes, 'interpreter', 'none', 'horizontalalignment', 'right', 'rotation', 45) 
 set(gca, 'position', [ 0.13 0.35 0.8 0.6])
-legend('Pd', 'Sp')
+legend('Pd', 'Pr')
 figure, boxplot(max(Sfit'),str2num(char(b.Y)))
 ylabel('Out-of-bag winning scores')
 set(gca, 'xtick', 1:length(classes), 'xticklabel', [])
@@ -50,12 +60,12 @@ hold on, plot(1:length(classes), maxthre, '*g')
 
 t = repmat(maxthre,length(Yfit),1);
 win = (Sfit > t);
-[i,j] =find(win);
+[i,j] = find(win);
 Yfit_max = NaN(size(Yfit));
 Yfit_max(i) = j;
 ind = find(sum(win')>1);
 for count = 1:length(ind),
-    ii = find(win(ind(count),:));
+%    ii = find(win(ind(count),:));
     [~,Yfit_max(ind(count))] = max(Sfit(ind(count),:));
 end;
 
@@ -66,7 +76,7 @@ Yfit_max(ind) = 0; %unclassified
 c2 = c2all(2:end,2:end); %skip the unclassified row/col
 total = sum(c2')';
 Pd2 = diag(c2)./total; %true pos rate among accepted classifications
-Sp2 = 1-(sum(c2)-diag(c2)')./total'; %specificity of accepted classifications
+Pr2 = 1-(sum(c2)-diag(c2)')./total'; %precision of accepted classifications
 sum(sum(c2)-diag(c2)')/sum(total)
 Pm2 = (sum(c1')-sum(c2'))./sum(c1'); %miss rate (true pos in unclassified)
 figure, bar([Pd2 Sp2' Pm2'])
@@ -76,6 +86,7 @@ set(gca, 'position', [ 0.13 0.35 0.8 0.6])
 legend('true pos rate', 'Sp', 'Pmissed')
 set(gca, 'position', [ 0.13 0.35 0.8 0.6])
 
+%what difference if add back unclassified according to max score (even if below maxthre
 ind = find(Yfit_max == 0);
 Yfit_max(ind) = str2num(char(Yfit(ind))); %unclassified
 %c2 = confusionmat(b.Y(ind),classes(Yfit_max(ind)));
@@ -83,7 +94,7 @@ Yfit_max(ind) = str2num(char(Yfit(ind))); %unclassified
 %c2 = c2all(2:end,2:end); %skip the unclassified row/col
 total = sum(c3')';
 Pd3 = diag(c3)./total; %true pos rate among accepted classifications
-Sp3 = 1-(sum(c3)-diag(c3)')./total'; %specificity of accepted classifications
+Pr3 = 1-(sum(c3)-diag(c3)')./total'; %specificity of accepted classifications
 sum(sum(c3)-diag(c3)')/sum(total)
 Pm3 = (sum(c1')-sum(c3'))./sum(c1'); %miss rate (true pos in unclassified)
 figure, bar([Pd3 Sp3' Pm3'])
