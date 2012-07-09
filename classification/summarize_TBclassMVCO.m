279f8a3a0f3e136d2cb4f93f92159677e76745b0
Heidi Sosik <hsosik@whoi.edu>
2012-07-09 18:06:06 +0000

    add output roi number list for a specified class
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@586 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: c1811490819f7fc8cc404e7664752a2e24b21bd5

--- classification/summarize_TBclassMVCO.m
+++ classification/summarize_TBclassMVCO.m
@@ -1,13 +1,28 @@
-function [classcount, classcount_above_thresh, class2useTB] = summarize_TBclassMVCO(classfile)
+function [classcount, classcount_above_optthresh, classcount_above_adhocthresh, class2useTB, roiid_list] = summarize_TBclassMVCO(classfile, adhocthresh, class2list)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+
 load(classfile)
 classcount = NaN(length(class2useTB),1);
-classcount_above_thresh = classcount;
+classcount_above_optthresh = classcount;
+
+if exist('adhocthresh', 'var'),
+    classcount_above_adhocthresh = classcount;
+    [maxscore, winclass] = max(TBscores');
+    if length(adhocthresh) == 1, adhocthresh = adhocthresh*ones(size(class2useTB)); end;
+end;
+classcount_weighted = [sum(TBscores,1)'; NaN];
 for ii = 1: length(class2useTB),
-    classcount(ii) = size(strmatch(class2useTB(ii), TBclass),1);
-    classcount_above_thresh(ii) = size(strmatch(class2useTB(ii), TBclass_above_threshold),1);
+    classcount(ii) = size(strmatch(class2useTB(ii), TBclass,'exact'),1);
+    classcount_above_optthresh(ii) = size(strmatch(class2useTB(ii), TBclass_above_threshold),1);
+    if exist('adhocthresh', 'var')
+        ind = (strcmp(class2useTB(ii), TBclass) & maxscore' >= adhocthresh(ii));
+        classcount_above_adhocthresh(ii) = sum(ind);
+        if ii == class2list, 
+            roiid_list = roinum(find(ind));
+        end;
+    end;
 end;
 
 end
