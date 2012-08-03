450e602594b4f26dec475891ffd054524d454b01
Joe Futrelle <joefutrelle@gmail.com>
2012-08-03 17:38:23 +0000

    mosaic endpoint now supports "page" argument.
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@612 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 00c505a62bcfa2b0be80331f7c8e31bdef41a744

--- IFCB_tools/mosaic.py
+++ IFCB_tools/mosaic.py
@@ -23,7 +23,7 @@ ASPECT_RATIO=0.5625
 
 """Create a mosaic of images from the an IFCB bin"""
 
-def __layout(bin, (width, height), sort_key, size=0):
+def __layout(bin, (width, height), size, sort_key, page=0):
     """Fit images into a rectangle using a bin packing algorithm. Returns structures
     describing the layout.
     
@@ -31,19 +31,25 @@ def __layout(bin, (width, height), sort_key, size=0):
     bin - the bin to get the images from (instance of BinFile)
     (width, height) - the pixel dimensions of the desired mosaic
     size - size threshold in pixels^2. images below this area threshold will be ignored"""
-    packer = JimScottRectanglePacker(width, height)
-    """Sort by largest first. This does not generate a "representative" mosaic, but one
-    that shows large and therefore interesting targets in a layout without much wasted space"""
     targets = sorted(bin.all_targets(), key=sort_key)
-    for target in targets:
-        h = target.info[WIDTH] # rotate 90 degrees
-        w = target.info[HEIGHT] # rotate 90 degrees
-        if w * h > size: # above the size threshold?
-            p = packer.TryPack(w, h) # attempt to fit
-            if p is not None:
-                yield {'x':p.x, 'y':p.y, WIDTH:w, HEIGHT:h, PID:target.info[PID], TARGET_NUMBER:target.info[TARGET_NUMBER]}
+    targetpid2layout = {}
+    current_page = 0
+    for current_page in range(0,page+1):
+        packer = JimScottRectanglePacker(width, height)
+        for target in targets:
+            if target.pid not in targetpid2layout:
+                h = target.info[WIDTH] # rotate 90 degrees
+                w = target.info[HEIGHT] # rotate 90 degrees
+                if h * w >= size:
+                    p = packer.TryPack(w,h)
+                    if p is not None:
+                        layout = {'x':p.x, 'y':p.y, WIDTH:w, HEIGHT:h, PID:target.info[PID], TARGET_NUMBER:target.info[TARGET_NUMBER]}
+                        targetpid2layout[target.pid] = { 'page': current_page, 'layout': layout }
+    for entry in targetpid2layout.values():
+        if entry['page'] == page:
+            yield entry['layout']
 
-def layout(bin, (width, height), size=0, sort_key=lambda t: 0 - (t.info[HEIGHT] * t.info[WIDTH])):
+def layout(bin, (width, height), size=0, sort_key=lambda t: 0 - (t.info[HEIGHT] * t.info[WIDTH]), page=0):
     """Fit images into a rectangle using a bin packing algorithm. Returns structures
     describing the layout.
     
@@ -51,11 +57,10 @@ def layout(bin, (width, height), size=0, sort_key=lambda t: 0 - (t.info[HEIGHT] 
     bin - the bin to get the images from (instance of BinFile)
     (width, height) - the pixel dimensions of the desired mosaic
     size - size threshold in pixels^2. images below this area threshold will be ignored"""
-    cache_key = ifcb.lid(bin.pid) + '/mosaic/'+str(width)+'x'+str(height)+'.json' # cache the layout
-    tiles = cache_obj(cache_key, lambda: list(__layout(bin, (width, height), sort_key, size)))
+    tiles = list(__layout(bin, (width, height), size, sort_key, page))
     return {'width':width, 'height':height, 'tiles':tiles}
     
-def mosaic(bin, (width, height), size=0, existing_layout=None):
+def mosaic(bin, (width, height), size=0, existing_layout=None, page=0):
     """Fit images into a rectangle using a bin packing algorithm. Returns an image.
     
     Parameters:
@@ -65,7 +70,7 @@ def mosaic(bin, (width, height), size=0, existing_layout=None):
     mosaic = Image.new('L', (width, height))
     mosaic.paste(160,(0,0,width,height))
     if existing_layout is None:
-        existing_layout = layout(bin, (width, height), size)
+        existing_layout = layout(bin, (width, height), size, page=page)
     for entry in existing_layout['tiles']:
         mosaic.paste(bin.target(entry[TARGET_NUMBER]).image(), (entry['x'], entry['y']))
     return mosaic
@@ -85,7 +90,7 @@ def box(w,aspectratio):
 
 def http(bin, fullarea, size_thresh, wh, out, format):
     try:
-        m = mosaic(bin, fullarea, size_thresh)
+        m = mosaic(bin, fullarea, size=size_thresh, page=page)
         # FIXME should print to out
         for h in ['Content-type: image/'+format,
                   'Cache-control: max-age=%d' % DATA_TTL,
@@ -100,7 +105,7 @@ def http(bin, fullarea, size_thresh, wh, out, format):
         raise IOError
 
 # entry point.
-def doit(pid,size='medium',format='jpg',headers=True,out=sys.stdout,fs_roots=FS_ROOTS):
+def doit(pid,size='medium',format='jpg',headers=True,out=sys.stdout,fs_roots=FS_ROOTS,page=0):
     format = dict(png='png', jpg='jpeg', gif='gif', json='json')[format] # validate
     # sizes map to widths, widths map to height via fixed 16:9 aspect ratio
     tw = {'icon':48, 'thumb':128, 'small':320, 'medium':800, 'large':1024, '720p':1280, '1080p':1920, 'max':2400}[size]
@@ -116,13 +121,13 @@ def doit(pid,size='medium',format='jpg',headers=True,out=sys.stdout,fs_roots=FS_
                           'Cache-control: max-age=31622400',
                           '']:
                     print h
-            json.dump(layout(bin, fullarea, size_thresh),out) # give it to em
+            json.dump(layout(bin, fullarea, size_thresh, page=page),out) # give it to em
         else:
             if headers:
                 cache_key = ifcb.lid(pid) + '/mosaic/'+str(tw)+'.'+format
                 cache_io(cache_key, lambda o: http(bin, fullarea, size_thresh, wh, o, format), out)
             else:
-                stream(thumbnail(mosaic(bin,fullarea,size_thresh),wh),out,format)
+                stream(thumbnail(mosaic(bin,fullarea,size_thresh,page=page),wh),out,format)
     except KeyError:
         print 'Status: 404\n'
 
@@ -137,7 +142,9 @@ def save_mosaic(bin,outfile=None,format='PNG',size=2400):
        stream(m,sys.stdout,format)
 
 if __name__=='__main__':
+    #doit('http://ifcb-data.whoi.edu/test/IFCB5_2012_212_204100','medium','json',0)
     pid = cgi.FieldStorage().getvalue('pid')
     size = cgi.FieldStorage().getvalue('size','medium')
+    page = int(cgi.FieldStorage().getvalue('page','0'))
     format = cgi.FieldStorage().getvalue('format','jpg')
-    doit(pid,size,format)
+    doit(pid,size,format,page=page)
