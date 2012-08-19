37d1fbaae1b9f4dc750df998d95f0e8512c8e623
Heidi Sosik <hsosik@whoi.edu>
2012-08-19 20:47:03 +0000

    forget one more for start of TAMU batch feature extraction
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@628 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: f343566da7174a4a1202e87e698f23668a01d02f

---
+++ feature_extraction/batch_features_bins/get_filelist.m
@@ -0,0 +1,12 @@
+function [ filelist ] = get_filelist( in_url, start_day, end_day )
+%function [ filelist ] = get_filelist( in_url, start_day, end_day) )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+filelist = [];
+start_day = datenum(start_day);
+end_day = datenum(end_day);
+for daycount = start_day:end_day,
+    filelist = [filelist list_day(datestr(daycount,29), in_url)];
+end
+end
