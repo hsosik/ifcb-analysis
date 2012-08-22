3bd1b5e07731f11bcaf8d262c5b55b430b6b72c4
Heidi Sosik <hsosik@whoi.edu>
2012-08-22 01:34:00 +0000

    add paths for linux
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@644 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 01397838f715c0d605fdb4a71c66564dc02805ca

--- feature_extraction/batch_features_bins/start_batch_features_saltpond.m
+++ feature_extraction/batch_features_bins/start_batch_features_saltpond.m
@@ -1,6 +1,9 @@
 %start_batch_features_tamu.m
 %configure and initiate batch processing for feature extractiom
 %NOTE: This is currently set for rotated camera. 
+addpath('/home/hsosik/ifcbcode/webservice_tools/'); 
+addpath('/home/hsosik/ifcbcode/feature_extraction/');
+addpath('/home/hsosik/ifcbcode/dipum_toolbox_2.0.1/');
 
 in_url = 'http://ifcb-data.whoi.edu/saltpond/'; %USER web services to access data
 out_base_dir = '//mnt/queenrose/ifcb_data_mvco_jun06/saltpond_features2012/'; %USER main blob output location
