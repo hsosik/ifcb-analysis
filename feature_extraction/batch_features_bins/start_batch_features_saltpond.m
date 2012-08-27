99e93d9e8f1c20a63e6023ec464724cb0127c3f0
Heidi Sosik <hsosik@whoi.edu>
2012-08-27 13:40:40 +0000

    correct errors in dir to locate files_done
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@649 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 3902b409a30e7221ff72867439f5c680aa7495bf

--- feature_extraction/batch_features_bins/start_batch_features_saltpond.m
+++ feature_extraction/batch_features_bins/start_batch_features_saltpond.m
@@ -1,12 +1,13 @@
 %start_batch_features_tamu.m
 %configure and initiate batch processing for feature extractiom
 %NOTE: This is currently set for rotated camera. 
-addpath('/home/hsosik/ifcbcode/webservice_tools/'); 
-addpath('/home/hsosik/ifcbcode/feature_extraction/');
-addpath('/home/hsosik/ifcbcode/dipum_toolbox_2.0.1/');
+%addpath('/home/hsosik/ifcbcode/webservice_tools/'); 
+%addpath('/home/hsosik/ifcbcode/feature_extraction/');
+%addpath('/home/hsosik/ifcbcode/dipum_toolbox_2.0.1/');
 
 in_url = 'http://ifcb-data.whoi.edu/saltpond/'; %USER web services to access data
-out_base_dir = '//mnt/queenrose/ifcb_data_mvco_jun06/saltpond_features2012/'; %USER main blob output location
+%out_base_dir = '//mnt/queenrose/ifcb_data_mvco_jun06/saltpond_features2012/'; %USER main blob output location
+out_base_dir = '//queenrose/ifcb_data_mvco_jun06/saltpond_features/'; %USER main blob output location
 year = 2012; %USER
 %USER choose start and end day to encompass range to process; already
 %completed or non-existent days will be skipped automatically
@@ -21,7 +22,7 @@ end;
 
 filelist = get_filelist(in_url, start_day, end_day);
 
-files_done = dir([out_dir 'D*.mat']);
+files_done = dir([out_dir 'D*.csv']);
 files_done = char(files_done.name);
 files_done = cellstr(files_done(:,1:end-4));
 filelist2 = setdiff(filelist, files_done); 
