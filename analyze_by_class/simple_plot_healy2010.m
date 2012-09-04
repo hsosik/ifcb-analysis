20fbdc65ec63b188f83abab6fe3d6648480aa9d9
Heidi Sosik <hsosik@whoi.edu>
2012-09-04 12:42:55 +0000

    add more plots
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@664 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: bad9a514e9f05670e97314785381fecbc1d73307

--- analyze_by_class/simple_plot_healy2010.m
+++ analyze_by_class/simple_plot_healy2010.m
@@ -22,3 +22,24 @@ for ii = 1:length(class2use),
     xlabel('Longitude')
     pause
 end;
+
+
+load([resultpath 'count_biovol_manual_12Jun2012']);
+[ lat, lon ] = get_healy2010_latlon( matdate );
+
+diatom_class = {'Asterionellopsis' 'Cerataulina' 'Chaetoceros' 'Corethron' 'Coscinodiscus' 'Cylindrotheca' 'DactFragCerataul' 'Dactyliosolen' ...
+    'Ditylum' 'Ephemera' 'Eucampia' 'Guinardia' 'Guinardia_flaccida' 'Guinardia_striata' 'Lauderia' 'Leptocylindrus' 'Licmophora' 'Odontella' ...
+    'Paralia' 'pennate' 'Pleurosigma' 'Pseudonitzschia' 'Rhizosolenia' 'Skeletonema' 'Stephanopyxis' 'Thalassionema' 'Thalassiosira' 'Fragilariopsis' };
+
+[~,diatom_ind] = intersect(class2use, diatom_class);
+
+dino_class = {'Ceratium' 'dino10' 'dino30' 'Dinophysis' 'Gonyaulax' 'Gyrodinium' 'Prorocentrum'};
+[~,dino_ind] = intersect(class2use, dino_class);
+
+mix_class = {'roundCell' 'clusterflagellate' 'flagellate' 'kiteflagellates' 'mix_elongated'};
+
+nonphyto_class = {'bad' 'ciliate' 'detritus' 'ciliate_mix' 'tintinnid' 'Myrionecta' 'Laboea'};
+[~,phyto_ind] = setdiff(class2use, nonphyto_class);
+diatom_biovol = sum(classbiovol(:,diatom_ind),2);
+dino_biovol = sum(classbiovol(:,dino_ind),2);
+phyto_biovol = sum(classbiovol(:,phyto_ind),2);
