c592cf20ab47fdd32df5dee53104a29d4019182a
Heidi Sosik <hsosik@whoi.edu>
2012-09-06 20:06:41 +0000

    revise to do both IFCB1 and IFCB5 files in same run
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@671 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 50d3ae7afe7395e7f708503dd531cab525e842e1

--- feature_extraction/biovolume/start_year.m
+++ feature_extraction/biovolume/start_year.m
@@ -1,68 +1,38 @@
 %basedir = '\\demi\ifcbold\G\IFCB\ifcb_data_MVCO_jun06\';  %%USER set, roi files, adc files
 basedir = '\\demi\ifcbnew\';  %%USER set, roi files, adc files
-daylist = dir([basedir 'IFCB5_2011_350*']); 
-%daylist = dir([basedir 'IFCB*']); 
+basedir2 = '\\demi\blobs\2012\';
+daystr = '2012_';
+daylist = dir([basedir2 daystr '*']); 
 daylist = daylist([daylist.isdir]);
-t = char(daylist.name); ind = find(t(:,10) ~= '2'); %skip 2012
-daylist = daylist(ind); clear t ind
-t = char(daylist.name); ind = strmatch('2010_222', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_082', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_091', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_167', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_168', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_169', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_093', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_283', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_153', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_152', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_322', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2011_298', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2011_215', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_094', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2010_318', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-t = char(daylist.name); ind = strmatch('2011_315', t(:,7:14)); %skip a day %2010_082?, 2010_222
-daylist(ind) = []; clear t ind
-
 filelist = [];
-num2do = 18;%30
+num2do = 300;%30
 for count = 1:length(daylist),
     disp(daylist(count).name)
-    filestemp = dir([basedir daylist(count).name '\IFCB*.roi']);
+    filestemp = [dir([basedir 'IFCB1_' daylist(count).name '\IFCB*.roi']); dir([basedir 'IFCB5_' daylist(count).name '\IFCB*.roi'])];
     filestemp = filestemp([filestemp.bytes] > 2);
     filelist = [filelist; filestemp(1:min([length(filestemp),num2do]))];
 end;
 filelist = {filelist.name}';
 filelist = regexprep(filelist,'.roi','');
 
-out_dir = '\\Queenrose\ifcb12\ifcb_data_mvco_jun06\biovolume\'; %G-drive share
-out_dir_all = {'\\Queenrose\ifcb12\ifcb_data_mvco_jun06\biovolume\biovolume2006\'...
-    '\\Queenrose\ifcb12\ifcb_data_mvco_jun06\biovolume\biovolume2007\'...
-    '\\Queenrose\ifcb12\ifcb_data_mvco_jun06\biovolume\biovolume2008\'...
-    '\\Queenrose\ifcb12\ifcb_data_mvco_jun06\biovolume\biovolume2009\'...
-    '\\Queenrose\ifcb12\ifcb_data_mvco_jun06\biovolume\biovolume2010\'...
-    '\\Queenrose\ifcb12\ifcb_data_mvco_jun06\biovolume\biovolume2011\'};
+%in_dir = ['\\demi\blobs\2012\' daystr '\'];
+out_dir = '\\Queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\biovolume\biovolume2012\'; %G-drive share
+%out_dir = '/mnt/queenrose/ifcb_data_mvco_jun06/biovolume/biovolume2012/'; %G-drive share
+
+%out_dir_all = {'\\Queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\biovolume\biovolume2006\'...
+%    '\\Queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\biovolume\biovolume2007\'...
+%    '\\Queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\biovolume\biovolume2008\'...
+%    '\\Queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\biovolume\biovolume2009\'...
+%    '\\Queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\biovolume\biovolume2010\'...
+%    '\\Queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\biovolume\biovolume2011\'...
+%    '\\Queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\biovolume\biovolume2012\'};
 in_dir = 'http://ifcb-data.whoi.edu/';
-%files_done = dir([out_dir 'IFCB*.mat']);
+
 files_done = dir([out_dir 'IFCB*.mat']);
-for count = 1:length(out_dir_all),
-    temp = dir([out_dir_all{count} 'IFCB*.mat']);
-    files_done = [files_done; temp];
-end;
+%for count = 1:length(out_dir_all),
+%    temp = dir([out_dir_all{count} 'IFCB*.mat']);
+%    files_done = [files_done; temp];
+%end;
 files_done = char(files_done.name);
 files_done = cellstr(files_done(:,1:end-4));
 filelist2 = setdiff(filelist, files_done); %1:4009 before 2010
