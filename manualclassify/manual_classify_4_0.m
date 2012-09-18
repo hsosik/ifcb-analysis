62614b691c1b6a17b4d8bdf0973e7cdf5cef95c1
Heidi Sosik <hsosik@whoi.edu>
2012-09-18 14:55:42 +0000

    comment out subdivide ciliates (not needed for Healy case, etc.)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@682 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 2aad09293c6e4c7e3d38d70b8671bc7299d729a4

--- manualclassify/manual_classify_4_0.m
+++ manualclassify/manual_classify_4_0.m
@@ -64,10 +64,10 @@ switch pick_mode
         classstr = 'ciliate'; %USER class to start from
         class2use_sub = [];  %use this if no subdividing 
         %new subclasses, first one for rois NOT in the class
-        class2use_sub = {'not_ciliate' 'ciliate_mix' 'tintinnid' 'Myrionecta' 'Laboea'}; %USER type or load list
-        class2use_sub = {'not_ciliate' 'ciliate_mix' 'tintinnid' 'Myrionecta' 'Laboea' 'S_conicum' 'tiarina' 'strombidium_1'...
-            'S_caudatum', 'Strobilidium_1' 'Tontonia' 'strombidium_2' 'S_wulffi' 'S_inclinatum' 'Euplotes' 'Didinium'...
-            'Leegaardiella' 'Sol' 'strawberry' 'S_capitatum'}; %USER type or load list
+        %class2use_sub = {'not_ciliate' 'ciliate_mix' 'tintinnid' 'Myrionecta' 'Laboea'}; %USER type or load list
+        %class2use_sub = {'not_ciliate' 'ciliate_mix' 'tintinnid' 'Myrionecta' 'Laboea' 'S_conicum' 'tiarina' 'strombidium_1'...
+        %    'S_caudatum', 'Strobilidium_1' 'Tontonia' 'strombidium_2' 'S_wulffi' 'S_inclinatum' 'Euplotes' 'Didinium'...
+        %    'Leegaardiella' 'Sol' 'strawberry' 'S_capitatum'}; %USER type or load list
         classnum_default_sub = strmatch('ciliate_mix', class2use_sub); %USER class for default
         class2use_pick2 = class2use_sub; %to set button labels
         [~,class2view1] = intersect(class2use, MCconfig.class2view1); %1:length(class2use);
