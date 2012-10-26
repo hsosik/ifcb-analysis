abdc1cacac26d91c60bf10163f2eaa1e86dcbb62
Heidi Sosik <hsosik@whoi.edu>
2012-10-26 14:51:14 +0000

    add example figures
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@696 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: f09b9f515a8bc9aac9766ff25a332ec1561e809f

--- classification/compile_summaries.m
+++ classification/compile_summaries.m
@@ -21,24 +21,35 @@ return
 
 
 load \\raspberry\d_work\IFCB1\ifcb_data_mvco_jun06\Manual_fromClass\summary\count_biovol_manual_13Sep2012_day
-figure, set(gcf, 'paperposition', [.25 2.5 8 2.5], 'units', 'inches')
-set(gcf, 'position', [5 4 8 2.5])
+%figure, set(gcf, 'paperposition', [.25 2.5 8 2.5], 'units', 'inches')
+%set(gcf, 'position', [5 4 8 2.5])
+figure, set(gcf, 'paperposition', [.25 2.5 11.5 2.45], 'units', 'inches')
+set(gcf, 'position', [5 4 11.5 2.45])
 
-ind = 13;
-[xmat, ymat ] = timeseries2ydmat(mdateTBall, classcountTB_above_adhocthreshall(:,ind)./ml_analyzedTBall);
-hold on, plot(xmat(:), ymat(:), 'linewidth', 1.5)
-plot(matdate_bin, classcount_bin(:,10)./ml_analyzed_mat_bin(:,10), 'r.')
+%ind = strmatch('Ditylum', class2useTB);
+ind = strmatch('Guinardia', class2useTB, 'exact');
+%[xmat, ymat ] = timeseries2ydmat(mdateTBall, classcountTB_above_adhocthreshall(:,ind)./ml_analyzedTBall);
+[xmat, ymat ] = timeseries2ydmat(mdateTBall, classcountTBall(:,ind)./ml_analyzedTBall);
+hold on, 
+%ind2 = strmatch('Ditylum', class2use);
+ind2 = strmatch('Guinardia', class2use, 'exact');
+plot(matdate_bin, classcount_bin(:,ind2)./ml_analyzed_mat_bin(:,ind2), 'r*')
+plot(xmat(:), ymat(:), 'linewidth', 1.5)
 %text(datenum(2007,1,1), 10, ['\it' class2useTB(ind)])
 datetick, set(gca, 'xgrid', 'on', 'box', 'on')
-ylim([0 12])
-legend('automated', 'manual')
-ylabel('Cells mL^{-1}')
+%ylim([0 15])
+ylim([0 80])
+legend('manual', 'automated')
+%ylabel('Chains mL^{-1}', 'fontsize', 18)
+ylabel('Cells mL^{-1}', 'fontsize', 12)
+set(gca, 'fontsize', 12)
 
-img = imread('http://ifcb-data.whoi.edu/mvco/IFCB5_2011_017_134444_01637.png');
+%img = imread('http://ifcb-data.whoi.edu/mvco/IFCB5_2011_017_134444_01637.png'); %Ditylum
+img = imread('http://ifcb-data.whoi.edu/mvco/IFCB5_2012_030_223923_03472.png'); %Guinardia
 t = size(img,1)/1000*1.0; s = size(img,2)/1000*1.0;
 i3 = axes('position', [-.05 .65 s t]);
 imshow(img)
-title('\itDitylum brightwellii')
-
-print Ditylum_timeseries -depsc
-print Ditylum_timeseries -dpng
+%title('\itDitylum brightwellii', 'fontsize', 12)
+title('\itGuinardia delicatula', 'fontsize', 12)
+%print Ditylum_timeseries -depsc
+%print Ditylum_timeseries -dpng
