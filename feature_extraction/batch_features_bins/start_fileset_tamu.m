c6f558ed6d4575eece4c14b96cc48a5841ed5335
Heidi Sosik <hsosik@whoi.edu>
2013-01-21 18:24:28 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@724 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: d35227fd129d2d201e5c2bd036e6ab06d19ff183

--- feature_extraction/batch_features_bins/start_fileset_tamu.m
+++ feature_extraction/batch_features_bins/start_fileset_tamu.m
@@ -1,11 +1,12 @@
-out_dir = '\\queenrose\g_work_ifcb1\tamu\features2010_v1\';
+out_dir = 'C:\work\IFCB2_COAST\temp\features2011_v1\';
 in_dir = 'http://toast.tamu.edu/ifcb3/'; %USER web services to access data
 filelist = [];
-for day = 1:2,
-    filelist = [filelist list_day(datestr(datenum(2010,0,day),29), in_dir)];
+for day = 238:238,
+    filelist = [filelist list_day(datestr(datenum(2011,0,day),29), in_dir)];
 end;
 disp(filelist(end))
-filelist = regexprep(filelist, in_dir, '')';
+%filelist = regexprep(filelist, in_dir, '')';
+filelist = regexprep(filelist, 'http://toast.tamu.edu/ifcb7/', '')';
 files_done = dir([out_dir 'IFCB*fea_v1.csv']);
 files_done = char(files_done.name);
 files_done = cellstr(files_done(:,1:end-11));
