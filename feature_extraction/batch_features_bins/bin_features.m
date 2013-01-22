34b764fb06302b09fac471773c6d6825c2e28dbb
Heidi Sosik <hsosik@whoi.edu>
2013-01-22 20:26:53 +0000

    Revise bin_features.m, biovolume.m, and configure.m to incorporate biovolume computations along with other features
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@727 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 04ba8972a6591c95d02b8d659e1349e0241551b9

--- feature_extraction/batch_features_bins/bin_features.m
+++ feature_extraction/batch_features_bins/bin_features.m
@@ -2,8 +2,10 @@ function [ ] = bin_features( in_dir, file, out_dir )
 %function [ ] = bin_features( in_dir, file, out_dir )
 %BIN_FEATURES Summary of this function goes here
 %modified from bin_blobs
+%Modified to incorporate biovolume computations (taken from bin_volume.m), Jan 2013
+%Heidi M. Sosik, Woods Hole Oceanographic Institution
 
-debug = true;
+debug = false;
 
 function log(msg) % not to be confused with logarithm function
     logmsg(['bin_features ' msg],debug);
@@ -30,22 +32,18 @@ for i = 1:nt,
     % get the image
     target.image = cell2mat(targets.image(i));
     target.blob_image = cell2mat(targets_blob.image(i));
-    %target = blob(target);
-    %target = apply_blob_min( target ); %get rid of blobs < blob_min
     target = blob_geomprop(target); 
     target = blob_rotate(target);
     target = blob_texture(target);
     target = blob_invmoments(target);
     target = blob_shapehist_stats(target);
     target = blob_RingWedge(target);
+    target = biovolume(target);
     target = blob_sumprops(target);
     target = blob_Hausdorff_symmetry(target);
     target = image_HOG(target);
     target = blob_rotated_geomprop(target);
     temp.features(i) = merge_structs(target.blob_props, target.image_props);
-%        temp.images(i).blob_image = target.blob_image;
-%        temp.images(i).blob_image_rotated = target.blob_image_rotated;
-%        temp.images(i).image = target.image;
 end
 
 if nt > 0,
@@ -53,14 +51,14 @@ if nt > 0,
     [ feature_mat, featitles, multiblob_features, multiblob_titles ] = make_feature_matrices(temp);
     
     %write the compiled feature csv file
-    fileout = regexprep(file, '.zip', '_fea_v1.csv');
+    fileout = regexprep(file, '.zip', '_fea_v2.csv');
     log(['SAVING ' fileout]);
    
     ds = dataset([feature_mat featitles]);
     export(ds, 'file', [out_dir fileout], 'delimiter', ',');
     
     %write the raw multi-blob features to separate csv file
-    fileout = regexprep(file, '.zip', '_multiblob_v1.csv');   
+    fileout = regexprep(file, '.zip', '_multiblob_v2.csv');   
     if ~isempty(multiblob_features), 
         ds = dataset([multiblob_features multiblob_titles]);
         export(ds, 'file', [out_dir 'multiblob' filesep fileout], 'delimiter', ',');
--- feature_extraction/biovolume/biovolume.m
+++ feature_extraction/biovolume/biovolume.m
@@ -1,40 +1,34 @@
 function [ target ] = biovolume( target )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+% Modify from biovolume.m to compute along with other features
+% Heidi M. Sosik, Woods Hole Oceanographic Institution
 
 volume = NaN;
-eqvdiam = NaN;
-majoraxis = NaN;
-minoraxis = NaN;
-t = regionprops(target.blob_image, 'Area', 'Orientation','ConvexArea','Eccentricity', 'EquivDiameter','MajorAxisLength', 'MinorAxisLength');
-if length(t) > 1,
+t = target.blob_props;
+
+if length(t.Area) > 1,
     bwl = bwlabel(target.blob_image);
 else
     bwl = target.blob_image;
 end;
+
 area_ratio = [t.ConvexArea]./[t.Area];
 p = [t.EquivDiameter]./[t.MajorAxisLength];
-for ii = 1:length(t),
+for ii = 1:length(t.Area),
     blob_now = bwl; blob_now(blob_now ~= ii) = 0;
     blob_now = logical(blob_now); %needed for cases with bwlabel applied above    
-    eqvdiam(ii) = t(ii).EquivDiameter;
-    majoraxis = t(ii).MajorAxisLength;
-    minoraxis = t(ii).MinorAxisLength;
-    if area_ratio(ii) < 1.2 || (t(ii).Eccentricity < 0.8 && p(ii) > 0.8), %solid of revolution cases
-        theta = -1*t(ii).Orientation;
+    if area_ratio(ii) < 1.2 || (t.Eccentricity(ii) < 0.8 && p(ii) > 0.8), %solid of revolution cases
+        theta = -1*t.Orientation(ii);
         blob_rot = imrotate(blob_now, theta, 'bilinear'); % rotates the filled image
         volume(ii) = volumewalk(blob_rot);
     else %distance map cases
        b =  bwboundaries(blob_now,8,'noholes');
        [M N] = size(blob_now);
        perim_img = bound2im(b{1},M,N);
-       %volume(ii) = rep_dist(perim_img);
        volume(ii) = distmap_volume(perim_img);
     end;
 end;
-target.Biovolume = volume;
-target.EquivDiameter = eqvdiam;
-target.MajorAxisLength = majoraxis;
-target.MinorAxisLength = minoraxis;
+target.blob_props.Biovolume = volume;
 end
 
