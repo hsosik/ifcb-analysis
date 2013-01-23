f130dc557cd2b5c387cbe283ad14f92f9137066f
Heidi Sosik <hsosik@whoi.edu>
2013-01-23 16:37:51 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@738 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 2250da1f809fadace95e937ead42c7a39650e995

--- feature_extraction/modify_adc/batch_tamu.m
+++ feature_extraction/modify_adc/batch_tamu.m
@@ -1,3 +1,5 @@
+addpath('/home/hsosik/ifcbcode/webservice_tools/');
+addpath('/home/hsosik/ifcbcode/feature_extraction/');
 yr = 2007;
 %adcmodbase = 'C:\work\IFCB3LisaCampbell\data\adcmod\';
 adcmodbase = '/scratch5/ifcb/tamu/adcmod/';
@@ -5,14 +7,15 @@ if ~exist(adcmodbase, 'dir'),
     mkdir(adcmodbase)
 end;
 in_dir = 'http://toast.tamu.edu/ifcb3/'; %USER web services to access data
+matlabpool
 for day = 254:366,
     filelist = list_day(datestr(datenum(yr,0,day),29), in_dir);
     disp(['processing ' num2str(length(filelist)) ' files'])
     if ~isempty(filelist),
-        for ii = 1:length(filelist),
+        parfor ii = 1:length(filelist),
             [~,filename] = fileparts(filelist{ii});
             disp(filename)
-            adcmodpath = [adcmodbase filename(1:14), '\'];
+            adcmodpath = [adcmodbase filename(1:14), filesep];
             adcmodfilename = [adcmodpath filename 'adc.mod'];
             if ~exist(adcmodfilename, 'file'),
                 [filestr1,status] = urlwrite([filelist{ii} '.adc'], [adcmodbase filename '.adc']);
@@ -39,3 +42,4 @@ for day = 254:366,
         end;
     end;
 end;
+matlabpool close
