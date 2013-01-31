1feac0df78f87a7a104947842620c28a076def2b
Heidi Sosik <hsosik@whoi.edu>
2013-01-31 18:53:17 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@752 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 854408022665f925d87ac36f4530055d3eb435e6

--- analyze_by_class/biovolume_summary_MVCO_manual.m
+++ analyze_by_class/biovolume_summary_MVCO_manual.m
@@ -53,55 +53,7 @@ classcarbon = classcount;
 ml_analyzed_mat = classcount;
 for loopcount = 1:length(mode_list),
     annotate_mode = char(mode_list(loopcount));
-    switch annotate_mode
-        case 'all categories'
-            %use them all
-            %class_cat = 1:numclass;
-            [~, class_cat] = setdiff(class2use_here, {'diatom_flagellate' 'other_interaction'});
-            manual_only = 0;
-            list_col = strmatch(annotate_mode, manual_list(1,:));
-            mode_ind = find(cell2mat(manual_list(2:end,list_col)));
-        case 'ciliates'
-            [~, class_cat] = intersect(class2use_here, ['ciliate' class2use_first_sub]);
-            manual_only = 0;
-            list_col = strmatch(annotate_mode, manual_list(1,:));
-            mode_ind = find(cell2mat(manual_list(2:end,list_col)) & ~cell2mat(manual_list(2:end,2)));
-        case 'ditylum'
-            [~, class_cat] = intersect(class2use_here, 'Ditylum');
-            manual_only = 0;
-            list_col = strmatch(annotate_mode, manual_list(1,:));
-            mode_ind = find(cell2mat(manual_list(2:end,list_col)) & ~cell2mat(manual_list(2:end,2)) & ~cell2mat(manual_list(2:end,strmatch('diatoms', mode_list)+1)));
-        case 'diatoms'
-            %all except mix, mix_elongated, and detritus
-            [~, class_cat] = setdiff(class2use_here, {'mix' 'detritus'});
-            manual_only = 0;
-            list_col = strmatch(annotate_mode, manual_list(1,:));
-            mode_ind = find(cell2mat(manual_list(2:end,list_col)) & ~cell2mat(manual_list(2:end,2)));
-        case 'big ciliates'
-            [~, class_cat] = intersect(class2use_here, {'tintinnid' 'Laboea'});
-            manual_only = 0;
-            list_col = strmatch(annotate_mode, manual_list(1,:));
-            mode_ind = find(cell2mat(manual_list(2:end,list_col)) & ~cell2mat(manual_list(2:end,2)) & ~cell2mat(manual_list(2:end,strmatch('ciliates', mode_list)+1)));
-        case 'special big only'
-            [~, class_cat] = intersect(class2use_here, {'Ceratium' 'Eucampia' 'Ephemera' 'bad' 'Dinophysis' 'Lauderia' 'Licmophora' 'Phaeocystis' 'Stephanopyxis' ...
-                'Coscinodiscus' 'Odontella' 'Guinardia_striata' 'tintinnid' 'Laboea' 'Hemiaulus' 'Paralia' 'Guinardia_flaccida' 'Corethron' 'Dactyliosolen' 'Dictyocha'...
-                'Dinobryon' 'Ditylum' 'Pleurosigma' 'Prorocentrum' 'Rhizosolenia' 'Thalassionema' 'clusterflagellate' 'kiteflagellates' 'Pyramimonas'});
-            manual_only = 1;
-            list_col = strmatch(annotate_mode, manual_list(1,:));
-            mode_ind = find(cell2mat(manual_list(2:end,list_col)) & ~cell2mat(manual_list(2:end,2)));
-        case 'ciliate_ditylum'
-            [~, class_cat] = intersect(class2use_here, ['Ditylum' 'ciliate' class2use_first_sub]);
-            manual_only = 0;
-            %list_col = strmatch(annotate_mode, manual_list(1,:));
-            mode_ind = find(~cell2mat(manual_list(2:end,2)) & cell2mat(manual_list(2:end,3)) & cell2mat(manual_list(2:end,4)) & ~cell2mat(manual_list(2:end,5)) & cell2mat(manual_list(2:end,6)));
-        case 'parasites'
-            [~, class_cat] = intersect(class2use_here, {'Chaetoceros_flagellate' 'Chaetoceros_pennate' 'Cerataulina_flagellate' 'G_delicatula_parasite' ...
-                'G_delicatula_external_parasite' 'Chaetoceros_other' 'diatom_flagellate' 'other_interaction' 'Chaetoceros_didymus_flagellate'});
-            manual_only = 0;
-            list_col = strmatch(annotate_mode, manual_list(1,:));
-            mode_ind = find(cell2mat(manual_list(2:end,list_col)) & ~cell2mat(manual_list(2:end,2)));
-    end;
-
+    [ class_cat, list_col, mode_ind, manual_only ] = config_annotate_mode( annotate_mode, class2use_here, class2use_first_sub, manual_list, mode_list );
     filelist = cell2struct(manual_list(mode_ind+1,1),{'name'},2);
     
     for filecount = 1:length(filelist),
