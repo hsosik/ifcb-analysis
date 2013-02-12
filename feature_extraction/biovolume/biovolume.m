b122292fd7f02a102e855ba27c56393ffde5aa8c
Heidi Sosik <hsosik@whoi.edu>
2013-02-12 14:57:02 +0000

    revise to handle cases with no blob above threshold in new v2 feature system
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@759 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 39d0fb2db047a907419464e0331a0038ef6bcea2

--- feature_extraction/biovolume/biovolume.m
+++ feature_extraction/biovolume/biovolume.m
@@ -16,17 +16,20 @@ end;
 area_ratio = [t.ConvexArea]./[t.Area];
 p = [t.EquivDiameter]./[t.MajorAxisLength];
 for ii = 1:length(t.Area),
-    blob_now = bwl; blob_now(blob_now ~= ii) = 0;
-    blob_now = logical(blob_now); %needed for cases with bwlabel applied above    
-    if area_ratio(ii) < 1.2 || (t.Eccentricity(ii) < 0.8 && p(ii) > 0.8), %solid of revolution cases
-        theta = -1*t.Orientation(ii);
-        blob_rot = imrotate(blob_now, theta, 'bilinear'); % rotates the filled image
-        volume(ii) = volumewalk(blob_rot);
-    else %distance map cases
-       b =  bwboundaries(blob_now,8,'noholes');
-       [M N] = size(blob_now);
-       perim_img = bound2im(b{1},M,N);
-       volume(ii) = distmap_volume(perim_img);
+    volume(ii) = NaN;
+    if t.Area(ii), %skip if no blob (area = 0) 
+        blob_now = bwl; blob_now(blob_now ~= ii) = 0;
+        blob_now = logical(blob_now); %needed for cases with bwlabel applied above    
+        if area_ratio(ii) < 1.2 || (t.Eccentricity(ii) < 0.8 && p(ii) > 0.8), %solid of revolution cases
+            theta = -1*t.Orientation(ii);
+            blob_rot = imrotate(blob_now, theta, 'bilinear'); % rotates the filled image
+            volume(ii) = volumewalk(blob_rot);
+        else %distance map cases
+           b =  bwboundaries(blob_now,8,'noholes');
+           [M N] = size(blob_now);
+           perim_img = bound2im(b{1},M,N);
+           volume(ii) = distmap_volume(perim_img);
+        end;
     end;
 end;
 target.blob_props.Biovolume = volume;
