804e4757b923c187bceabba2a41b8bb22a0dbaa0
Heidi Sosik <hsosik@whoi.edu>
2013-03-12 19:04:22 +0000

    update with comments
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@776 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: ad6f7c1c4f99b4bfdadb61b6dcb9f37eced4a9e8

--- analyze_by_class/get_ciliate_ind.m
+++ analyze_by_class/get_ciliate_ind.m
@@ -1,6 +1,7 @@
 function [ ind_out, class_label ] = get_ciliate_ind( class2use, class_label )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%function [ ind_out, class_label ] = get_ciliate_ind( class2use, class_label )
+% MVCO class list specific to return of indices that correspond to all variants of ciliates
+%  Heidi M. Sosik, Woods Hole Oceanographic Institution, March 2013
 
 class2get = {'ciliate' 'ciliate_mix' 'tintinnid' 'Myrionecta' 'Laboea' 'S_conicum' 'tiarina' 'strombidium_1' 'S_caudatum'...
     'Strobilidium_1' 'Tontonia' 'strombidium_2' 'S_wulffi'  'S_inclinatum' 'Euplotes' 'Didinium' 'Leegaardiella' 'Sol' 'strawberry' 'S_capitatum'};
