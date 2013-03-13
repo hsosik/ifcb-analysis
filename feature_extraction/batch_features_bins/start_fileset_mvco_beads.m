ef800e6c1b3aa4ab1c6ab20a6c66d85382689fcd
Heidi Sosik <hsosik@whoi.edu>
2013-03-13 19:43:14 +0000

    configure batch to run MVCO bead time series
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@777 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 04d319ab21c3c49f9e0554a34edf6ede89b4dff6

---
+++ feature_extraction/batch_features_bins/start_fileset_mvco_beads.m
@@ -0,0 +1,18 @@
+addpath('/home/hsosik/ifcbcode/webservice_tools/');
+addpath('/home/hsosik/ifcbcode/feature_extraction/');
+addpath('/home/hsosik/ifcbcode/dipum_toolbox_2.0.1/');
+out_dir = '/mnt/queenrose/ifcb_data_mvco_jun06/features_beads_v2/';
+in_dir = 'http://demi.whoi.edu/mvco_beads/'; %USER web services to access data
+filelist = [];
+yr = 2006;
+for day = 1:366,
+    filelist = [filelist list_day(datestr(datenum(r,0,day),29), in_dir)];
+end;
+disp(filelist(end))
+filelist = regexprep(filelist, in_dir, '')';
+files_done = dir([out_dir 'IFCB*fea_v2.csv']);
+files_done = char(files_done.name);
+files_done = cellstr(files_done(:,1:end-11));
+filelist2 = setdiff(filelist, files_done); 
+disp(['processing ' num2str(length(filelist2)) ' files'])
+batch_features( in_dir, filelist2, out_dir );
