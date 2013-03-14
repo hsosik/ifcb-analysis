c076996816f623856533bef5011f2972b4a7530f
Heidi Sosik <hsosik@whoi.edu>
2013-03-14 21:55:20 +0000

    Modified to handle instances where csv file contains only headers; also removed apparently unnecessary loop manipulating headers (keys)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@779 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 3a5ddebc588a97d6185587f9bfbb781ba5b541b0

--- webservice_tools/csv2targets.m
+++ webservice_tools/csv2targets.m
@@ -12,14 +12,22 @@ else
 end
 
 [~, pos] = textscan(csv_data,'%s',1,'Delimiter','');
-cols = textscan(csv_data(pos+1:end), fmt, 'Delimiter',',');
-
+if pos < length(csv_data), %add else case to deal with instances with only headers 
+    cols = textscan(csv_data(pos+1:end), fmt, 'Delimiter',',');
+else
+    cols = cell(1,length(headers));
+end;
+%%
+%this section seems to do nothing? (Heidi)
 % wonky format conversion necessary.
-keys = {};
-n = length(headers);
-for count = 1:n
-    keys{end+1} = char(headers{count});
-end
+%keys = {};
+%n = length(headers);
+%for count = 1:n
+%    keys{end+1} = char(headers{count});
+%end
+
+%%
+keys = headers; %Heidi: instead of prev section
 
 targets = cell2struct(cols,keys,2);
 
