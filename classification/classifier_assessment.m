71a10d8eec34ffe340ce1ef1fc73b023a016dec4
Heidi Sosik <hsosik@whoi.edu>
2013-03-19 15:34:09 +0000

    fix bug in check for adhoc_thre field in config; echo filenames to screen
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@785 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: a14bfe056e72fc7bca1b258f12b6ffb878c015c2

--- classification/classifier_assessment.m
+++ classification/classifier_assessment.m
@@ -4,6 +4,7 @@ function [ config, output ] = classifier_assessment( config )
 filelist = config.filelist;
 for ii = 1:length(filelist),
     [~,fname] = fileparts(filelist(ii).name);
+    disp(fname)
     t1 = load([config.manual_path fname]);
     t2 = load([config.class_path fname '_class_v1']); 
     
@@ -20,7 +21,7 @@ for ii = 1:length(filelist),
     end;
 
     if strmatch('TBclass_above_adhoc_threshold', config.TB2summarize),
-        if exist('config.adhoc_thre', 'var'),
+        if isfield(config, 'adhoc_thre'),
             t2.TBclass_above_adhoc_threshold = apply_TBthreshold(t2.class2useTB, t2.TBscores, config.adhoc_thre);
         else
             disp('You must specify config.adhoc_thre if you want to analyze TBclass_above_adhoc_threshold')
