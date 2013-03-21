e5f01fe2946908875f06650ea07ccda17c4d9c30
Heidi Sosik <hsosik@whoi.edu>
2013-03-21 18:31:54 +0000

    revise to simply and make more generic
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@789 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: ce1ea3382559269abccce6fbf670230b23fa591a

--- IFCB_tools/IFCB_volume_analyzed.m
+++ IFCB_tools/IFCB_volume_analyzed.m
@@ -1,20 +1,22 @@
-function [ ml_analyzed ] = IFCB_volume_analyzed( hdrfile )
-%function [ ml_analyzed ] = IFCB_volume_analyzed( hdrfile )
+function [ ml_analyzed ] = IFCB_volume_analyzed( hdrfilename )
+%function [ ml_analyzed ] = IFCB_volume_analyzed( hdrfilename )
+% hdrfilename input as char or cell complete with path (or url)
 % reads sample run time and "inhibit" time from IFCB hdr file 
 % returns associated estimate of sample volume analyzed (in milliliters)
-% assuming standard IFCB configuration with 5 mL syringe and pump
-% operating at 40 steps per second and 48000 steps per syringe
+% assuming standard IFCB configuration with sample syringe operating 
+% at 0.25 mL per minute
 % Applies only to IFCB instruments after 007 and higher (except 008)
 % Heidi M. Sosik, Woods Hole Oceanographic Institution, September 2012
 
-steps_per_sec = 40;
-ml_per_step = 5/48000;
-flowrate = ml_per_step * steps_per_sec;  %ml/sec
-ml_analyzed = NaN;
-hdr = IFCBxxx_readhdr(hdrfile); 
-if ~isempty(hdr),
-    looktime = hdr.runtime - hdr.inhibittime;
-    ml_analyzed = flowrate.*looktime;
+flowrate = 0.25; %milliliters per minute for syringe pump
+if ischar(hdrfilename), hdrfilename = cellstr(hdrfilename); end;
+ml_analyzed = NaN(size(hdrfilename));
+for count = 1:length(hdrfilename),
+    hdr = IFCBxxx_readhdr(hdrfilename{count});
+    if ~isempty(hdr),
+        looktime = hdr.runtime - hdr.inhibittime; %seconds
+        ml_analyzed(count) = flowrate.*looktime/60;
+    end;
 end;
 
 end
