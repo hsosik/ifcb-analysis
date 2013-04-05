2f221abde72e224e34829cadca6638a9bc84cabb
Emily Brownlee <ebrownlee@whoi.edu>
2013-04-05 19:14:02 +0000

    set up to look at Emily Peacock's samples
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@815 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 89e97418d7bd8fb10bb8b0913947ddcf532f3de5

--- manualclassify/get_MCconfigDock.m
+++ manualclassify/get_MCconfigDock.m
@@ -10,22 +10,46 @@ MCconfig.filenum2start = 1;  %USER select file number to begin (within the chose
 %MCconfig.pick_mode = 'correct_or_subdivide'; %USER choose one from case list
 MCconfig.pick_mode = 'raw_roi';
 
-MCconfig.resultpath = '\\queenrose\e_work\IFCB_dock\manual_class\'; %USER set
+%MCconfig.resultpath = '\\queenrose\e_work\IFCB_dock\manual_class\'; %USER set
+MCconfig.resultpath = '\\queenrose\e_work\Emily_Peacock\manual_class\'; %USER set
 MCconfig.class_filestr = '_class_v1'; %USER set, string appended on roi name for class files
 
-temp = load('class2use_MVCOmanual3', 'class2use');
-MCconfig.class2use = [temp.class2use];
+temp = load('class2use_MVCOmanual3', 'class2use'); 
+%MCconfig.class2use = [temp.class2use 'unclassified'];
+MCconfig.class2use = temp.class2use;
 MCconfig.default_class = 'other';
+temp = load('class2use_MVCOciliate', 'class2use_sub4'); 
+MCconfig.class2use_sub = temp.class2use_sub4;
+MCconfig.sub_default_class = 'Ciliate_mix';
+MCconfig.classstr = 'ciliate';
 big_only = 0; %case for picking Laboea and tintinnids only
 
 MCconfig.class2view1 = MCconfig.class2use; %case to view all
+%MCconfig.class2view1 = { }; %example to skip view1 
 %MCconfig.class2view1 = setdiff(MCconfig.class2use, {'bad' 'mix'}); %example to skip a few
-%MCconfig.class2view1 = intersect(MCconfig.class2use, {'ciliate'}); %example to select a few
-%MCconfig.class2view1 = intersect(MCconfig.class2use, {'Ceratium'}); %example to select a few
+%MCconfig.class2view1 = intersect(MCconfig.class2use, {'Ditylum'}); %example to select a few
+MCconfig.class2view2 = MCconfig.class2use_sub; %case to view all ciliates
+%MCconfig.class2view2 = { }; %example to skip view2
 
 if ~exist(MCconfig.resultpath, 'dir'),
     dos(['mkdir ' resultpath]);
 end;
 
 end
+
+% temp = load('class2use_MVCOmanual3', 'class2use');
+% MCconfig.class2use = [temp.class2use];
+% MCconfig.default_class = 'other';
+% big_only = 0; %case for picking Laboea and tintinnids only
+% 
+% MCconfig.class2view1 = MCconfig.class2use; %case to view all
+% %MCconfig.class2view1 = setdiff(MCconfig.class2use, {'bad' 'mix'}); %example to skip a few
+% %MCconfig.class2view1 = intersect(MCconfig.class2use, {'ciliate'}); %example to select a few
+% %MCconfig.class2view1 = intersect(MCconfig.class2use, {'Ceratium'}); %example to select a few
+% 
+% if ~exist(MCconfig.resultpath, 'dir'),
+%     dos(['mkdir ' resultpath]);
+% end;
+
+% end
 
