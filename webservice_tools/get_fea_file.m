909762a4bedbb92280f9a78ee22d46c981c70344
Heidi Sosik <hsosik@whoi.edu>
2013-04-08 12:43:10 +0000

    function to read in feature csv
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@818 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 2ad45d08f147c501f956e05a19b54428aef20f02

---
+++ webservice_tools/get_fea_file.m
@@ -0,0 +1,25 @@
+function [ feadata, feahdrs ] = get_fea_file( fullfilename )
+%function [ feadata, feahdrs ] = get_fea_file( filename )
+%import IFCB feature file, parse to data matrix
+%target values and return them in structure (hdr)
+%Heidi M. Sosik, Woods Hole Oceanographic Institution, April 2012
+
+if isequal(fullfilename(1:4), 'http'), 
+    [filestr,status] = urlwrite(fullfilename, 'temp.csv');
+    if status,
+        fullfilename = filestr;
+    else
+        disp(['Error reading ' fullfilename]);
+        feadata = [];
+        feahdrs = '';
+        return
+    end;
+end;
+t = importdata(fullfilename, ','); 
+feahdrs = t.colheaders;
+feadata = t.data;
+%remove temporary file if read from URL
+if exist('status', 'var'), delete(filestr); end;
+
+end
+
