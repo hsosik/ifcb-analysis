1e23ab045b7e954725f38493dcd219e04b70c2ac
Heidi Sosik <hsosik@whoi.edu>
2013-04-11 14:27:29 +0000

    revise to better handle case with no files to process
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@823 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 2aaba66efc103630dcedc92c50fdd7d6d3cee41a

--- classification/batch_classification/start_batch_classify.m
+++ classification/batch_classification/start_batch_classify.m
@@ -3,7 +3,7 @@ yr = 2013;
 out_dir = ['\\queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\class' num2str(yr) '_v1\'];
 in_dir = 'http://demi.whoi.edu/mvco/';
 filelist = [];
-for day = 95:95,
+for day = 70:96,
     filelist = [filelist list_day(datestr(datenum(yr,0,day),29), in_dir)];
 end;
 filelist = regexprep(filelist, in_dir, '')';
@@ -11,6 +11,8 @@ files_done = dir([out_dir 'IFCB*class_v1.mat']);
 files_done = char(files_done.name);
 files_done = cellstr(files_done(:,1:end-13));
 filelist2 = setdiff(filelist, files_done); 
-filelist2 = strcat(filelist2,'_features.csv');
 disp(['processing ' num2str(length(filelist2)) ' files'])
-batch_classify( in_dir, filelist2, out_dir, classifierName );
+if ~isempty(filelist2),
+    filelist2 = strcat(filelist2,'_features.csv');
+    batch_classify( in_dir, filelist2, out_dir, classifierName );
+end;
