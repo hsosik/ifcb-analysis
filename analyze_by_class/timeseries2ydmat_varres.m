f69f9d90a3cbed20a66e4f37b3ca5199b7446989
Heidi Sosik <hsosik@whoi.edu>
2013-04-13 13:11:50 +0000

    more general case that allows any resolution for time axis of output matrices (decimal year days)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@827 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: a73af059cc0a44fa71219503e4611543dcfdd429

---
+++ analyze_by_class/timeseries2ydmat_varres.m
@@ -0,0 +1,24 @@
+function [ mdate_mat, y_mat, yearlist, yd ] = timeseries2ydmat_varres( mdate, y, width )
+%function [ mdate_mat, y_mat, yearlist, yd ] = timeseries2ydmat_varres( mdate, y, width )
+%accept an input time series (vector of dates in mdate; vector of y-values
+%in y) and output a matrix of mean values for each yearday by year 
+%Heidi M. Sosik, Woods Hole Oceanographic Institution, March 2012
+
+yd = (1:width:366)';
+dv = datevec(mdate); 
+yearlist = (dv(1,1):dv(end,1));
+mdate_year = datenum(yearlist,0,0);
+mdate_mat = repmat(yd,1,length(yearlist))+repmat(mdate_year,length(yd),1);
+yearday_all = mdate-datenum(dv(:,1),0,0);
+y_mat = NaN(size(mdate_mat));
+
+for count = 1:length(yearlist),    
+    iii = find(dv(:,1) == yearlist(count));
+    for day = yd(1):yd(end),
+        ii = find(floor(yearday_all(iii)) == day);
+        if ~isempty(ii),
+            y_mat(day,count) = nanmean(y(iii(ii)),1);
+        end;
+    end;
+end;
+end
