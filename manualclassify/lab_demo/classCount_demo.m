95f01ee7159f4a82eb7e480d4b051aaf02a8860b
Heidi Sosik <hsosik@whoi.edu>
2013-04-27 01:21:05 +0000

    code for lab demo April 2013
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@829 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 661a362d92a4e23ba103f975ba61d16b27d7dbb5

--- manualclassify/lab_demo/classCount_demo.m
+++ manualclassify/lab_demo/classCount_demo.m
@@ -1,5 +1,5 @@
 close all
-resultpath = '\\queenrose\g_work_ifcb1\Demo_28Apr2012\ManualClassify\';
+resultpath = '\\queenrose\g_work_ifcb1\Demo_27Apr2013\ManualClassify\';
 
 filelist = dir([resultpath '*.mat']);
 filelist = {filelist.name};
@@ -8,11 +8,16 @@ load([resultpath filelist{1}])
 class2use = class2use_manual;
 ml = NaN(1,length(filelist));
 classcount = NaN(length(class2use), length(filelist));
-[class2use_plot, ind] = setdiff(class2use, {'other_small', 'other_large'});
+%[class2use_plot, ind] = setdiff(class2use, {'other_small', 'other_large'});
+[class2use_plot, ind] = setdiff(class2use, {'other'});
+temp = char(filelist); temp = cellstr(temp(:,1:end-4));
+t = load('\\RASPBERRY\d_work\IFCB1\code_mar10_mvco\ml_analyzed_all');
+[~,~,ii] = intersect(temp,t.filelist_all);
+ml = t.ml_analyzed(ii);  clear t temp
 for ii = 1:length(filelist),
     fname = filelist{ii};
     load([resultpath fname])
-    ml(ii) = ml_analyzed;
+    %ml(ii) = ml_analyzed;
     for classnum = 1:length(class2use),
         classcount(classnum,ii) = size(find(classlist(:,2) == classnum),1); %manual only
     end;
@@ -20,7 +25,7 @@ for ii = 1:length(filelist),
     classcount_plot = classcount(ind,:);
     [classcount_sort,sind] = sort(classcount_plot(:,ii), 'descend');
     sind = sind(classcount_sort > 0);
-    bar(classcount(sind,ii)./ml(ii),.5);
+    bar(classcount_plot(sind,ii)./ml(ii),.5);
     %set(gca, 'xtick', 1:length(sind), 'xticklabel', class2use_plot(sind))
     set(gca, 'xtick',  1:length(sind), 'xticklabel', [])
     axpos = get(gca, 'position'); axpos([2,4]) = axpos([2,4]).*[3 .7]; set(gca, 'position', axpos)
@@ -33,6 +38,6 @@ for ii = 1:length(filelist),
         title(['MVCO ' datestr(matdate(ii))])
     end  
 end;
-clear ii ml_analyzed classnum list_titles classlist class2use_auto class2use_sub class2use_manual resultpath
+%clear ii ml_analyzed classnum list_titles classlist class2use_auto class2use_sub class2use_manual resultpath
 
 
