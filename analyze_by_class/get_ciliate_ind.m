021317abe92f44c61ec7d736a6587eb46e28c4ab
Emily Brownlee <ebrownlee@whoi.edu>
2013-05-02 23:16:20 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@834 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 2d0dac56262c02291c3b32a1b08e56d1a2bd9ccd

--- analyze_by_class/get_ciliate_ind.m
+++ analyze_by_class/get_ciliate_ind.m
@@ -1,12 +1,12 @@
-function [ ind_out, class_label ] = get_ciliate_ind( class2use, class_label )
+function [ ind_out, class_label ] = get_ciliate_ind( class2use, class_label)
 %function [ ind_out, class_label ] = get_ciliate_ind( class2use, class_label )
 % MVCO class list specific to return of indices that correspond to all variants of ciliates
 %  Heidi M. Sosik, Woods Hole Oceanographic Institution, March 2013
 
-class2get = {'ciliate' 'ciliate_mix' 'tintinnid' 'Myrionecta' 'Laboea' 'S_conicum' 'tiarina' 'strombidium_1' 'S_caudatum'...
-    'Strobilidium_1' 'Tontonia' 'strombidium_2' 'S_wulffi'  'S_inclinatum' 'Euplotes' 'Didinium' 'Leegaardiella' 'Sol' 'strawberry' 'S_capitatum'};
+class2get = {'ciliate_mix' 'Didinium_sp' 'Euplotes_sp' 'Laboea_strobila' 'Leegaardiella_ovalis' 'Mesodinium_sp' 'Pleuronema_sp' 'Strobilidium_morphotype1' 'Strobilidium_morphotype2' 'Strombidium_capitatum' 'Strombidium_caudatum' 'Strombidium_conicum' 'Strombidium_inclinatum' 'Strombidium_morphotype1' 'Strombidium_morphotype2' 'Strombidium_oculatum' 'Strombidium_wulffi' 'Tiarina_fusus' 'Tintinnid' 'Tontonia_appendiculariformis' 'Tontonia_gracillima'};
 
 [~,ind_out] = intersect(class2use, class2get);
+ind_out = sort(ind_out);
 
 end
 
