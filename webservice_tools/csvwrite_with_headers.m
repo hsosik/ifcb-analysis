64b5dcc67c7ce97baafcd3d73812e541618bf1d2
Joe Futrelle <joefutrelle@gmail.com>
2013-06-13 18:33:01 +0000

    slowed down blob logging added workaround for slow export(dataset) usage see #2454
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@854 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 7523b2781257522f976bbab562659132b9e01f49

---
+++ webservice_tools/csvwrite_with_headers.m
@@ -0,0 +1,19 @@
+function [ ] = csvwrite_with_headers( filename, m, headers )
+% filename = output file name
+% m = some matrix
+% headers = cell array of header names, same width as matrix
+
+header_string = headers{1};
+for i = 2:length(headers)
+    header_string = [header_string,',',headers{i}];
+end
+
+% write headers
+fid = fopen(filename,'w');
+fprintf(fid,'%s\r\n',header_string);
+fclose(fid);
+
+% append CSV representation of matrix
+dlmwrite(filename, m,'-append','delimiter',',');
+end
+
