75f669c49683b17135399be4b78ab52ffa0525b6
Heidi Sosik <hsosik@whoi.edu>
2013-06-19 18:16:17 +0000

    update micron_factor for current blob/feature algorithms; also currently configured to ignore "large diatom" carbon:biovol (all computed with generic protist relationship)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@856 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 8b72d323c7608e43b6ed236150a28f662b28076b

--- analyze_by_class/biovolume_summary_ICESCAPE_manual.m
+++ analyze_by_class/biovolume_summary_ICESCAPE_manual.m
@@ -3,7 +3,8 @@ metapath = '\\floatcoat\IFCBdata\IFCB8_HLY1101\metadata\';
 load([metapath 'ml_analyzed']) %load the milliliters analyzed for all sample files
 biovolpath_base = '\\floatcoat\IFCBdata\IFCB8_HLY1101\biovolume\';
 %micron_factor = 1/3.4; %microns per pixel  FIX!!!!!!!
-micron_factor = 1/4.23; %USER, instrument specific entry for pixels per micron %IFCB8 4.23 from beads in IFCB8_2011_203_135906, see est_micron_factor.m
+%micron_factor = 1/4.23; %USER, instrument specific entry for pixels per micron %IFCB8 4.23 from beads in IFCB8_2011_203_135906, see est_micron_factor.m
+micron_factor = 1/3.92; %USER, instrument specific entry for pixels per micron %IFCB8 4.23 from beads in IFCB8_2011_203_135906, see est_micron_factor.m, updated with new features 
 
 resfilelist = dir([resultpath 'IFCB*.mat']);
 [~,ia, ib] = intersect(regexprep({resfilelist.name}', '.mat', ''), filelist);
@@ -37,7 +38,7 @@ matdate = datenum(year,0,yearday,hour,min,sec);
 clear fstr yearday hour min sec
 
 %load([resultpath filelist(1).name]) %read first file to get classes
-temp = load('\\floatcoat\LaneyLab\projects\HLY1101\work_IFCB8\code_Aug2012\class2use_HLY1101', 'class2use');
+temp = load('\\floatcoat\LaneyLab\projects\HLY1101\work_IFCB8\code\class2use_HLY1101', 'class2use');
 class2use_manual_first = [temp.class2use]; clear temp
 numclass = length(class2use_manual_first);
 class2use_here = class2use_manual_first;
@@ -64,19 +65,23 @@ lgdiatom_flag = zeros(size(class2use_here)); lgdiatom_flag(diatom_ind) = 1;
         classlist = classlist(tind,:); 
         if ~isequal(class2use_manual, class2use_manual_first)
             disp('class2use_manual does not match previous files!!!')
-            if isequal(class2use_manual, class2use_manual_first(1:length(class2use_manual))),
+            t = min([length(class2use_manual) length(class2use_manual_first)]);
+            if isequal(class2use_manual(1:t), class2use_manual_first(1:t)),
                 disp('class2use_manual missing entries on end')
-            else
-                keyboard
+            %else
+                %keyboard
             end;
         end;
         temp = zeros(1,numclass); %init as zeros for case of subdivide checked but none found, classcount will only be zero if in class_cat, else NaN
         tempvol = temp;
+        %tempcarbon = temp;
         for classnum = 1:numclass,
             cind = find(classlist(:,2) == classnum | (isnan(classlist(:,2)) & classlist(:,3) == classnum));
             temp(classnum) = length(cind);
             tempvol(classnum) = nansum(targets.Biovolume(cind)*micron_factor.^3); %cubic microns
-            tempcarbon(classnum) =  nansum(biovol2carbon(targets.Biovolume(cind)*micron_factor.^3,lgdiatom_flag(classnum))); %picograms
+            %tempcarbon(classnum) =  nansum(biovol2carbon(targets.Biovolume(cind)*micron_factor.^3,lgdiatom_flag(classnum))); %picograms
+            %%%%%%%%%%TEMPORARY results without using the large diatom C:vol
+            tempcarbon(classnum) =  nansum(biovol2carbon(targets.Biovolume(cind)*micron_factor.^3,0)); %picograms
         end;
         classcount(filecount,:) = temp;
         classbiovol(filecount,:) = tempvol;
@@ -91,5 +96,5 @@ if ~exist([resultpath 'summary\'], 'dir')
 end;
 datestr = date; datestr = regexprep(datestr,'-','');
 units = {'biovol is in cubic microns, carbon is in picograms'};
-save([resultpath 'summary\count_biovol_manual_' datestr], 'matdate', 'ml_analyzed', 'classcount', 'classbiovol', 'filelist', 'class2use', 'metadata', 'classcarbon')
+save([resultpath 'summary\count_biovol_manual_junk_' datestr], 'matdate', 'ml_analyzed', 'classcount', 'classbiovol', 'filelist', 'class2use', 'metadata', 'classcarbon', 'units')
 
