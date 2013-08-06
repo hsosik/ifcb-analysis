9eb0e3a0d426483b361f9c4db338d5699f9c5bdc
yannick <yannick>
2013-08-06 18:48:25 +0000

    Updated to includes changes to batch mode, function input to start_manual_classify, sorting according to size, and deprecate user need to change get_MCfilelist
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@869 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 1e1e67e78ea8e1f082d74aa7828f10dd6fc1e05b

--- manualclassify/get_MCfilelist.m
+++ manualclassify/get_MCfilelist.m
@@ -1,10 +1,25 @@
 function [MCconfig, filelist, classfiles ] = get_MCfilelist( MCconfig )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+% index=2;
 
-basepath = 'C:\work\IFCB11\';
-classpath = MCconfig.classpath;
-filelist = dir([basepath '\D2012\D20120615\*.adc']);
+switch MCconfig.batchmode
+    case 'no'
+%         basepath = 'J:';
+        basepath = MCconfig.basepath;
+        classpath = MCconfig.classpath;
+        % filelist = dir([basepath '\D2012\D20121121\*.adc'])
+        filelist = dir([basepath MCconfig.filepath '/*.adc'])
+    case 'yes'
+        classpath = MCconfig.classpath;
+        % filelist = dir([basepath '\D2012\D20121121\*.adc'])
+        filelist = dir([MCconfig.resultpath MCconfig.filepath])
+        basepath = MCconfig.basepath;
+end
+
+
+%removing files from the list to start at the required file
+% filelist=filelist(index:end);
 
 if isempty(filelist),
 %    disp('No files found. Check paths or file specification in get_MCconfig.')
