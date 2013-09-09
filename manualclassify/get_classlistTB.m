50d39f0cfe869ce0d846ab2958de547a8a1e5dd5
Emily Peacock <epeacock@whoi.edu>
2013-09-09 19:58:10 +0000

    fixed mistaken reference to classnum_default that should have been classnum_default_sub
    Heidi and Emily P.
    
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@886 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 52d2492d1a125e4df0d6e08f3a67a1d92d169671

--- manualclassify/get_classlistTB.m
+++ manualclassify/get_classlistTB.m
@@ -56,7 +56,7 @@ if exist([manualfilename], 'file')  %~isempty(tempdir)
         end;
         classnum = strmatch(classstr, class2use_manual); %default class number from original list
         classlist((classlist(:,manual_col) == classnum & isnan(classlist(:,sub_col))),sub_col) = classnum_default_sub; %classnum_default; %find any new additions to manual start category
-        classlist(classlist(:,auto_col) == classnum & isnan(classlist(:,manual_col)) & isnan(classlist(:,sub_col)),sub_col) = classnum_default; %set default from auto
+        classlist(classlist(:,auto_col) == classnum & isnan(classlist(:,manual_col)) & isnan(classlist(:,sub_col)),sub_col) = classnum_default_sub; %set default from auto
         [overlap, ind_sub, ind] = intersect(class2use_sub, class2use);
         for count1 = 1:length(overlap), %set any overlap categories from previous in sub_col, if not already marked in sub_col
             %classlist(classlist(:,auto_col) == ind(count1) & isnan(classlist(:,sub_col)), sub_col) = ind_sub(count1); %set to new class number in subcol
