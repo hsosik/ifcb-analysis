1e4c34dbdbb2f87145a15a69477d23de7d5f512a
Emily Peacock <epeacock@whoi.edu>
2013-09-09 19:56:10 +0000

    fixed mistaken reference to classnum_default which should have been classnum_default_sub (causing erroneous class numbers from default class2view1 to show up in sub4class ('ciliate' list).
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@885 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 0b5a829d5d28362a66e5f786eac735bf66a0b5dc

--- manualclassify/manual_classify_4_0.m
+++ manualclassify/manual_classify_4_0.m
@@ -209,7 +209,7 @@ for filecount = filenum2start:length(filelist),
                             %classlist(~isnan(classlist(:,sub_col)) & ~isnan(classlist(:,2)) & classlist(:,2) ~= strmatch(classstr, class2use_manual), sub_col) = NaN;
                             %1/15/10, recast above so the subdivide ID overrides instead (i.e., just skip above line)
                             classlist(classlist(:,sub_col) >= 2,2) = strmatch(classstr, class2use_manual);  %reassign manual column (#2) with relevant sub_col entries
-                            classlist(classlist(:,2) == strmatch(classstr, class2use_manual) & isnan(classlist(:,sub_col)), sub_col) = classnum_default;  % = 2; changed 1/15/10 ??correct??
+                            classlist(classlist(:,2) == strmatch(classstr, class2use_manual) & isnan(classlist(:,sub_col)), sub_col) = classnum_default_sub;  % = 2; changed 1/15/10 ??correct??
                             eval(['class2use_sub' num2str(sub_col) '= class2use_sub;'])
                             mark_col = sub_col; %reset col for ID in classlist %comment out 9/29/09 Heidi
                         end;
