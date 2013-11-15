fab8b965f52358aa71006edf6f3f0417dfa4dc69
Heidi Sosik <hsosik@whoi.edu>
2013-11-15 21:38:20 +0000

    case with settings for Brosnahan lab experiment
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@905 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: e76044e42b70779619ed3dc28db1fe5fb64edce1

--- classification/compile_train_features3.m
+++ classification/compile_train_features3.m
@@ -1,7 +1,8 @@
 %load output %load result file produced by batch_features_train.m
 %outdir = 'C:\work\IFCB\ifcb_data_MVCO_jun06\train_04Nov2011_fromWebServices\';
-outdir = 'C:\work\IFCB3LisaCampbell\Training_images_PNG\';
-maxn = 300;
+outdir = '\\queenrose\E_work\Brosnahan_Temp\manual_fromWeb\';
+savedir = '\\queenrose\E_work\Brosnahan_Temp\';
+maxn = 300; %USER select
 temp = dir([outdir '*.mat']);
 classes = {temp.name};
 for i = 1:length(classes),
@@ -41,7 +42,7 @@ end;
 
 train = cell2mat(feamat)';
 
-save compiled_train_tamu3 train class_vector classes targets featitles
+save([savedir 'compiled_train_lab'], 'train', 'class_vector', 'classes', 'targets', 'featitles')
 %save compiled_train_mvco train class_vector classes targets featitles
 
 clear t_temp classcount i n maxn temp output outdir out class ind feature_mat
