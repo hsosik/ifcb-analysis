08213cd5fa8b4abc35e7c4435ac141f8201c9010
Heidi Sosik <hsosik@whoi.edu>
2013-12-04 21:11:18 +0000

    compute confidence interval on a count
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@914 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: f115e41b61ddaf6d60a40d447da54db8ee923308

---
+++ IFCB_tools/poisson_count_ci.m
@@ -0,0 +1,17 @@
+function [ ci] = poisson_count_ci( N, ciint)
+%function [ ciu, cil ] = poisson_count_ci( N, alpha )
+%   Compute upper and lower confidence intervals on a count from Poisson distribution
+%   Input:  N - vector of count values
+%           ciint - value less than 1 specifying width of confidence
+%           interval (e.g., 0.95 for 95% ci)
+%   Output: upper, lower - upper a lower confidence intervals, respectively
+%
+% Heidi M. Sosik, Woods Hole Oceanographic Institution, Sep 2013
+
+alpha = 1-ciint;
+for ii = 1:length(N),
+    [~,ci(ii,:)] = poissfit(N(ii),alpha);
+end;
+
+end
+
