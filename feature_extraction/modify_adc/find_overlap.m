dba00231393adefb137d0271da8ad4687a1fc9ba
Heidi Sosik <hsosik@whoi.edu>
2013-12-12 23:57:49 +0000

    add number of overlapping pixels to outputs
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@917 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 1975e9d020accd3616b8a01fbd755e57a7a938e6

--- feature_extraction/modify_adc/find_overlap.m
+++ feature_extraction/modify_adc/find_overlap.m
@@ -1,4 +1,4 @@
-function [ offsetx_est, offsety_est ] = find_overlap( imgA, imgB )
+function [ offsetx_est, offsety_est, npix ] = find_overlap( imgA, imgB )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -10,53 +10,64 @@ imgB1 = imgB - mode([imgA(:); imgB(:)]) ;
 %Rnorm = xcorr2fft(abs(imgB1),abs(imgA1)) ;
 R = xcorr2(imgB1,imgA1) ;
 Rnorm = xcorr2(abs(imgB1),abs(imgA1)) ;
-Rcheck = R - Rnorm ; 
+Rcheck = R - Rnorm ;
 
 z = find(Rcheck(:) == 0) ; % Find offsets with perfect correlation
 offsetx_est = NaN;
 offsety_est = NaN;
+npix = NaN;
 if ~isempty(z),
-[zind(:,1) zind(:,2)] = ind2sub([size(imgA,1)+size(imgB,1)-1 size(imgA,2)+size(imgB,2)-1], z) ;
+    [zind(:,1) zind(:,2)] = ind2sub([size(imgA,1)+size(imgB,1)-1 size(imgA,2)+size(imgB,2)-1], z) ;
+    
+    % Determine the associated size of the overlap regions
+    m = ones(1,size(imgA,1)) ;
+    n = ones(1,size(imgB,1)) ;
+    overlap_cols = xcorr(n,m) ;
+    
+    m = ones(1,size(imgA,2)) ;
+    n = ones(1,size(imgB,2)) ;
+    overlap_rows = xcorr(n,m) ;
+    zind(:,3) = overlap_cols(zind(:,1)) .* overlap_rows(zind(:,2)) ;
+    
+    % Figure out what delays they correspond to
+    offx = (-size(imgA,1)+1:(size(imgB,1))) ;
+    offy = (-size(imgA,2)+1:(size(imgB,2))) ;
+    
+    zind(:,1) = offx(zind(:,1)) ;
+    zind(:,2) = offy(zind(:,2)) ;
+    
+    % Select the perfect correlation offset that corresponds to the biggest overlap
+    [a b] = max(zind(:,3));
+    %figure(6), plot(zind(:,3), '.-')
+    
+    offsetx_est = -zind(b,1);
+    offsety_est = -zind(b,2);
+    
+    % xpos_temp = [1; offsetx_est];
+    % ypos_temp = [1; offsety_est];
+    %
+    %xr = [xpos_temp xpos_temp + [size(imgA,1); size(imgB,1)]-1];
+    %yr = [ypos_temp ypos_temp + [size(imgA,2); size(imgB,2)]-1];
+    
+    s = intersect(0:size(imgA,1)-1, offsetx_est:size(imgB,1) + offsetx_est - 1);
+    t = intersect(0:size(imgA,2)-1, offsety_est:size(imgB,2) + offsety_est - 1);
 
-% Determine the associated size of the overlap regions
-m = ones(1,size(imgA,1)) ;
-n = ones(1,size(imgB,1)) ;
-overlap_cols = xcorr(n,m) ;
+    npix = length(s)*length(t); %how many pixels overlap
 
-m = ones(1,size(imgA,2)) ;
-n = ones(1,size(imgB,2)) ;
-overlap_rows = xcorr(n,m) ;
-zind(:,3) = overlap_cols(zind(:,1)) .* overlap_rows(zind(:,2)) ;
-
-% Figure out what delays they correspond to
-offx = (-size(imgA,1)+1:(size(imgB,1))) ;
-offy = (-size(imgA,2)+1:(size(imgB,2))) ;
-
-zind(:,1) = offx(zind(:,1)) ;
-zind(:,2) = offy(zind(:,2)) ;
-
-% Select the perfect correlation offset that corresponds to the biggest overlap
-[a b] = max(zind(:,3));
-%figure(6), plot(zind(:,3), '.-')
-
-offsetx_est = -zind(b,1);
-offsety_est = -zind(b,2);
-
-% xpos_temp = [1; offsetx_est];
-% ypos_temp = [1; offsety_est];
-% 
-%xr = [xpos_temp xpos_temp + [size(imgA,1); size(imgB,1)]-1];
-%yr = [ypos_temp ypos_temp + [size(imgA,2); size(imgB,2)]-1];
-
-s = intersect(0:size(imgA,1)-1, offsetx_est:size(imgB,1) + offsetx_est - 1);
-t = intersect(0:size(imgA,2)-1, offsety_est:size(imgB,2) + offsety_est - 1);
-
-%check to make sure the peak is a real overlap with no pixel differences
-if isempty(t) || isempty(s), keyboard, end;
-if ~isempty(find(imgA(s+1,t+1)-imgB(s-offsetx_est+1,t-offsety_est+1), 1)),
-    offsetx_est = NaN;
-    offsety_est = NaN;
-end;
+    %check to make sure the peak is a real overlap with no pixel differences
+    if isempty(t) || isempty(s), keyboard, end;
+    if ~isempty(find(imgA(s+1,t+1)-imgB(s-offsetx_est+1,t-offsety_est+1), 1)),
+        offsetx_est = NaN;
+        offsety_est = NaN;
+        npix = NaN;
+    else
+        if npix > 2 & npix <=50,
+            disp(npix),
+            clf, imagesc(0,0,imgA), colormap('gray')
+            hold on, imagesc(offsety_est, offsetx_est, imgB), colormap('gray'), axis auto
+            pause
+        end;
+    end;
 end;
 
 end
