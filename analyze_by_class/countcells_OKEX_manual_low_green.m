3a51d0421dbf6dcacb4ca99e7c442d77b6b329f6
Emily Brownlee <ebrownlee@whoi.edu>
2014-01-27 17:10:09 +0000

    updated countcells OKEX to create result files for cells above green/chl threshold
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@926 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: acab44b1e8e39886be7d774c7b4ecf111e1fb9b6

---
+++ analyze_by_class/countcells_OKEX_manual_low_green.m
@@ -0,0 +1,102 @@
+%resultpath = '\\mellon\saltpond\manualclassify\';
+resultpath = '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\Manual_fromClass\Alt\';
+%resultpath = 'C:\Users\Emily Fay\Documents\Ciliate_Code\IFCB_14\Van\results\alt\';
+%resultpath = 'C:\Users\Emily Fay\Documents\Ciliate_Code\IFCB_14\Van\results\normal\';
+%urlbase = 'http://ifcb-data.whoi.edu/saltpond/';
+urlbase = 'http://ifcb-data.whoi.edu/OkeanosExplorerAug2013_IFCB014/';
+%basepath = 'C:\Users\Emily Fay\Documents\Ciliate_Code\IFCB_14\Van\';
+filelist = dir([resultpath 'D*.mat']);
+basepath = '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\';
+
+%calculate date
+matdate = IFCB_file2date({filelist.name});
+
+load([resultpath filelist(1).name]) %read first file to get classes
+numclass1 = length(class2use_manual);
+numclass2 = length(class2use_sub4);
+numclass = numclass1 + numclass2;
+class2use_manual_first = class2use_manual;
+class2use_first_sub = class2use_sub4;
+class2use_here = [class2use_manual_first class2use_sub4];
+classcount = NaN(length(filelist),numclass);  %initialize output
+ml_analyzed = NaN(length(filelist),1);
+
+
+
+%for filecount = 15,
+for filecount = 1:length(filelist),
+    filename = filelist(filecount).name;
+    disp(filename)
+    hdrname = [basepath regexprep(filename, 'mat', 'hdr')]; 
+    adcname = [basepath regexprep(filename, 'mat', 'adc')];
+   ml_analyzed(filecount) = IFCB_volume_analyzed(hdrname);
+    load([resultpath filename])
+    adcdata = load(adcname);
+    
+    %refline(0.4 ,-1.5)
+    %refline(0.028,0.0023);
+    chl = log10(adcdata(:,5)); green = log10(adcdata(:,4));
+    %low_green_ind = find(green > (chl-0.0015)/0.25);
+    %low_green_ind = find(green < ((chl-0.0023)/0.028));
+    low_green_ind = find(green < ((chl+1.5)/0.4));
+    classlist=classlist(low_green_ind,1:4);
+    
+       
+    temp = zeros(1,numclass);
+    for classnum = 1:numclass1,
+                temp(classnum) = size(find(classlist(:,2) == classnum | (isnan(classlist(:,2)) & classlist(:,3) == classnum)),1);
+    end;
+       
+            for classnum = 1:numclass2,
+                temp(classnum+numclass1) = size(find(classlist(:,4) == classnum),1);
+            end;
+       
+        classcount((filecount),:) = temp;
+        
+    end;
+
+clear class2use_manual class2use_auto class2use_sub* classlist
+class2use = class2use_here;
+if ~exist([resultpath 'summary\'], 'dir')
+    mkdir([resultpath 'summary\'])
+end;
+datestr = date; datestr = regexprep(datestr,'-','');
+save([resultpath 'summary\count_manual_low_green' datestr], 'matdate', 'ml_analyzed', 'classcount', 'filelist', 'class2use')
+
+
+% load '\\queenrose\IFCB010_OkeanosExplorerAug2013\data\Manual_fromClass\summary\count_manual_15Jan2014.mat'
+% figure %example
+% classnum = 72; %90 for tintinnid
+% plot(matdate, classcount(:,classnum)./ml_analyzed, '.-')
+% datetick('x')
+% ylabel([class2use{classnum} ' (mL^{-1})'])
+% hold on
+% 
+% load '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\Manual_fromClass\summary\count_manual_15Jan2014.mat'
+% 
+% classnum = 72; %90 for tintinnid
+% plot(matdate, classcount(:,classnum)./ml_analyzed, 'r.-')
+% legend('IFCB10','IFCB14 stained ');
+% 
+% % figure %example
+% % classnum = 1;
+% % plot(matdate, classcount(:,classnum)./ml_analyzed, '.-')
+% % datetick('x')
+% % ylabel([class2use{classnum} ' (mL^{-1})'])
+
+load '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\Manual_fromClass\Alt\summary\count_manual_low_green23Jan2014.mat'
+figure %example
+classnum = 72; %90 for tintinnid
+plot(matdate, classcount(:,classnum)./ml_analyzed, '.-')
+datetick('x')
+ylabel([class2use{classnum} ' (mL^{-1})'])
+hold on
+
+load '\\queenrose\IFCB010_OkeanosExplorerAug2013\data\Manual_fromClass\summary\count_manual_19Jan2014.mat'
+
+classnum = 72; %90 for tintinnid
+plot(matdate, classcount(:,classnum)./ml_analyzed, 'r.-')
+legend('IFCB14 low green','IFCB10  ');
+
+
+
