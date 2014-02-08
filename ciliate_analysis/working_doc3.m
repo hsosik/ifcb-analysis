b374bf16931b2cec4656313737eb8e12e4e58b83
Emily Brownlee <ebrownlee@whoi.edu>
2014-02-08 01:53:46 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@950 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 73dd5323a7180338e23e70399846643b939b591c

--- ciliate_analysis/working_doc3.m
+++ ciliate_analysis/working_doc3.m
@@ -1,8 +1,8 @@
 
 %refline(0.020, 0.0033)
-chl = (adcdata(:,5)); green = adcdata(:,4));
-low_green_ind = find(green < ((chl+2.05)/0.20)); 
-classlist=classlist(low_green_ind,1:4);
+chl = adcdata(:,5); green = adcdata(:,4);
+low_green_ind = find(green < ((chl-0.0033)/0.020));%low green
+high_green_ind = find(green >= ((chl-0.0033)/0.020));
 
 
 figure1 = figure;
@@ -18,9 +18,11 @@ hold(axes1,'all');
 hold on
 
 m = 0.020; b = 0.0033;  x = 0.0001:0.01:2;
-plot((adcdata(:,4)),(adcdata(:,5)), '*', 'markersize', 1);
+%plot((adcdata(:,4)),(adcdata(:,5)), '*', 'markersize', 1);
+plot(green(low_green_ind), chl(low_green_ind), '*','markersize', 1.5)
+plot(green(high_green_ind), chl(high_green_ind), 'g*','markersize', 1.5)
 hold on
-plot(x, m*x+b, '-r');
+plot(x, m*x+b, '-k');
 xlim([0.0016 0.3])
 ylim([0.0025 0.11])
 axis square
