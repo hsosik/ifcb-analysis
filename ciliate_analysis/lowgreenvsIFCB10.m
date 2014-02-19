163c6291dc4ed4495c518828766e61821299fa79
Emily Brownlee <ebrownlee@whoi.edu>
2014-02-19 18:43:49 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@963 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 587849c53c23e87b720b22e71e225ce56390ef50

--- ciliate_analysis/lowgreenvsIFCB10.m
+++ ciliate_analysis/lowgreenvsIFCB10.m
@@ -1,22 +1,49 @@
-%I made a change
-load '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\Manual_fromClass\Alt\summary\count_manual_low_green27Jan2014.mat'
 
-load '\\queenrose\IFCB010_OkeanosExplorerAug2013\data\Manual_fromClass\summary\count_manual_19Jan2014.mat'
+load '/Volumes/IFCB014_OkeanosExplorerAug2013/data/Manual_fromClass/Alt/summary/count_manual_low_green27Jan2014.mat'
 
-min_diff= 1/24;
+load '/Volumes/IFCB010_OkeanosExplorerAug2013/data/Manual_fromClass/summary/count_manual_19Jan2014.mat'
+
+min_diff= 1/48;
 match_ind=NaN(size(matdate_lowgreen));
 
+
 for ii=1:length(matdate_lowgreen);
-    [min_ii, i2]=min(abs(matdate_lowgreen(ii)-matdate(ii)));
+    [min_ii, i2]=min(abs(matdate_lowgreen(ii)-matdate));
  
     if min_ii <= min_diff
         match_ind(ii)=i2;
+        %i2=match_ind(ii);
     end
 end;
 
-iii=find(isnan(i2));
 
-[matdate_lowgreen(iii)-matdate(i2(iii))]*24  %as a check, should appear as less than 1
+iii=find(isnan(match_ind));
+
+match_ind(find(isnan(match_ind))) = [];
+
+
+[matdate_lowgreen(iii)-matdate(match_ind(iii))]*24 %as a check, should appear as less than 1
+[matdate_lowgreen-matdate(match_ind)]*24 
+
+ciliate_classcount=classcount(:, 72:92);
+total_ciliate_classcount=mean(ciliate_classcount,2);
+
+low_green_ciliate_classcount=classcount_lowgreen(:, 72:92);
+total_low_green_ciliate_classcount=mean(low_green_ciliate_classcount,2);
+
+count1=total_low_green_ciliate_classcount(1:31)./ml_analyzed(1:31);
+count2=total_low_green_ciliate_classcount(36:46)./ml_analyzed(36:46);
+low_green_count_total= [count1; count2];
+low_green_count_total_day=total_low_green_ciliate_classcount(2:17)./ml_analyzed(2:17);
+
+count_total=ciliate_classcount(match_ind)./ml_analyzed(match_ind);
+count_total_day=ciliate_classcount(match_ind(2:17))./ml_analyzed(match_ind(2:17));
+
+figure
+plot(mean(count_total_day),mean(low_green_count_total_day), '*');
+
+
+
     
 
 
@@ -29,33 +56,3 @@ iii=find(isnan(i2));
 
 
 
-IFCB10_filename={filelist.name}';
-IFCB14_filename={filelist_lowgreen.name}';
-
-low_green_ind = find(green < ((chl+1.5)/0.4));
-    classlist=classlist(low_green_ind,1:4);
-    
-    ii = find(ifcb_filename(:,11:12) == 'D');
-    
-    IFCB10_filename(:(11:12)
-    D20130824T211357_IFCB010.mat
-    
-     ii = find(ifcb_filename(:,11:12)
-     
-     matdate = NaN(size(ifcb_filename,1),1);
-ii = find(ifcb_filename(:,1) == 'D');
-%datenum(year, month, day, hour, minute, second)
-matdate(ii) = datenum(str2num(ifcb_filename(ii,2:5)), str2num(ifcb_filename(ii,6:7)), str2num(ifcb_filename(ii,8:9)), str2num(ifcb_filename(ii,11:12)), str2num(ifcb_filename(ii,13:14)), str2num(ifcb_filename(ii,15:16)));
-ii = find(ifcb_filename(:,1) == 'I');
-%datenum(year, 0, yearday, hour, minute, second)
-matdate(ii) = datenum(str2num(ifcb_filename(ii,7:10)),0,str2num(ifcb_filename(ii,12:14)), str2num(ifcb_filename(ii,16:17)), str2num(ifcb_filename(ii,18:19)), str2num(ifcb_filename(ii,20:21)));
-
-% fstr = char(filelist);
-% year = str2num(fstr(:,2:5));
-% month=str2num(fstr(:,6:7));
-% yearday = str2num(fstr(:,8:9));
-% hour = str2num(fstr(:,11:12));
-% min = str2num(fstr(:,13:14));
-% sec = str2num(fstr(:,15:16));
-% matdate = datenum(year, month, day, hour, minute, second);
-end
