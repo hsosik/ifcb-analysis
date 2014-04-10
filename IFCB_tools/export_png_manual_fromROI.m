fe20ebe4355097d336a30c4052d31c2f2321071c
Heidi Sosik <hsosik@whoi.edu>
2014-04-10 00:55:44 +0000

    read manual annotations and output pngs by reading ROI files (calls export_png_from_ROIlist.m function)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@976 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: c350c895659166f2c6c8f786d8f40530765fec0b

---
+++ IFCB_tools/export_png_manual_fromROI.m
@@ -0,0 +1,40 @@
+%USER SET PATHS
+%where are your manual classification results? same resultpath as for manual_classify
+resultpath = '\\raspberry\d_work\IFCB1\ifcb_data_mvco_jun06\Manual_fromClass\';
+resultpath = 'C:\work\IFCB\ifcb_data_MVCO_jun06\Manual_fromClass\';
+outputpath = '\\raspberry\d_work\IFCB1\ifcb_data_MVCO_jun06\manual_test\'; %USER where to write out pngs
+outputpath = 'C:\work\IFCB\ifcb_data_MVCO_jun06\manual_test\'; %USER where to write out pngs
+roibasepath = '\\demi\vol3\';
+%urlbase = 'http://ifcb-data.whoi.edu/mvco/'; %USER where is your dashboard\web server
+
+resultfilelist = dir([resultpath 'I*.mat']);
+resultfilelist = char(resultfilelist.name);
+resultfilelist = cellstr(resultfilelist(:,1:end-4));
+
+for filecount = 1:length(resultfilelist),
+    resultfile = char(resultfilelist(filecount));    
+    load([resultpath resultfile])
+
+    %USER CHOOSE A LINE AND EDIT FOR YOUR CASE
+    %category = class2use_manual; %use this syntax to export ALL categories
+    category = {'Ceratium', 'Ditylum'}; %use this syntax to export ONLY the listed categories
+    %category = setdiff(class2use_manual, {'bad' 'ciliate' 'detritus'});  %use this syntax to export all EXCEPT the listed categories
+
+    disp(resultfile)
+    %make subdirs for tiffs
+    for count = 1:length(category),
+        if ~exist([outputpath char(category(count))], 'dir'),
+            mkdir([outputpath char(category(count))]);
+        end;
+    end;
+
+    roipath = [roibasepath filesep resultfile(1:end-7) filesep];
+    %loop over classes and save pngs to subdirs
+    for count2 = 1:length(category);
+        classnum = strmatch(category(count2), class2use_manual, 'exact');
+        ind = find(classlist(:,2) == classnum | (isnan(classlist(:,2)) & classlist(:,3) == classnum));
+        %   ind = find(classlist(:,2) == classnum);  %MANUAL ONLY
+        roinum = classlist(ind,1);
+        export_png_from_ROIlist([roipath resultfile],roinum, [outputpath filesep category{count2}]);
+    end;
+end
