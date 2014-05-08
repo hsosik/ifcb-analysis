15b73cd8513487c111f41799fff8724daf20859f
Emily Brownlee <ebrownlee@whoi.edu>
2014-05-08 17:50:18 +0000

    added a runtype identifier to result files
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@981 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 7f4ba5efdf0fddd03ac6ac712c070997930ac19e

---
+++ analyze_by_class/countcells_OKEX_manual_runtype.m
@@ -0,0 +1,91 @@
+%resultpath = '\\mellon\saltpond\manualclassify\';
+%resultpath = '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\Manual_fromClass\Normal\';
+%resultpath = 'C:\Users\Emily Fay\Documents\Ciliate_Code\IFCB_14\Scut_testing\Normal\results\';
+%resultpath = 'C:\Users\Emily Fay\Documents\Ciliate_Code\IFCB_14\Van\results\alt\';
+%resultpath = '/Volumes/IFCB14_Dock/data/Manual_fromClass/Non_Mixing_2-17-14/Alt/';
+%resultpath = '/Volumes/IFCB14_Dock/data/Manual_fromClass/Mixing_non_stain_2-22-14/Alt/';
+%resultpath = '/Volumes/IFCB14_Dock/data/Manual_fromClass/Mxing_non_stain_3-1-14/Alt/';
+resultpath = '/Users/markmiller/Documents/Experiments/IFCB_14/New_Tubing/Manual_fromClass/';
+%urlbase = 'http://ifcb-data.whoi.edu/saltpond/';
+%urlbase = 'http://ifcb-data.whoi.edu/OkeanosExplorerAug2013_IFCB014/';
+%basepath = 'C:\Users\Emily Fay\Documents\Ciliate_Code\IFCB_14\Van\';
+%basepath = '/Volumes/IFCB14_Dock/data/';
+basepath = '/Users/markmiller/Documents/Experiments/IFCB_14/New_Tubing/';
+filelist = dir([resultpath 'D*.mat']);
+
+%calculate date
+matdate = IFCB_file2date({filelist.name});
+
+
+load([resultpath filelist(1).name]) %read first file to get classes
+numclass1 = length(class2use_manual);
+numclass2 = length(class2use_sub4);
+numclass = numclass1 + numclass2;
+class2use_manual_first = class2use_manual;
+class2use_first_sub = class2use_sub4;
+class2use_here = [class2use_manual_first class2use_sub4];
+classcount = NaN(length(filelist),numclass);  %initialize output
+ml_analyzed = NaN(length(filelist),1);
+
+
+for filecount = 1:length(filelist),
+    %for filecount=1;
+    filename = filelist(filecount).name;
+    disp(filename)
+    hdrname = ([basepath regexprep(filename, 'mat', 'hdr')]);%for when there is no dashboard url
+    %hdrname = [urlbase regexprep(filename, 'mat', 'hdr')]; 
+   ml_analyzed(filecount) = IFCB_volume_analyzed(hdrname);
+   runtype=IFCB_runtype(hdrname);
+    
+   load([resultpath filename])
+    temp = zeros(1,numclass);
+    for classnum = 1:numclass1,
+                temp(classnum) = size(find(classlist(:,2) == classnum | (isnan(classlist(:,2)) & classlist(:,3) == classnum)),1);
+    end;
+       
+
+            for classnum = 1:numclass2,
+                temp(classnum+numclass1) = size(find(classlist(:,4) == classnum),1);
+            end;
+       
+        classcount((filecount),:) = temp;
+        
+    end;
+
+clear class2use_manual class2use_auto class2use_sub* classlist
+class2use = class2use_here;
+if ~exist([resultpath 'summary/'], 'dir')
+    mkdir([resultpath 'summary/'])
+end;
+datestr = date; datestr = regexprep(datestr,'-','');
+save([resultpath 'summary/count_manual_' datestr], 'matdate', 'ml_analyzed', 'classcount', 'filelist', 'class2use')
+
+
+% load '\\queenrose\IFCB010_OkeanosExplorerAug2013\data\Manual_fromClass\summary\count_manual_19Jan2014.mat'
+% figure %example
+% classnum = 72; %90 for tintinnid
+% plot(matdate, classcount(:,classnum)./ml_analyzed, '.-')
+% datetick('x')
+% ylabel([class2use{classnum} ' (mL^{-1})'])
+% hold on
+% 
+% load '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\Manual_fromClass\Alt\summary\count_manual_05Feb2014.mat'
+% classnum = 72; %90 for tintinnid
+% plot(matdate, classcount(:,classnum)./ml_analyzed, 'r.-')
+% 
+% legend('IFCB10','IFCB14 stained');
+% % 
+% % load '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\Manual_fromClass\Normal\summary\count_manual_23Jan2014.mat'
+% % classnum = 90; %90 for tintinnid
+% % plot(matdate, classcount(:,classnum)./ml_analyzed, 'k.-')
+% % 
+% % legend('IFCB10','IFCB14 stained ', 'IFCB14 normal');
+% % 
+% 
+% % 
+% % % figure %example
+% % % classnum = 1;
+% % % plot(matdate, classcount(:,classnum)./ml_analyzed, '.-')
+% % % datetick('x')
+% % % ylabel([class2use{classnum} ' (mL^{-1})'])
+
