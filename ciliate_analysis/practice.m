dfdea73f086cc46ba0fb72ecae21d4e7e19f64b3
Emily Brownlee <ebrownlee@whoi.edu>
2014-07-26 12:14:47 +0000

    practice scripts for dock data
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1044 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 8b47225c0cfc415f5bafb08089f691e65248de3a

---
+++ ciliate_analysis/practice.m
@@ -0,0 +1,86 @@
+%%
+
+load '/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/5-11-14//Manual_fromClass/summary/count_manual_12May2014.mat'
+
+runtype_alt_ind=strmatch('ALT', runtype);
+runtype_normal_ind=strmatch('NORMAL',runtype);
+
+Ciliate_mix_classcount_norm=sum(classcount(runtype_normal_ind,72),1);
+Mesodinium_classcount_norm=sum(classcount(runtype_normal_ind,77),1);
+Laboea_classcount_norm=sum(classcount(runtype_normal_ind,75),1);
+Tintinnid_classcount_norm=sum(classcount(runtype_normal_ind,90),1);
+Gyro_classcount_norm=sum(classcount(runtype_normal_ind,36),1);
+Proto_classcount_norm=sum(classcount(runtype_normal_ind,32),1);
+
+Ciliate_mix_classcount_alt=sum(classcount(runtype_alt_ind,72),1);
+Mesodinium_classcount_alt=sum(classcount(runtype_alt_ind,77),1);
+Laboea_classcount_alt=sum(classcount(runtype_alt_ind,75),1);
+Tintinnid_classcount_alt=sum(classcount(runtype_alt_ind,90),1);
+Gyro_classcount_alt=sum(classcount(runtype_alt_ind,36),1);
+Proto_classcount_alt=sum(classcount(runtype_alt_ind,32),1);
+
+normal_ciliate_bin=[Ciliate_mix_classcount_norm Mesodinium_classcount_norm Laboea_classcount_norm Tintinnid_classcount_norm Gyro_classcount_norm Proto_classcount_norm];
+normal_ciliate_ml=sum(ml_analyzed(runtype_normal_ind));
+[normal_ci] = poisson_count_ci(normal_ciliate_bin, 0.95);
+normal_ci_low=normal_ci(:,1);
+normal_ci_upper=normal_ci(:,2);
+
+alt_ciliate_ml=sum(ml_analyzed(runtype_alt_ind));
+alt_ciliate_bin=[Ciliate_mix_classcount_alt Mesodinium_classcount_alt Laboea_classcount_alt Tintinnid_classcount_alt Gyro_classcount_alt Proto_classcount_alt];
+[alt_ci] = poisson_count_ci(alt_ciliate_bin, 0.95);
+alt_ci_low=alt_ci(:,1);
+alt_ci_upper=alt_ci(:,2);
+
+
+b = [normal_ciliate_bin./normal_ciliate_ml; alt_ciliate_bin./alt_ciliate_ml]';
+errdata1 = [b(:,1)'-(normal_ci_low./normal_ciliate_ml)'; (normal_ci_upper./normal_ciliate_ml-b(:,1))']';
+errdata2 = [b(:,2)'-(alt_ci_low./alt_ciliate_ml)'; (alt_ci_upper./alt_ciliate_ml-b(:,2))']'; 
+errdata = [errdata1' errdata2'];
+figure
+h = bar('v6', b);
+set(gca, 'linewidth', 2, 'fontsize', 18,'fontname', 'arial')
+%set(gca, 'xticklabel', class2use','fontsize', 18, 'fontname', 'arial');
+set(gca,'fontsize',18, 'fontname', 'arial')
+set(gca,'XTickLabel',{'Ciliate Mix','Meso','Laboea','Tintinnid','Gyro','Proto','Dactyliosolen','Dictyocha','Dinobryon','Ditylum','Ephemera','Eucampia','Eucampia_groenlandica','Guinardia','Guinardia_flaccida','Leptocylindrus','Pleurosigma','Pseudonitzschia','Rhizosolenia','Skeletonema','Thalassionema','Thalassiosira','Thalassiosira_dirty','bad','ciliate','detritus','pennate','mix','clusterflagellate','crypto','dino10','dino30','Dinophysis','Euglena','flagellate','Gyrodinium','kiteflagellates','Lauderia','Licmophora','Phaeocystis','Prorocentrum','Pyramimonas','roundCell','Stephanopyxis','Tropidoneis','other','Cerataulina','Coscinodiscus','Gonyaulax','Odontella','Guinardia_striata','Paralia','mix_elongated','Hemiaulus','unclassified','Chaetoceros_flagellate','Chaetoceros_pennate','Cerataulina_flagellate','G_delicatula_parasite','G_delicatula_external_parasite','Chaetoceros_other','diatom_flagellate','other_interaction','Chaetoceros_didymus','Leptocylindrus_mediterraneus','Chaetoceros_didymus_flagellate','pennates_on_diatoms','Parvicorbicula_socialis','Delphineis','G_delicatula_detritus','amoeba','Ciliate_mix','Didinium_sp','Euplotes_sp','Laboea_strobila','Leegaardiella_ovalis','Mesodinium_sp','Pleuronema_sp','Strobilidium_morphotype1','Strobilidium_morphotype2','Strombidium_capitatum','Strombidium_caudatum','Strombidium_conicum','Strombidium_inclinatum','Strombidium_morphotype1','Strombidium_morphotype2','Strombidium_oculatum','Strombidium_wulffi','Tiarina_fusus','Tintinnid','Tontonia_appendiculariformis','Tontonia_gracillima'},...
+    'XTick',[1 2 3 4 5 6],...
+    'LineWidth',2,...
+    'FontSize',18,...
+    'FontName','arial');
+
+
+xdata = get(h,'XData');
+sizz = size(b);
+
+%determine the number of bars and groups
+NumGroups = sizz(1);
+SizeGroups = sizz(2);
+NumBars = SizeGroups * NumGroups;
+
+% Use the Indices of Non Zero Y values to get both X values 
+% for each bar. xb becomes a 2 by NumBars matrix of the X values.
+INZY = [1 3];
+xb = [];
+
+for i = 1:SizeGroups
+for j = 1:NumGroups
+xb = [xb xdata{i}(INZY, j)];
+end
+end
+
+%find the center X value of each bar.
+for i = 1:NumBars
+centerX(i) = (xb(1,i) + xb(2,i))/2;
+end
+
+% To place the error bars - use the following:
+hold on;
+%eh = errorbar(centerX,b,errdata); If you are using MATLAB 6.5 (R13)
+%eh = errorbar('v6',centerX,b,errdata);
+eh = errorbar('v6',centerX,b,errdata(1,:), errdata(2,:));
+
+set(eh(1),'linewidth',2); % This changes the thickness of the errorbars
+set(eh(1),'color','k'); % This changes the color of the errorbars
+set(eh(2),'linestyle','none'); % This removes the connecting line
+ylabel('Cell concentration (mL^{-1})','fontsize', 18, 'fontname', 'arial');
+lh = legend('Normal Settings', 'Alt Settings');
+set(lh, 'box', 'off')
