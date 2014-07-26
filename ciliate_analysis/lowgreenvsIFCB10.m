7b652ab1f9d0254bb5e82c8ae7dbdff608365cd6
Emily Brownlee <ebrownlee@whoi.edu>
2014-07-26 11:54:46 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@996 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 35d8e3378b3cf4798b8ae3d62c0ed4e0852db94e

--- ciliate_analysis/lowgreenvsIFCB10.m
+++ ciliate_analysis/lowgreenvsIFCB10.m
@@ -3,7 +3,7 @@ load '/Volumes/IFCB014_OkeanosExplorerAug2013/data/Manual_fromClass/Alt/summary/
 
 load '/Volumes/IFCB010_OkeanosExplorerAug2013/data/Manual_fromClass/summary/count_manual_19Jan2014.mat'
 
-min_diff= 1/48;
+min_diff= 1/24;
 match_ind=NaN(size(matdate_lowgreen));
 
 
@@ -31,28 +31,48 @@ total_ciliate_classcount=mean(ciliate_classcount,2);
 low_green_ciliate_classcount=classcount_lowgreen(:, 72:92);
 total_low_green_ciliate_classcount=mean(low_green_ciliate_classcount,2);
 
-count1=total_low_green_ciliate_classcount(1:31)./ml_analyzed(1:31);
-count2=total_low_green_ciliate_classcount(36:46)./ml_analyzed(36:46);
-low_green_count_total= [count1; count2];
-low_green_count_total_day=total_low_green_ciliate_classcount(2:17)./ml_analyzed(2:17);
 
-count_total=ciliate_classcount(match_ind)./ml_analyzed(match_ind);
-count_total_day=ciliate_classcount(match_ind(2:17))./ml_analyzed(match_ind(2:17));
+
+nind = find(~isnan(match_ind)); %finds when there are no NaNs
 
 figure
-plot(mean(count_total_day),mean(low_green_count_total_day), '*');
+plot(matdate_lowgreen(nind), classcount_lowgreen(nind,72)./ml_analyzed_lowgreen(nind), '.-')
+hold on
+plot(matdate(match_ind(nind)), classcount(match_ind(nind),72)./ml_analyzed(match_ind(nind)), 'r.-')
+legend('low green', 'IFCB 10')
+
+figure
+plot(classcount_lowgreen(nind,72)./ml_analyzed_lowgreen(nind), classcount(match_ind(nind),72)./ml_analyzed(match_ind(nind)), '.')
 
 
+[low_green_mdate_mat, low_green_mat, yearlist, yd ] = timeseries2ydmat(matdate_lowgreen(nind), classcount_lowgreen(nind,72)./ml_analyzed_lowgreen(nind));
+[mdate_mat, y_mat, yearlist, yd ] = timeseries2ydmat(matdate(match_ind(nind)), classcount(match_ind(nind),72)./ml_analyzed(match_ind(nind)));
 
-    
+%y_mat 236:240
+
+figure
+plot(yd(236:240), low_green_mat(236:240), '.-')
+hold on
+plot(yd(236:240), y_mat(236:240), 'r.-')
+legend('low green', 'IFCB 10')
+
+figure
+plot(low_green_mat(236:240), y_mat(236:240), '.');
 
 
+% nind = find(isnan(match_ind)); %finds when there are NaNs
+% match_ind(nind) = [];
+% c2 = classcount(match_ind,72)./ml_analyzed(match_ind);
+% c1 = classcount_lowgreen(:,72)./ml_analyzed_lowgreen;
+% c1(nind) = [];
 
-
-
-
-
-
-
-
-
+% count1=total_low_green_ciliate_classcount(1:31)./ml_analyzed(1:31);
+% count2=total_low_green_ciliate_classcount(36:46)./ml_analyzed(36:46);
+% low_green_count_total= [count1; count2];
+% low_green_count_total_day=total_low_green_ciliate_classcount(2:17)./ml_analyzed(2:17);
+% 
+% count_total=ciliate_classcount(match_ind)./ml_analyzed(match_ind);
+% count_total_day=ciliate_classcount(match_ind(2:17))./ml_analyzed(match_ind(2:17));
+% 
+% figure
+% plot(mean(count_total_day),mean(low_green_count_total_day), '*');
