e51f8ce336cec40cdf473f825d3d9ffdbeca4d3b
Emily Brownlee <ebrownlee@whoi.edu>
2014-07-26 12:17:59 +0000

    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1060 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    Former-commit-id: 5997bc7ee8c668dc4dcd9ceced87e814967fbc99

--- ciliate_analysis/working_doc3.m
+++ ciliate_analysis/working_doc3.m
@@ -1,20 +1,20 @@
 
 
-load '\\queenrose\IFCB010_OkeanosExplorerAug2013\data\Manual_fromClass\summary\count_manual_19Jan2014.mat'
+load '/Volumes/IFCB010_OkeanosExplorerAug2013/data/Manual_fromClass/summary/count_manual_19Jan2014.mat'
 ciliate_classcount_IFCB10=classcount(9:73,72:92);
 ciliate_classcount_IFCB10_total=sum(ciliate_classcount_IFCB10,2);
 ciliate_bin_perml_IFCB10=sum(ciliate_classcount_IFCB10_total)/sum(ml_analyzed(9:73));
 [IFCB10_ci] = poisson_count_ci(sum(ciliate_classcount_IFCB10_total), 0.95);
 IFCB10_ci_ml=IFCB10_ci/sum(ml_analyzed(9:73));
 
-load '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\Manual_fromClass\Alt\summary\count_manual_highhigh2_green05Feb2014.mat'
+load '/Volumes/IFCB014_OkeanosExplorerAug2013/data/Manual_fromClass/summary_old/alt_summary_old/count_manual_highhigh2_green05Feb2014.mat'
 ciliate_classcount_high_green=classcount(2:16,72:92);
 ciliate_classcount_high_green_total=sum(ciliate_classcount_high_green,2);
 ciliate_bin_perml_high_green=sum(ciliate_classcount_high_green_total)/sum(ml_analyzed(2:16));
 [high_green_ci] = poisson_count_ci(sum(ciliate_classcount_high_green_total), 0.95);
 high_green_ci_ml=high_green_ci/sum(ml_analyzed(2:16));
 
-load '\\queenrose\IFCB014_OkeanosExplorerAug2013\data\Manual_fromClass\Alt\summary\count_manual_lowlow2_green05Feb2014.mat'
+load '/Volumes/IFCB014_OkeanosExplorerAug2013/data/Manual_fromClass/summary_old/alt_summary_old/count_manual_lowlow2_green05Feb2014.mat'
 ciliate_classcount_low_green=classcount(2:16,72:92);
 ciliate_classcount_low_green_total=sum(ciliate_classcount_low_green,2);
 ciliate_bin_perml_low_green=sum(ciliate_classcount_low_green_total)/sum(ml_analyzed(2:16));
@@ -40,18 +40,25 @@ plot(xaxis_2, points, '.k', 'markersize', 0.1);
 errorbar(xaxis_2, points, lower, upper, 'r', 'Marker', 'none', 'LineStyle', 'none', 'Linewidth', 1 , 'color' ,[0 0 0]);
 set(bar1(1),'FaceColor',[0 0 1]);
 set(bar1(2),'FaceColor',[0 1 0]);
-ylabel('Ciliates (mL^{-1})', 'fontsize', 12, 'fontname', 'arial');
-set(gca,'xticklabel',{'IFCB 10','IFCB 14'}, 'fontsize', 12, 'fontname', 'arial');
+ylabel('Ciliate abundance (cell mL^{-1})\bf', 'fontsize',24, 'fontname', 'Times New Roman');
+set(gca,'xticklabel',{'Traditional IFCB','Staining IFCB'}, 'fontsize', 24, 'fontname', 'Times New Roman');
 axis square
 
+%%
 
-
-
-Lysotracker_analysis
+load '/Volumes/IFCB014_OkeanosExplorerAug2013/data/continuous/Manual_fromClass/D20130825T161009_IFCB014.mat'
+adcdata=load ('/Volumes/IFCB014_OkeanosExplorerAug2013/data/continuous/D20130825T161009_IFCB014.adc');
+% use IFCB 14  D20130825T161009_IFCB014
+%Lysotracker_analysis
 %refline(0.020, 0.0033)
 chl = adcdata(:,5); green = adcdata(:,4);
 low_green_ind = find(green < ((chl-0.0033)/0.020));%low green
 high_green_ind = find(green >= ((chl-0.0033)/0.020));
+ciliate_roi_ind=find(~isnan(classlist(:,4)));
+
+ciliate_high_green=find(green >= ((chl-0.0033)/0.020) & ~isnan(classlist(:,4)));
+
+
 
 
 figure1 = figure;
@@ -68,25 +75,50 @@ hold on
 
 m = 0.020; b = 0.0033;  x = 0.0001:0.01:2;
 %plot((adcdata(:,4)),(adcdata(:,5)), '*', 'markersize', 1);
-plot(green(low_green_ind), chl(low_green_ind), '*','markersize', 1.5)
-plot(green(high_green_ind), chl(high_green_ind), 'g*','markersize', 1.5)
+plot(green(low_green_ind), chl(low_green_ind), '*','markersize', 2)
+plot(green(high_green_ind), chl(high_green_ind), 'g*','markersize', 4)
 hold on
 plot(x, m*x+b, '-k');
 xlim([0.0016 0.3])
 ylim([0.0025 0.11])
 axis square
-ylabel('Chlorophyll fluorescence', 'fontsize', 14, 'fontname', 'arial');
-xlabel('FDA fluorescence','fontsize', 14, 'fontname', 'arial');
-set(gca, 'FontSize',14);
+ylabel('Chlorophyll fluorescence', 'fontsize', 24, 'fontname', 'arial');
+xlabel('FDA fluorescence','fontsize', 24, 'fontname', 'arial');
+set(gca, 'FontSize',24);
 
+figure2 = figure;
 
- figure
-plot((adcdata(:,4)),(adcdata(:,5)), '*', 'markersize', 1);
+% Create axes
+axes1 = axes('Parent',figure2,'YScale','log','YMinorTick','on',...
+    'XScale','log',...
+    'XMinorTick','on',...
+    'PlotBoxAspectRatio',[1 1 1],...
+    'FontSize',14);
+box(axes1,'on');
+hold(axes1,'all');
 hold on
-m = 0.03; b = 0.0033;  x = 0.0001:0.01:0.2;
-plot(x, m*x+b, '-r');
-xlim([0 0.2]);
-ylim([0 0.03]);
+
+m = 0.020; b = 0.0033;  x = 0.0001:0.01:2;
+plot((adcdata(:,4)),(adcdata(:,5)), '.', 'markersize',10);
+hold on
+%plot(green(low_green_ind), chl(low_green_ind), '*','markersize', 2)
+plot(green(ciliate_high_green), chl(ciliate_high_green), 'g.','markersize', 30)
+plot(green(high_green_ind(7:11)), chl(high_green_ind(7:11)), 'g.','markersize', 30)
+hold on
+plot(x, m*x+b, '-k');
+xlim([0.0016 0.3])
+ylim([0.0025 0.11])
+axis square
+ylabel('Chlorophyll fluorescence', 'fontsize', 24, 'fontname', 'arial');
+xlabel('FDA fluorescence','fontsize', 24, 'fontname', 'arial');
+set(gca, 'FontSize',24);
+%  figure
+% plot((adcdata(:,4)),(adcdata(:,5)), '*', 'markersize', 1);
+% hold on
+% m = 0.03; b = 0.0033;  x = 0.0001:0.01:0.2;
+% plot(x, m*x+b, '-r');
+% xlim([0 0.2]);
+% ylim([0 0.03]);
 
 
 %refline(0.03, .0033)
