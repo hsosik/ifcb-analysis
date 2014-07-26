9d96cd3db241807a2510bdb646cd602531910503
Emily Brownlee <ebrownlee@whoi.edu>
2014-07-26 12:12:29 +0000

    modifying data to only use peak signals above a certain threshold
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1030 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 0028e0f6082408c30f7c067a959c92268ba058e8

---
+++ ciliate_analysis/peak_signals_PE_July.m
@@ -0,0 +1,108 @@
+
+% D20140702T133113_IFCB014.mat %nonstained
+% D20140702T142456_IFCB014.mat %nonstained
+% D20140702T151700_IFCB014.mat %nonstained
+% D20140702T161507_IFCB014.mat %nonstained
+% D20140702T170505_IFCB014.mat %nonstained
+% D20140702T181021_IFCB014.mat %nonstained
+
+load '/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/Manual_fromClass/D20140702T133113_IFCB014.mat'
+IFCB_14_adcdata=load('/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/D20140702T133113_IFCB014.adc');
+
+ciliate_sensitivity_ind=find(classlist(:,4)==1 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+mesodinium_sensitivity_ind=find(classlist(:,4)==6 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+tintinnid_sensitivity_ind=find(classlist(:,4)==19 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+laboea_sensitivity_ind=find(classlist(:,4)==4 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+proto_sensitivity_ind=find(classlist(:,2)==31 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+gyro_sensitivity_ind=find(classlist(:,2)==36 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+
+load '/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/Manual_fromClass/D20140702T142456_IFCB014.mat'
+IFCB_14_adcdata=load('/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/D20140702T142456_IFCB014.adc');
+
+ciliate_sensitivity_ind_2=find(classlist(:,4)==1 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+mesodinium_sensitivity_ind_2=find(classlist(:,4)==6 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+tintinnid_sensitivity_ind_2=find(classlist(:,4)==19 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+laboea_sensitivity_ind_2=find(classlist(:,4)==4 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+proto_sensitivity_ind_2=find(classlist(:,2)==31 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+gyro_sensitivity_ind_2=find(classlist(:,2)==36 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+
+ciliate_sensitivity_ind=[ciliate_sensitivity_ind_2; ciliate_sensitivity_ind];
+mesodinium_sensitivity_ind=[mesodinium_sensitivity_ind_2; mesodinium_sensitivity_ind];
+tintinnid_sensitivity_ind=[tintinnid_sensitivity_ind_2; tintinnid_sensitivity_ind];
+laboea_sensitivity_ind=[laboea_sensitivity_ind_2; laboea_sensitivity_ind];
+proto_sensitivity_ind=[proto_sensitivity_ind_2; proto_sensitivity_ind];
+gyro_sensitivity_ind=[gyro_sensitivity_ind_2; gyro_sensitivity_ind];
+
+load '/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/Manual_fromClass/D20140702T151700_IFCB014.mat'
+IFCB_14_adcdata=load('/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/D20140702T151700_IFCB014.adc');
+
+ciliate_sensitivity_ind_2=find(classlist(:,4)==1 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+mesodinium_sensitivity_ind_2=find(classlist(:,4)==6 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+tintinnid_sensitivity_ind_2=find(classlist(:,4)==19 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+laboea_sensitivity_ind_2=find(classlist(:,4)==4 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+proto_sensitivity_ind_2=find(classlist(:,2)==31 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+gyro_sensitivity_ind_2=find(classlist(:,2)==36 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+
+ciliate_sensitivity_ind=[ciliate_sensitivity_ind_2; ciliate_sensitivity_ind];
+mesodinium_sensitivity_ind=[mesodinium_sensitivity_ind_2; mesodinium_sensitivity_ind];
+tintinnid_sensitivity_ind=[tintinnid_sensitivity_ind_2; tintinnid_sensitivity_ind];
+laboea_sensitivity_ind=[laboea_sensitivity_ind_2; laboea_sensitivity_ind];
+proto_sensitivity_ind=[proto_sensitivity_ind_2; proto_sensitivity_ind];
+gyro_sensitivity_ind=[gyro_sensitivity_ind_2; gyro_sensitivity_ind];
+
+
+load '/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/Manual_fromClass/D20140702T161507_IFCB014.mat'
+IFCB_14_adcdata=load('/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/D20140702T161507_IFCB014.adc');
+
+ciliate_sensitivity_ind_2=find(classlist(:,4)==1 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+mesodinium_sensitivity_ind_2=find(classlist(:,4)==6 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+tintinnid_sensitivity_ind_2=find(classlist(:,4)==19 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+laboea_sensitivity_ind_2=find(classlist(:,4)==4 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+proto_sensitivity_ind_2=find(classlist(:,2)==31 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+gyro_sensitivity_ind_2=find(classlist(:,2)==36 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+
+ciliate_sensitivity_ind=[ciliate_sensitivity_ind_2; ciliate_sensitivity_ind];
+mesodinium_sensitivity_ind=[mesodinium_sensitivity_ind_2; mesodinium_sensitivity_ind];
+tintinnid_sensitivity_ind=[tintinnid_sensitivity_ind_2; tintinnid_sensitivity_ind];
+laboea_sensitivity_ind=[laboea_sensitivity_ind_2; laboea_sensitivity_ind];
+proto_sensitivity_ind=[proto_sensitivity_ind_2; proto_sensitivity_ind];
+gyro_sensitivity_ind=[gyro_sensitivity_ind_2; gyro_sensitivity_ind];
+
+load '/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/Manual_fromClass/D20140702T170505_IFCB014.mat'
+IFCB_14_adcdata=load('/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/D20140702T170505_IFCB014.adc');
+
+ciliate_sensitivity_ind_2=find(classlist(:,4)==1 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+mesodinium_sensitivity_ind_2=find(classlist(:,4)==6 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+tintinnid_sensitivity_ind_2=find(classlist(:,4)==19 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+laboea_sensitivity_ind_2=find(classlist(:,4)==4 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+proto_sensitivity_ind_2=find(classlist(:,2)==31 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+gyro_sensitivity_ind_2=find(classlist(:,2)==36 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+
+ciliate_sensitivity_ind=[ciliate_sensitivity_ind_2; ciliate_sensitivity_ind];
+mesodinium_sensitivity_ind=[mesodinium_sensitivity_ind_2; mesodinium_sensitivity_ind];
+tintinnid_sensitivity_ind=[tintinnid_sensitivity_ind_2; tintinnid_sensitivity_ind];
+laboea_sensitivity_ind=[laboea_sensitivity_ind_2; laboea_sensitivity_ind];
+proto_sensitivity_ind=[proto_sensitivity_ind_2; proto_sensitivity_ind];
+gyro_sensitivity_ind=[gyro_sensitivity_ind_2; gyro_sensitivity_ind];
+
+load '/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/Manual_fromClass/D20140702T181021_IFCB014.mat'
+IFCB_14_adcdata=load('/Users/markmiller/Documents/Experiments/IFCB_14/Microscopy_vs_Imager/7-2-14/D20140702T181021_IFCB014.adc');
+
+ciliate_sensitivity_ind_2=find(classlist(:,4)==1 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+mesodinium_sensitivity_ind_2=find(classlist(:,4)==6 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+tintinnid_sensitivity_ind_2=find(classlist(:,4)==19 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+laboea_sensitivity_ind_2=find(classlist(:,4)==4 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+proto_sensitivity_ind_2=find(classlist(:,2)==31 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+gyro_sensitivity_ind_2=find(classlist(:,2)==36 & IFCB_14_adcdata(:,9)> 0.012 & IFCB_14_adcdata(:,8)> 0.012);
+
+ciliate_sensitivity_ind=[ciliate_sensitivity_ind_2; ciliate_sensitivity_ind];
+mesodinium_sensitivity_ind=[mesodinium_sensitivity_ind_2; mesodinium_sensitivity_ind];
+tintinnid_sensitivity_ind=[tintinnid_sensitivity_ind_2; tintinnid_sensitivity_ind];
+laboea_sensitivity_ind=[laboea_sensitivity_ind_2; laboea_sensitivity_ind];
+proto_sensitivity_ind=[proto_sensitivity_ind_2; proto_sensitivity_ind];
+gyro_sensitivity_ind=[gyro_sensitivity_ind_2; gyro_sensitivity_ind];
+
+
+
+
+
