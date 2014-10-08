c6265b8337815c611436390b75ce1d66a62758e4
Heidi Sosik <hsosik@whoi.edu>
2014-10-08 19:13:15 +0000

    revise for updated class labels
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1106 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 46fbbd88d4ad8bfedf5f114c78c594112ae1d817

--- analyze_by_class/get_G_delicatula_ind.m
+++ analyze_by_class/get_G_delicatula_ind.m
@@ -3,7 +3,7 @@ function [ ind_out, class_label ] = get_G_delicatula_ind( class2use, class_label
 % MVCO class list specific to return of indices that correspond to all variants of Guinardia delicatula
 %  Heidi M. Sosik, Woods Hole Oceanographic Institution, March 2013
 
-class2get = {'Guinardia' 'G_delicatula_parasite' 'G_delicatula_external_parasite'};
+class2get = {'Guinardia_delicatula' 'G_delicatula_parasite' 'G_delicatula_external_parasite'};
 
 [~,ind_out] = intersect(class2use, class2get);
 ind_out = sort(ind_out);
