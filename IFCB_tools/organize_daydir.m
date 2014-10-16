dfb214776eb7addf56b48e0644d6920f4bbc3b10
Heidi Sosik <hsosik@whoi.edu>
2014-10-16 18:53:14 +0000

    convert to function and generalize to work for IFCB* or D* file types
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1110 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 6f3312feb1dc995357051dedc58b3791b8514cc4

--- IFCB_tools/organize_daydir.m
+++ IFCB_tools/organize_daydir.m
@@ -1,31 +1,40 @@
+function [ ] = organize_daydir( inpath, outpath )
+%function [ ] = organize_daydir( inpath, outpath )
+%   Move IFCB data files from location specified by 'inpath' into daily
+%   sub-directories created (if needed) under 'outpath'
+%   If only one path is specified, outpath = inpath
+%example: organize_daydir( '\\sosiknas1\IFCB_data\IFCB2_C211A_SEA2007\data\', '\\sosiknas1\IFCB_data\IFCB2_C211A_SEA2007\data\' )
+%equivalent to organize_daydir( '\\sosiknas1\IFCB_data\IFCB2_C211A_SEA2007\data\' )
 
-basepath ='/Volumes/IFCB_data/IFCB14_Dock/data/';
-filelist = dir([basepath 'D*.roi']);
+if ~exist('outpath', 'var'),
+    outpath = inpath;
+end;
 
-c=struct2cell(filelist)';
-filenames=c(:,1);
+filelist = dir(fullfile(inpath, '*.roi'));
+if isempty(filelist),
+    disp('no roi files found')
+    return
+end;
+filenames={filelist.name}';
 
-filename_char=char(filenames);
-day_filenames=filename_char(:,1:9);
-filenames_unique= unique(day_filenames,'rows');
+if strmatch('I', filenames{1}(1)),
+    dirstrlength = 14;
+elseif strmatch('D', filenames{1}(1)),
+    dirstrlength = 9;
+else
+    disp('no IFCB* or D* files detected')
+    return
+end;
 
+filename_char = char(filenames);
+day_filenames = filename_char(:,1:dirstrlength);
+filenames_unique = unique(day_filenames,'rows');
 
-dir_path ='/Volumes/IFCB_data/IFCB14_Dock/dir_practice/';
-cd(dir_path)
-for i=1:size(filenames_unique,1);
-    filename=[filenames_unique(i,:)];
-    if ~exist(fullfile(filename),'dir')
-    mkdir(fullfile(filename));
+for ii = 1:size(filenames_unique,1);
+    dirname = fullfile(outpath,filenames_unique(ii,:));
+    if ~exist(dirname,'dir')
+        mkdir(dirname);
     end
+    source_str = [fullfile(inpath, filenames_unique(ii,:)) '*'];
+    movefile(source_str, dirname)
 end
-
-%data_source='/Volumes/IFCB_data/IFCB14_Dock/data/';
-for i=1:size(filenames_unique,1)
-    %for i=2:size(filenames_unique,1);
-    filename=[filenames_unique(i,:)];
-    source_file=fullfile([basepath filename]);
-    if ~isempty(strmatch(filename,filename_char));
-    movefile([source_file '*'], fullfile([dir_path filename]))
-    end
-end
-
