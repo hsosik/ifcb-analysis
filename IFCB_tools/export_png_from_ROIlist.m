5d0d7f42d2cfce9669f90a4e43e9aad634e01753
Heidi Sosik <hsosik@whoi.edu>
2014-10-29 18:12:17 +0000

    move ROInumbers to last input argument so it can be left off
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1116 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: ff6d82b10dbe9108720adfd3d26731eafa5df47c

--- IFCB_tools/export_png_from_ROIlist.m
+++ IFCB_tools/export_png_from_ROIlist.m
@@ -1,5 +1,5 @@
-function [  ] = export_png_from_ROIlist( ROIfile_withpath, ROInumbers, outputpath)
-%function [  ] = export_png_from_ROIlist( ROIfile_withpath, ROInumbers, outputpath )
+function [  ] = export_png_from_ROIlist( ROIfile_withpath, outputpath, ROInumbers )
+%function [  ] = export_png_from_ROIlist( ROIfile_withpath, outputpath, ROInumbers )
 %save png files to disk from ROI file, if no ROInumbers passed in, then all are exported
 %Heidi M. Sosik, Woods Hole Oceanographic Institution, March 2014
 
@@ -22,7 +22,7 @@ end;
 if ~exist('ROInumbers', 'var'),
     [ROInumbers] = find(x>0);
 end;
-fid=fopen([basedir filesep ROIfile]);% '.roi']);
+fid=fopen(ROIfile_withpath);% '.roi']);
 for count = 1:length(ROInumbers),
     num = ROInumbers(count);
     fseek(fid, startbyte(num), -1);
@@ -30,7 +30,7 @@ for count = 1:length(ROInumbers),
     img = reshape(img, x(num), y(num));
     pngname = [filename '_' num2str(num,'%05.0f') '.png'];
     if length(img) > 0,
-        imwrite(uint8(img'), [outputpath filesep pngname], 'png');
+        imwrite(uint8(img'), fullfile(outputpath, pngname));
     end;
 end;
 fclose(fid);
