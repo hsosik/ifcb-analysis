c0e0831499c209e3bd3f94e1cef6e09e4d5986ad
Heidi Sosik <hsosik@whoi.edu>
2014-11-14 13:16:52 +0000

    fix bug associated with not clearing temporary merged feature structure between files
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1119 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: d2ec13ffb1738095b5d55de2321c9cf6c5a75bae

--- feature_extraction/batch_features_bins/start_fileset_fromROI.m
+++ feature_extraction/batch_features_bins/start_fileset_fromROI.m
@@ -1,6 +1,7 @@
-in_dir = 'C:\IFCB\D20140811_IFCB103\'; %USER
-in_dir_blob = 'C:\IFCB\blobs\'; %USER
-out_dir = 'C:\IFCB\features\'; %USER
+
+in_dir = '\\QUEENROSE\IFCB14_Dock\ditylum\Ben_data\';%USER 
+in_dir_blob = '\\QUEENROSE\IFCB14_Dock\ditylum\Ben_blobs\'; %USER main blob output location
+out_dir = '\\QUEENROSE\IFCB14_Dock\ditylum\Ben_features\'; %USER
 
 bins = dir([in_dir '*.adc']);
 bins = regexprep({bins.name}', '.adc', '');
@@ -22,16 +23,12 @@ for bincount = 1:length(bins),
         mkdir(png_path)
         mkdir(png_path2)
     end;
-%    imglist = dir([in_dir bins{bincount} filesep '*.png']);
-%    imglist = {imglist.name}';
     [targets, imglist] = get_images_fromROI([in_dir bins{bincount} '.roi']);
     targets_blob = get_blob_bin_file([in_dir_blob  bins{bincount} '.zip']);
 
     for i = 1:length(imglist),
         target = empty_target;
         % get the image
-        %target.image = imread([in_dir bins{bincount} filesep imglist{i}]);
-        %target.blob_image = imread([in_dir_blob bins{bincount} '_blobs_v2' filesep imglist{i}]);
         target.image = cell2mat(targets.image(i));
         target.blob_image = cell2mat(targets_blob.image(i));
         target = blob_geomprop(target); 
@@ -50,23 +47,21 @@ for bincount = 1:length(bins),
     end
     
     [ feature_mat, featitles, multiblob_features, multiblob_titles ] = make_feature_matrices(temp);
+    clear temp
     
     fileout = [bins{bincount} '_fea_v2.csv'];
     %write the compiled feature csv file
-    %fileout = regexprep(file, '.zip', '_fea_v2.csv');
     
     ds = dataset([feature_mat featitles]);
     export(ds, 'file', [out_dir fileout], 'delimiter', ',');
     
     %write the raw multi-blob features to separate csv file
-    %fileout = regexprep(file, '.zip', '_multiblob_v2.csv');
     fileout = [bins{bincount} '_multiblob_v2.csv'];
     if ~isempty(multiblob_features), 
         ds = dataset([multiblob_features multiblob_titles]);
         export(ds, 'file', [out_dir 'multiblob' filesep fileout], 'delimiter', ',');       
     end; 
-    
-    
+      
 end
 
 
