d8fe5541d0d95e76613c59bf865044fb387ad737
Heidi Sosik <hsosik@whoi.edu>
2014-12-01 17:42:35 +0000

    revise to get file list from feature directory
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1123 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 38107965145d7b7e42440c43f9073db122d5b672

--- classification/batch_classification/start_batch_classify.m
+++ classification/batch_classification/start_batch_classify.m
@@ -1,22 +1,27 @@
 classifierName = 'MVCO_trees_25Jun2012';
-yr = 2006;
-out_dir = ['\\queenrose\g_work_ifcb1\ifcb_data_mvco_jun06\class' num2str(yr) '_v1\'];
-in_dir = 'http://ifcb-data.whoi.edu/mvco/';
+yr = 2014;
+out_dir = ['\\SosikNAS1\IFCB_products\MVCO\class\class' num2str(yr) '_v1\'];%
+%in_dir = 'http://ifcb-data.whoi.edu/mvco/';
 %for V2 web services, set fea_dir = in_dir;
-fea_dir = 'C:\work\IFCB\ifcb_data_MVCO_jun06\features2006_v2\';
+fea_dir = '\\SosikNAS1\IFCB_products\MVCO\features\features2014_v2\';
 filelist = [];
-for day = 157:157,
-    filelist = [filelist list_day(datestr(datenum(yr,0,day),29), in_dir)];
-end;
-filelist = regexprep(filelist, in_dir, '')';
+%for day = 157:157,
+%    filelist = [filelist list_day(datestr(datenum(yr,0,day),29), in_dir)];
+%end;
+disp('Checking for files to run')
+filelist = dir([fea_dir '*.csv']);
+filelist = {filelist.name}';
+filelist = regexprep(filelist, '_fea_v2.csv', '')';
+%filelist = regexprep(filelist, in_dir, '')';
 files_done = dir([out_dir 'IFCB*class_v1.mat']);
 files_done = char(files_done.name);
 files_done = cellstr(files_done(:,1:end-13));
 filelist2 = setdiff(filelist, files_done);
-if isequal(in_dir, fea_dir),
-    filelist2 = strcat(filelist2,'_features.csv');
-else
-    filelist2 = strcat(filelist2,'_fea_v2.csv');  %USER specify v1 or v2 features as appropriate
-end;
+%if isequal(in_dir, fea_dir),
+%    filelist2 = strcat(filelist2,'_features.csv');
+%else
+filelist2 = strcat(filelist2,'_fea_v2.csv');  %USER specify v1 or v2 features as appropriate
+%end;
 disp(['processing ' num2str(length(filelist2)) ' files'])
 batch_classify( fea_dir, filelist2, out_dir, classifierName );
+
