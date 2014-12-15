7dcaa419061dd4e63635d6da995e452a5e758a4d
Emily Peacock <epeacock@whoi.edu>
2014-12-15 16:03:13 +0000

    Modified to accommodate the data file organization on sosiknas1
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1127 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 1db71688665c1686a3c4a34461a5b3543fe0494b

--- IFCB_tools/resolve_MVCOfiles.m
+++ IFCB_tools/resolve_MVCOfiles.m
@@ -9,16 +9,23 @@ if isstruct(filelist),
     filelist = regexprep(filelist, ext, ''); %strip off extension
 end;
 %   classpath = ['\\queenrose\g_work_IFCB1\ifcb_data_mvco_jun06\classxxxx_24may07_' class_filestr 'revDec11\']; 
-classpath = ['\\queenrose\g_work_IFCB1\ifcb_data_mvco_jun06\classxxxx_v1\'];  %case for new results with TBclassifier
-basedir_all = {'\\demi\vol1'; '\\demi\vol2'; '\\demi\vol3'};
+%classpath = ['\\queenrose\g_work_IFCB1\ifcb_data_mvco_jun06\classxxxx_v1\'];  %case for new results with TBclassifier
+classpath = ['\\sosiknas1\IFCB_products\MVCO\class\classxxxx_v1\'];  %case for new results with TBclassifier
+basedir = ['\\sosiknas1\IFCB_data\MVCO\data\xxxx']
+%basedir_all = {'\\demi\vol1'; '\\demi\vol2'; '\\demi\vol3'};
 stitchpath = '\\queenrose\ifcb_data_mvco_jun06\stitchxxxx\';  %%USER set, roi stitch info files
 
 matdate = IFCB_file2date(filelist);
 [year,~,~] = datevec(matdate);
-basedir_ind = ones(size(year))*2; %default to vol2 
-basedir_ind(year >= 2012) = 1;
-basedir_ind(year <= 2009) = 3;
-basedir = basedir_all(basedir_ind);
+% basedir_ind = ones(size(year))*2; %default to vol2 
+% basedir_ind(year >= 2012) = 1;
+% basedir_ind(year <= 2009) = 3;
+% basedir = basedir_all(basedir_ind);
+
+ii = findstr(basedir(1,:), 'xxxx'); %added by EP to accomodate SOSIKNAS file structure 12-15-2014
+basedir = repmat(basedir, length(year),1); %added by EP to accomodate SOSIKNAS file structure 12-15-2014
+basedir(:,ii:ii+3) = num2str(year);%added by EP to accomodate SOSIKNAS file structure 12-15-2014
+
 files = char(filelist);
 sep = repmat('\',length(year),1);
 basedir = [char(basedir) sep files(:,1:14) sep];
