9b3fdf04b1edcd46fc5ca41279835f24f8c71775
Emily Peacock <epeacock@whoi.edu>
2015-02-13 14:56:51 +0000

    added categories 'Parvicorbicula_socialis' 'Chrysochromulina' 'Pyramimonas_longicauda' 'flagellate_sp1' 'flagellate_sp2' 'flagellate_sp3'
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1145 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: e7f1c5c6452a170bc59f116a5fccf33b4ebb65ca

--- analyze_by_class/get_mix_ind.m
+++ analyze_by_class/get_mix_ind.m
@@ -2,7 +2,8 @@ function [ ind_out, class_label ] = get_mix_ind( class2use, class_label )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-class2get = {'mix' 'clusterflagellate' 'crypto' 'Euglena' 'flagellate' 'kiteflagellates' 'Phaeocystis' 'Pyramimonas' 'roundCell' 'mix_elongated'};
+class2get = {'mix' 'clusterflagellate' 'crypto' 'Euglena' 'flagellate' 'kiteflagellates' 'Phaeocystis' 'Pyramimonas' 'roundCell' 'mix_elongated'...
+    'Parvicorbicula_socialis' 'Chrysochromulina' 'Pyramimonas_longicauda' 'flagellate_sp1' 'flagellate_sp2' 'flagellate_sp3'};
 
 [~,ind_out] = intersect(class2use, class2get);
 ind_out = sort(ind_out);
