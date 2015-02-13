735cb634e9fd7ed1cc2223d73aeb5ee23d13d756
Emily Peacock <epeacock@whoi.edu>
2015-02-13 14:18:23 +0000

    I added the new dino classes from class2use_MVCOmanual5.mat
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1142 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 7486799d67d89b2c4bf05dfe960feb19bd452d2e

--- analyze_by_class/get_dino_ind.m
+++ analyze_by_class/get_dino_ind.m
@@ -2,7 +2,8 @@ function [ ind_out, class_label ] = get_dino_ind( class2use, class_label )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-class2get = {'Ceratium' 'dino30' 'Dinophysis' 'Gyrodinium' 'Prorocentrum' 'Gonyaulax'};
+class2get = {'Ceratium' 'dino30' 'Dinophysis' 'Gyrodinium' 'Prorocentrum' 'Gonyaulax' 'Akashiwo' 'Amphidinium_sp' 'Cochlodinium' 'Heterocapsa_rotundata'...
+    'Heterocapsa_triquetra' 'Karenia' 'Katodinium+Torodinium' 'Protoperidinium' 'Proterythropsis_sp' 'dino_large1'};
 
 [~,ind_out] = intersect(class2use, class2get);
 ind_out = sort(ind_out);
