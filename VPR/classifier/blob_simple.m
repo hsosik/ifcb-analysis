c1e2723d3a600bceb7651962d92d475588b7954a
Heidi Sosik <hsosik@whoi.edu>
2015-03-03 13:33:21 +0000

    tweak morphological processing for VPR case
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1159 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 068a74046443038b80b21da4d5072072fe8f5130

--- VPR/classifier/blob_simple.m
+++ VPR/classifier/blob_simple.m
@@ -42,17 +42,17 @@ config = target.config;
 %%%img_blob = imerode(img_blob, seD);
 
 img = target.image;
-pc3 = config.pc3; %get phase congruency parameters
-[M m , ~, ~, ~, ~, ~] = phasecong3(img, pc3.nscale, pc3.norient, pc3.minWaveLength, pc3.mult, pc3.sigmaOnf, pc3.k, pc3.cutOff, pc3.g, pc3.noiseMethod);
-config.hysthresh.high = .3;
-config.hysthresh.low = .18;
-img_blob = hysthresh(M+m, config.hysthresh.high, config.hysthresh.low);
-% omit spurious edges along margins
-img_blob(1,img_blob(2,:)==0)=0;
-img_blob(end,img_blob(end-1,:)==0)=0;
-img_blob(img_blob(:,2)==0,1)=0;
-img_blob(img_blob(:,end-1)==0,end)=0;
-img_edge = img_blob; %keep this to plot?
+% pc3 = config.pc3; %get phase congruency parameters
+% [M m , ~, ~, ~, ~, ~] = phasecong3(img, pc3.nscale, pc3.norient, pc3.minWaveLength, pc3.mult, pc3.sigmaOnf, pc3.k, pc3.cutOff, pc3.g, pc3.noiseMethod);
+% config.hysthresh.high = .3;
+% config.hysthresh.low = .18;
+% img_blob = hysthresh(M+m, config.hysthresh.high, config.hysthresh.low);
+% % omit spurious edges along margins
+% img_blob(1,img_blob(2,:)==0)=0;
+% img_blob(end,img_blob(end-1,:)==0)=0;
+% img_blob(img_blob(:,2)==0,1)=0;
+% img_blob(img_blob(:,end-1)==0,end)=0;
+% img_edge = img_blob; %keep this to plot?
 
 % now use kmean clustering approach to make sure dark areas are included
 img_dark = kmean_segment2(img);
@@ -60,16 +60,19 @@ img_dark = kmean_segment2(img);
 img_blob = img_dark;
 %keyboard
 
-img_blob = imclose(img_blob, seD);
-img_blob = imdilate(img_blob, seD);
-img_blob = bwmorph(img_blob, 'thin', 2); %20 oct 2011, Heidi thinks 3 times here might be better than previous 1
+%img_blob = imclose(img_blob, seD); img_blob_close = img_blob;
+%img_blob = imdilate(img_blob, seD); img_blob_dilate = img_blob;
+img_blob = imclose(img_blob, se3); img_blob_close = img_blob;
+img_blob = imdilate(img_blob, se2); img_blob_dilate = img_blob;
+
+img_blob = bwmorph(img_blob, 'thin', 3); %20 oct 2011, Heidi thinks 3 times here might be better than previous 1
 %img_blob = imfill(img_blob, 'holes');
 
 %%img_blob = imdilate(img_blob, [se90 se0]);
 %%img_blob = imfill(img_blob, 'holes');
 %%img_blob = imclearborder(img_blob, 4);
 %%img_blob = imerode(img_blob, seD);
-%%img_blob = imerode(img_blob, seD);
+img_blob = imerode(img_blob, seD);
 
 
 %% morphological processing 
@@ -77,18 +80,18 @@ img_blob = bwmorph(img_blob, 'thin', 2); %20 oct 2011, Heidi thinks 3 times here
 %img_blob = blob_s;
 %%img_blob = imdilate(img_blob, se2);
 %%img_blob = bwmorph(img_blob, 'thin', 3); %20 oct 2011, Heidi thinks 3 times here might be better than previous 1
-%img_blob = imfill(img_blob, 'holes');
+%%img_blob = imfill(img_blob, 'holes');
 %img_blob = imclearborder(img_blob, 4);
 %%img_blob = imerode(img_blob, seD);
 
 target.blob_image = img_blob;
 
-%target.config.blob_min = 30;
+target.config.blob_min = 700;
 target = apply_blob_min( target ); %get rid of blobs < blob_min
 
 if config.plot, %go to graphing routine if requested
-    img_proc_plot(target, M+m, img_edge, img_dark)
-    %img_proc_plot(target, blob_s, img_blob , img_dark)
+    %img_proc_plot(target, M+m, img_edge, img_dark)
+    img_proc_plot(target, img_blob_close, img_blob_dilate , img_dark)
     pause
 end;
 
