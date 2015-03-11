278b1e3feb8e15fe340136cafb58a96eca6d3125
Emily Peacock <epeacock@whoi.edu>
2015-03-11 16:03:37 +0000

    updated to reflect to file/directory name structure.
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1172 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: a7a7ab7bf4f72d7a57346128686851a4f9543f61

--- VPR/start_batch_classify_VPR.m
+++ VPR/start_batch_classify_VPR.m
@@ -1,12 +1,13 @@
-savedir = '\\sosiknas1\Lab_data\VPR\NBP1201NewTrain201502\classifier\';
-classifierName = [savedir 'RossSea_Trees_02Mar2015'];
-out_dir = ['\\sosiknas1\Lab_data\VPR\vpr3\class_RossSea_Trees_02Mar2015\'];
+savedir = '\\SosikNAS1\Lab_data\VPR\NBP1201\NBP1201NewTrain201502\classifier3\';
+classifierName = 'RossSea_Trees_09Mar2015';
+out_dir = ['\\SosikNAS1\Lab_data\VPR\NBP1201\vpr3\class_' classifierName '\'];
 if ~exist(out_dir, 'dir')
     mkdir(out_dir)
 end;
+classifierName = [savedir classifierName];
 %in_dir = 'http://mellon.whoi.edu/lab/';
 %for V2 web services, set fea_dir = in_dir;
-fea_dir = '\\sosiknas1\Lab_data\VPR\vpr3\features\';
+fea_dir = '\\SosikNAS1\Lab_data\VPR\NBP1201\vpr3\features3\';
 filelist = dir([fea_dir '*.csv']);
 filelist = {filelist.name}';
 %filelist = [];
@@ -14,7 +15,7 @@ filelist = {filelist.name}';
 %    filelist = [filelist list_day(datestr(datenum(yr,0,day),29), in_dir)];
 %end;
 %filelist = regexprep(filelist, fea_dir, '')';
-files_done = dir([out_dir 'd*class_v1.mat']);
+files_done = dir([out_dir 'N*class_v1.mat']);
 files_done = char(files_done.name);
 files_done = cellstr(files_done(:,1:end-13));
 filelist2 = setdiff(filelist, files_done);
