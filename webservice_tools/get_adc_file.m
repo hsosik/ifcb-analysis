d596c76bfb9a090df0a3381397a75a35820aa717
Heidi Sosik <hsosik@whoi.edu>
2015-03-19 16:06:52 +0000

    function to load IFCB adc file (web services or local access)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1181 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: e7619a65a77888808e21f195f95f7991553ab1d4

---
+++ webservice_tools/get_adc_file.m
@@ -0,0 +1,23 @@
+function [ adc_data ] = get_adc_file( fullfilename )
+%function [ feadata, feahdrs ] = get_csv_file( filename )
+%import IFCB csv file
+%Heidi M. Sosik, Woods Hole Oceanographic Institution, February 2014
+
+if isequal(fullfilename(1:4), 'http'), 
+    [~,f] = fileparts(fullfilename);
+    [filestr,status] = urlwrite(fullfilename, [f '.adc']);
+    if status,
+        fullfilename = filestr;
+    else
+        disp(['Error reading ' fullfilename]);
+        feadata = [];
+        feahdrs = '';
+        return
+    end;
+end;
+adc_data = load([filestr]);
+%remove temporary file if read from URL
+if exist('status', 'var'), delete(filestr); end;
+
+end
+
