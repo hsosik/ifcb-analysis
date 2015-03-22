d71efbb3908049de271db7712cac7e89019e5593
Heidi Sosik <hsosik@whoi.edu>
2015-03-22 19:04:22 +0000

    scripts to convert VisualPlankton output into IFCB classlist file format
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1200 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 489dc6d4ed0e1481caadd541372c6e88e6597446

---
+++ VPR/misc/convert_aid2class.m
@@ -0,0 +1,59 @@
+function [ ] = convert_aid2class( roi_dir_base, aid_indir, class_outdir, class2use )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+%convert_aid2class('\\blackburn.whoi.edu\public\nbp1201_vpr\rois\', '\\blackburn.whoi.edu\public\nbp1201_vpr\autoid\', 'c:\work\vpr\aid_class\', class2use)
+
+if~exist(class_outdir, 'dir'),
+    mkdir(class_outdir)
+end;
+list_titles = {'roi number'    'manual'    'auto'};
+classnum_default = strmatch('unknown', class2use);
+for count = 1:length(class2use),
+    indir = [aid_indir filesep class2use{count} filesep 'aid' filesep];
+    disp(indir)
+    if exist(indir, 'dir'),
+        flist = dir([indir '*.h*']);
+        flist = cellstr(char(flist.name));
+        for fnum = 1:length(flist)
+            disp(flist(fnum))
+            d = importdata([indir flist{fnum}]);
+            [roi_dir] = fileparts(d{1});
+            roi_dir = [roi_dir_base regexprep(roi_dir, 'c:\\data\\NBP12_01\\rois\\', '') filesep];
+            ii = findstr(filesep, roi_dir);
+            outfile = [class_outdir 'NPB1201' regexprep(roi_dir(ii(end-3)+1:end),'\', '') '.mat'];
+            d = char(d);
+            ii = findstr(filesep, d(1,:));
+            dnum = str2num(d(:,(ii(end)+6):end-5));
+            if ~exist(outfile),
+                eval(['[s,roilist] = dos(''dir ' roi_dir filesep '*.tif /B'');'])
+                tt = strfind(roilist, char(10)); tt = tt(1)-1;
+                roilist = regexprep(roilist, char(10), '');
+                roilist = cellstr(reshape(roilist,tt,length(roilist)/tt)');
+                roilist = char(sort(roilist)); 
+                roilist = roilist(:,6:end-4);
+                %roilist = regexprep(roilist, 'roi..', '');
+                %roilist = regexprep(roilist, '..tif', '');
+                roilist = str2num(char(roilist));
+                c.classlist = NaN(length(roilist),3);
+                c.classlist(:,1) = roilist;
+                c.classlist(:,3) = classnum_default;
+                c.class2use_auto = class2use;
+                c.list_titles = list_titles;
+            else
+                c = load(outfile);
+            end;
+            [~, ia, ib] = intersect(c.classlist(:,1), dnum);
+            if length(ib) ~= length(dnum),
+                disp('missing ROI data corresponding to aid output')
+                keyboard
+            end;
+            c.classlist(ia,3) == count;
+            save(outfile, '-struct', 'c')
+        end;
+    else
+        disp(['missing aid output for class: ' class2use{count}])
+    end;
+end;
+
+end
+
