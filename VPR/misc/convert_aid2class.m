828ee3230a2cc0b5c6862cd58cbb5958582eeca0
Heidi Sosik <hsosik@whoi.edu>
2015-03-23 15:03:06 +0000

    revise for robust handling of directory location in aid input files (seems to vary across files)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1201 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 5bbfa2d27d9fafdc859898f1e93828f5323ab917

--- VPR/misc/convert_aid2class.m
+++ VPR/misc/convert_aid2class.m
@@ -1,25 +1,26 @@
 function [ ] = convert_aid2class( roi_dir_base, aid_indir, class_outdir, class2use )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-%convert_aid2class('\\blackburn.whoi.edu\public\nbp1201_vpr\rois\', '\\blackburn.whoi.edu\public\nbp1201_vpr\autoid\', 'c:\work\vpr\aid_class\', class2use)
+%convert_aid2class('\\blackburn.whoi.edu\public\nbp1201_vpr\rois\', '\\blackburn.whoi.edu\public\nbp1201_vpr\autoid\', '\\sosiknas1\Lab_data\VPR\NBP1201\aid_class\', class2use)
 
 if~exist(class_outdir, 'dir'),
     mkdir(class_outdir)
 end;
 list_titles = {'roi number'    'manual'    'auto'};
 classnum_default = strmatch('unknown', class2use);
-for count = 1:length(class2use),
+for count = 2:2, %length(class2use),
     indir = [aid_indir filesep class2use{count} filesep 'aid' filesep];
     disp(indir)
     if exist(indir, 'dir'),
         flist = dir([indir '*.h*']);
         flist = cellstr(char(flist.name));
-        for fnum = 1:length(flist)
+        for fnum = 101:length(flist)
             disp(flist(fnum))
             d = importdata([indir flist{fnum}]);
             [roi_dir] = fileparts(d{1});
-            roi_dir = [roi_dir_base regexprep(roi_dir, 'c:\\data\\NBP12_01\\rois\\', '') filesep];
+            %roi_dir = [roi_dir_base regexprep(roi_dir, 'c:\\data\\NBP12_01\\rois\\', '') filesep];
             ii = findstr(filesep, roi_dir);
+            roi_dir = [roi_dir_base roi_dir(ii(end-2)+1:end)];
             outfile = [class_outdir 'NPB1201' regexprep(roi_dir(ii(end-3)+1:end),'\', '') '.mat'];
             d = char(d);
             ii = findstr(filesep, d(1,:));
@@ -31,8 +32,6 @@ for count = 1:length(class2use),
                 roilist = cellstr(reshape(roilist,tt,length(roilist)/tt)');
                 roilist = char(sort(roilist)); 
                 roilist = roilist(:,6:end-4);
-                %roilist = regexprep(roilist, 'roi..', '');
-                %roilist = regexprep(roilist, '..tif', '');
                 roilist = str2num(char(roilist));
                 c.classlist = NaN(length(roilist),3);
                 c.classlist(:,1) = roilist;
