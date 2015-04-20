361d90a2e9e002b9ee59d595b1e25f9062237dba
Heidi Sosik <hsosik@whoi.edu>
2015-04-20 17:39:45 +0000

    fix help line
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1258 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 271bf694e1feeef85e4b68064c8ea9097a0c6e7c

--- manualclassify/ReleaseFocus.m
+++ manualclassify/ReleaseFocus.m
@@ -1,4 +1,5 @@
-function ReleaseFocus(fig)
+%function ReleaseFocus(fig)
+% function ReleaseFocus(fig)
 % disable/enable all uicontrols in a figure window to force focus back to
 % main axes, from http://www.mathworks.com/matlabcentral/newsreader/view_thread/235825
         set(findobj(fig, 'Type', 'uicontrol'), 'Enable', 'off');
