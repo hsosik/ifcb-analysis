bd351646d158ce215c7d2ae6f01d94ca7b060d18
Heidi Sosik <hsosik@whoi.edu>
2015-04-21 19:27:34 +0000

    remove accidental commenting of line 1!
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1266 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: b5e1dddce24e0e9b59905cc181d34b7b4904000c

--- manualclassify/ReleaseFocus.m
+++ manualclassify/ReleaseFocus.m
@@ -1,4 +1,4 @@
-%function ReleaseFocus(fig)
+function ReleaseFocus(fig)
 % function ReleaseFocus(fig)
 % disable/enable all uicontrols in a figure window to force focus back to
 % main axes, from http://www.mathworks.com/matlabcentral/newsreader/view_thread/235825
