c157b0d4caa02854b0d9176578afc4f6eb05ee9a
Heidi Sosik <hsosik@whoi.edu>
2015-04-23 14:04:40 +0000

    example standard case for use with ManageMCconfig GUI
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1283 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: dc75bda6b06554dcaab50b174a375d41645ceef8

---
+++ IFCB_tools/resolve_files2gui.m
@@ -0,0 +1,39 @@
+function files_struct = resolve_files2gui(filelist, baseroipath, baseclasspath, class_filestr);
+%function files_struct = resolve_files2gui(filelist, basepath, classpath, class_filestr);
+%Example template 
+% Heidi M. Sosik, Woods Hole Oceanographic Institution, 31 May 2009
+
+% if isstruct(filelist),
+%     filelist = {filelist.name}';
+%     [~, ~, ext] = fileparts(filelist{1});
+%     filelist = regexprep(filelist, ext, ''); %strip off extension
+% end;
+
+[~,files] = cellfun(@fileparts,filelist, 'uniformoutput', false);
+
+%matdate = IFCB_file2date(files);
+%[year,~,~] = datevec(matdate);
+files = char(files);
+n = length(filelist);
+sep = repmat(filesep,n,1);
+
+%create lists of roi/class files with fullpath
+roibase = repmat(char(baseroipath),n,1);
+classbase = repmat(char(baseclasspath),n,1);
+roiext = repmat('.roi',n,1);
+classext = repmat([class_filestr '.mat'],n,1);
+if files(1,1) == 'D',
+    roipath = [roibase files(:,2:5) sep files(:,1:9) sep];
+    classpath = [classbase files(:,2:5) sep files(:,1:9) sep]; %edit and use this line instead of above in case of different structure for class and roi locations
+else      
+    roipath = [roibase sep files(:,7:10) sep files(:,1:14) sep];
+    classpath = [classbase sep files(:,7:10) sep files(:,1:14) sep]; %edit and use this line instead of above in case of different structure for class and roi locations
+end;
+roifiles = cellstr([roipath files roiext]);
+classfiles = cellstr([classpath files classext]);
+
+files_struct.classfiles = classfiles;
+files_struct.roifiles = roifiles;
+
+end
+
