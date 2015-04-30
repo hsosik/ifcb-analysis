261c3ba25cd66aaf059a4aecd5f2e3a1c61792cb
Heidi Sosik <hsosik@whoi.edu>
2015-04-30 02:01:25 +0000

    MVCO file resolver formatted for use with new manual_classify GUIs
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1289 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 1590e62f5ac5f48d8a38c34de24903a06d1a5124

---
+++ IFCB_tools/resolve_MVCOfiles4gui.m
@@ -0,0 +1,41 @@
+function files_struct = resolve_MVCOfiles4gui(filelist, class_filestr);
+%function [filelist, classfiles, stitchfiles] = resolve_MVCOfiles(filelist, class_filestr);
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+if isstruct(filelist),
+    filelist = {filelist.name}';
+end;
+[~,filelist] = cellfun(@fileparts,filelist, 'uniformoutput', false); %filename only, no path, no ext
+
+classpath = fullfile('\\sosiknas1\IFCB_products\MVCO\class\classxxxx_v1\');  %case for new results with TBclassifier
+basedir = fullfile('\\sosiknas1\IFCB_data\MVCO\data\xxxx');
+stitchpath = fullfile('\\SOSIKNAS1\IFCB_products\MVCO\stitch\stitchxxxx\');  %%USER set, roi stitch info files
+
+n = length(filelist);
+matdate = IFCB_file2date(filelist);
+[year,~,~] = datevec(matdate);
+roiext = repmat('.roi',n,1);
+
+ii = findstr(basedir(1,:), 'xxxx'); %added by EP to accomodate SOSIKNAS file structure 12-15-2014
+basedir = repmat(basedir, length(year),1); %added by EP to accomodate SOSIKNAS file structure 12-15-2014
+basedir(:,ii:ii+3) = num2str(year);%added by EP to accomodate SOSIKNAS file structure 12-15-2014
+
+files = char(filelist);
+sep = repmat(filesep,length(year),1);
+basedir = [char(basedir) sep files(:,1:14) sep];
+%filelist = cellstr([basedir files roiext]);
+
+ii = findstr(classpath(1,:), 'xxxx');
+classpath = repmat(classpath, length(year),1);
+classpath(:,ii:ii+3) = num2str(year);
+ii = findstr(stitchpath(1,:), 'xxxx');
+stitchpath = repmat(stitchpath, length(year),1);
+stitchpath(:,ii:ii+3) = num2str(year);
+
+files_struct.roifiles = cellstr([basedir files roiext]);
+files_struct.classfiles = cellstr([classpath files repmat(class_filestr, length(year),1) repmat('.mat', length(year),1)]);
+files_struct.stitchfiles = cellstr([stitchpath files repmat('_roistitch', length(year),1) repmat('.mat', length(year),1)]);
+
+end
+
