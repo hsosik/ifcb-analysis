bf23e81de7ce90e33ef1c38857d8b750c48e20e9
Emily Peacock <epeacock@whoi.edu>
2015-07-29 15:53:08 +0000

    added example plotting for referencing tamug_table which designates which data is from what sampling (eg, timeseries, culture, spatial)
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1355 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 27e7c260959d0bb0e54c148e176c0db2290084bc

--- classification/countcells_allTBnew_TAMUG.m
+++ classification/countcells_allTBnew_TAMUG.m
@@ -44,12 +44,12 @@ num2dostr = num2str(length(classfiles));
 ml_analyzed = NaN(size(classfiles));
 %adhocthresh = 0.5.*ones(size(class2use)); %assign all classes the same adhoc decision threshold between 0 and 1
 %adhocthresh(strmatch('Ditylum', class2use, 'exact')) = 0.45; %reassign value for specific class
-
+adhocthresh=tamug_threshold(class2use);
 for filecount = 1:length(classfiles)
     if ~rem(filecount,10), disp(['reading ' num2str(filecount) ' of ' num2dostr]), end;
     ml_analyzed(filecount) = IFCB_volume_analyzed(hdrfiles{filecount});
     if exist('adhocthresh', 'var'),
-        [classcount(filecount,:), classcount_above_optthresh(filecount,:), classcount_above_adhocthresh(filecount,:)] = summarize_TBclass(classfiles{filecount});
+        [classcount(filecount,:), classcount_above_optthresh(filecount,:), classcount_above_adhocthresh(filecount,:)] = summarize_TBclass(classfiles{filecount}, adhocthresh);
     else
         [classcount(filecount,:), classcount_above_optthresh(filecount,:)] = summarize_TBclass(classfiles{filecount});
     end;
@@ -65,7 +65,7 @@ filelistTB = filelist;
 class2useTB = class2use;
 classcountTB = classcount;
 classcountTB_above_optthresh = classcount_above_optthresh;
-clear mdate filelist class2use classcount classcount_above_optthresh classcount_above_adhocthresh filecount yr* classfiles in_dir num2dostr
+clear mdate filelist class2use classcount classcount_above_optthresh filecount yr* classfiles in_dir num2dostr
 
 if exist('adhocthresh', 'var'),
     classcountTB_above_adhocthresh = classcount_above_adhocthresh;
@@ -74,29 +74,29 @@ else
     save([path_out 'summary_allTB'] , 'class2useTB', 'classcountTB', 'classcountTB_above_optthresh', 'ml_analyzedTB', 'mdateTB', 'filelistTB', 'classpath_generic')
 end;
 
-% example to search categorical columns for example plotting.
-row_numbers = find(tamug_table.sample_type2 == 'timeseries' |tamug_table.sample_type == 'timeseries'); % gives list of row numbers that meets criteria
+% % example to search categorical columns for example plotting.
+ row_numbers = find(tamug_table.sample_type2 == 'timeseries' |tamug_table.sample_type == 'timeseries'); % gives list of row numbers that meets criteria
 filelist = tamug_table{row_numbers,{'filename'}}; %gives a list of files that meet the criteria
+ filelistTBstr = cellstr(filelistTB);
+ row_numbers = find(ismember(filelistTBstr, filelist)==1);
+% 
+% enddate = '01/08/15'; % enter start date here
+% startdate = '11/10/14'; % enter end date here
+% enddate = datenum(enddate);
+% startdate = datenum(startdate);
+% matdate= datenum(tamug_table.date);
+% row_numbers = find(matdate >= startdate & matdate <= enddate & tamug_table.sample_type == 'timeseries');
 
-temp = find(ismember(filelistTBstr, filelist)==1);
-
-enddate = '01/08/15'; % enter start date here
-startdate = '11/10/14'; % enter end date here
-enddate = datenum(enddate);
-startdate = datenum(startdate);
-matdate= datenum(tamug_table.date);
-row_numbers = find(matdate >= startdate & matdate <= enddate & tamug_table.sample_type == 'timeseries');
-
-%example plotting code for plotting just certain rows (load summary file first)
-figure, subplot(2,1,1)
-classind = 25;
-plot(mdateTB(temp), classcountTB(temp,classind)./ml_analyzedTB(temp), 'r.')
-
-%example plotting code for all of the data (load summary file first)
-figure, subplot(2,1,1)
-classind = 25;
-plot(mdateTB, classcountTB(:,classind)./ml_analyzedTB, '.-')
-hold on
+% %example plotting code for plotting just certain rows (load summary file first)
+ figure, subplot(2,1,1)
+ classind = 25;
+ plot(mdateTB(row_numbers), classcountTB(row_numbers,classind)./ml_analyzedTB(row_numbers), 'r.')
+% 
+% %example plotting code for all of the data (load summary file first)
+ figure, subplot(2,1,1)
+ classind = 25;
+ plot(mdateTB, classcountTB(:,classind)./ml_analyzedTB, '.-')
+ hold on
 plot(mdateTB, classcountTB_above_optthresh(:,classind)./ml_analyzedTB, 'g.-')
 legend('All wins', 'Wins above optimal threshold')
 ylabel([class2useTB{classind} ', mL^{ -1}'])
