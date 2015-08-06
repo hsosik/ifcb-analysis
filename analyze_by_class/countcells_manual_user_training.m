7ea41c619007ac4e598bbadfb4f9f07b1394b6cc
Heidi Sosik <hsosik@whoi.edu>
2015-08-06 17:59:59 +0000

    example for user training
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1372 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 18c740607029e706339a41b235c05f741722414d

---
+++ analyze_by_class/countcells_manual_user_training.m
@@ -0,0 +1,51 @@
+resultpath = 'C:\work\IFCB\user_training_test_data\manual\'; %USER
+roibasepath = 'C:\work\IFCB\user_training_test_data\data\'; %USER
+filelist = dir([resultpath 'D*.mat']);
+
+%calculate date
+matdate = IFCB_file2date({filelist.name});
+
+load([resultpath filelist(1).name]) %read first file to get classes
+numclass = length(class2use_manual);
+class2use_manual_first = class2use_manual;
+class2use_here = class2use_manual;
+classcount = NaN(length(filelist),numclass);  %initialize output
+ml_analyzed = NaN(length(filelist),1);
+for filecount = 1:length(filelist),
+    filename = filelist(filecount).name;
+    disp(filename)
+    hdrname = [roibasepath filesep filename(2:5) filesep filename(1:9) filesep regexprep(filename, 'mat', 'hdr')]; 
+    ml_analyzed(filecount) = IFCB_volume_analyzed(hdrname);
+     
+    load([resultpath filename])
+    if ~isequal(class2use_manual, class2use_manual_first)
+        [t,ii] = min([length(class2use_manual_first), length(class2use_manual)]);
+        if ~isequal(class2use_manual(1:t), class2use_manual_first(1:t)),
+            disp('class2use_manual does not match previous files!!!')
+            keyboard
+        else
+            if ii == 1, class2use_manual_first = class2use_manual; end; %new longest list
+        end;
+    end;
+    for classnum = 1:numclass,
+            %classcount(filecount,classnum) = size(find(classlist(:,2) == classnum | (isnan(classlist(:,2)) & classlist(:,3) == classnum)),1);
+            classcount(filecount,classnum) = size(find(classlist(:,2) == classnum),1); %manual only
+    end;
+    clear class2use_manual class2use_auto class2use_sub* classlist
+end;
+
+class2use = class2use_here;
+if ~exist([resultpath 'summary\'], 'dir')
+    mkdir([resultpath 'summary\'])
+end;
+datestr = date; datestr = regexprep(datestr,'-','');
+save([resultpath 'summary\count_manual_' datestr], 'matdate', 'ml_analyzed', 'classcount', 'filelist', 'class2use')
+
+return
+
+figure %example
+classnum = 1;
+plot(matdate, classcount(:,classnum)./ml_analyzed, '.-')
+datetick('x')
+ylabel([class2use{classnum} ' (mL^{-1})'])
+
