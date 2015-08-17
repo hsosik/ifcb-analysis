c1a185952330f3c07e38f4c274f11573454a0cf1
Heidi Sosik <hsosik@whoi.edu>
2015-08-17 21:58:15 +0000

    restructure as function
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1384 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: d8b29502373079382feb0013a66490b4365d13ef

--- classification/batch_classification/start_classify_batch_user_training.m
+++ classification/batch_classification/start_classify_batch_user_training.m
@@ -1,24 +1,29 @@
-classifierName = 'C:\work\IFCB\user_training_test_data\manual\summary\UserExample_Trees_06Aug2015'; %USER what classifier do you want to apply
-yr = 2014; %USER what is the year of your data set
-
-out_dir = ['C:\work\IFCB\user_training_test_data\class\class' num2str(yr) '_v1\'];% USER what is the base path for output files
-%in_dir = 'http://ifcb-data.whoi.edu/mvco/';
-%for V2 web services, set fea_dir = in_dir;
-fea_dir = ['C:\work\IFCB\user_training_test_data\features\' num2str(yr) filesep]; %USER where are your feature files
-
-filelist = [];
+function [ ] = start_classify_batch_user_training(classifierName , in_dir_feature, out_dir_class)
+%function [ ] = start_classify_batch_user_training(classifierName , in_dir_feature, out_dir_class)
+%For example:
+%   start_classify_batch_user_training('C:\work\IFCB\user_training_test_data\manual\summary\UserExample_Trees_06Aug2015' , 'C:\work\IFCB\user_training_test_data\features\2014\', 'C:\work\IFCB\user_training_test_data\class\class2014_v1\')
+%
+%IFCB image classification: configure and initiate batch classification from extracted features and input classifier
+% run make_TreeBaggerClassifier_user_training.m first to produce random forest classifier stored in file (classiferName)
+%Heidi M. Sosik, Woods Hole Oceanographic Institution, August 2015
+%
+%Example input:
+%   classifierName = 'C:\work\IFCB\user_training_test_data\manual\summary\UserExample_Trees_06Aug2015'; USER what classifier do you want to apply (full path)
+%   in_dir_features = 'C:\work\IFCB\user_training_test_data\features\2014\'; %USER where are your feature files
+%   out_dir_class = 'C:\work\IFCB\user_training_test_data\class\class2014_v1\';% USER what is the base path for output files
 
 disp('Checking for files to run')
-filelist = dir([fea_dir '*.csv']);
+filelist = dir([in_dir_feature '*.csv']);
 filelist = {filelist.name}';
 filelist = regexprep(filelist, '_fea_v2.csv', '')';
-%filelist = regexprep(filelist, in_dir, '')';
-files_done = dir([out_dir 'IFCB*class_v1.mat']);
+files_done = dir([out_dir_class 'IFCB*class_v1.mat']);
 files_done = char(files_done.name);
 files_done = cellstr(files_done(:,1:end-13));
 filelist2 = setdiff(filelist, files_done);
 filelist2 = strcat(filelist2,'_fea_v2.csv');  %USER specify v1 or v2 features as appropriate
 
 disp(['processing ' num2str(length(filelist2)) ' files'])
-batch_classify( fea_dir, filelist2, out_dir, classifierName );
+batch_classify( in_dir_feature, filelist2, out_dir_class, classifierName );
+
+end
 
