e6c818d60bedb30453c4028fdf72259f64b55e62
Heidi Sosik <hsosik@whoi.edu>
2015-08-17 22:00:23 +0000

    fix comments / help
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1388 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 1d71e2ef0a39670b1cfb0c991ab9d47cb9681b19

--- feature_extraction/batch_features_bins/start_feature_batch_user_training.m
+++ feature_extraction/batch_features_bins/start_feature_batch_user_training.m
@@ -1,16 +1,16 @@
 function [ ] = start_feature_batch_user_training(in_dir_base , in_dir_blob_base, out_dir, parallel_proc_flag)
 %function [ ] = start_feature_batch_user_training(in_dir_base , in_dir_blob_base, out_dir, parallel_proc_flag)
-%Example:
+%For example:
 %start_feature_batch_user_training('C:\work\IFCB\user_training_test_data\data\2014\' , 'C:\work\IFCB\user_training_test_data\blobs\2014\', 'C:\work\IFCB\user_training_test_data\features\2014\', false)
 %
-%IFCB image processing; Heidi M. Sosik, Woods Hole Oceanographic Institution, Aug 2015
-%configure and initiate batch processing for feature extractiom
+%IFCB image processing: configure and initiate batch processing for feature extraction
+%Heidi M. Sosik, Woods Hole Oceanographic Institution, August 2015
 %
 %example input variables:
 %   out_dir = 'C:\work\IFCB\user_training_test_data\features\2014\'; %USER where to output features
 %   in_dir_base = 'C:\work\IFCB\user_training_test_data\data\2014\'; %USER where to access roi data
 %   in_dir_blob_base = 'C:\work\IFCB\user_training_test_data\blobs\2014\'; %USER where to access blobs
-%   parallel_proc_flag = false; 
+%   parallel_proc_flag = false; %USER true for parallel processing
 
 if ~exist('parallel_proc_flag', 'var')
     parallel_proc_flag = false; %default
