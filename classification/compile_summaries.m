f4a2532d254c4d5af2b0a0488e1e8b762466462a
Heidi Sosik <hsosik@whoi.edu>
2015-09-22 17:12:54 +0000

    update for revised input variable names
    git-svn-id: https://beagle.whoi.edu/svn/ifcb/trunk@1441 9f5e5bc8-6430-4f94-ad00-aad15e9f9366
    
    Former-commit-id: 57538b7ce5866c31579f6b6708404bf50bcd1f48

--- classification/compile_summaries.m
+++ classification/compile_summaries.m
@@ -1,18 +1,18 @@
-classcountTBall = [];
-classcountTB_above_adhocthreshall = [];
-classcountTB_above_optthreshall = [];
-ml_analyzedTBall = [];
-mdateTBall = [];
-filelistTBall = [];
+classcountTB = [];
+classcountTB_above_adhocthresh = [];
+classcountTB_above_optthresh = [];
+ml_analyzedTB = [];
+mdateTB = [];
+filelistTB = [];
 
-for yr = 2006:2012,
-    temp = load(['summary_allTBpt7' num2str(yr)]);
-    classcountTBall = [ classcountTBall; temp.classcountTB];
-    classcountTB_above_adhocthreshall = [ classcountTB_above_adhocthreshall; temp.classcountTB_above_adhocthresh];
-    classcountTB_above_optthreshall = [ classcountTB_above_optthreshall; temp.classcountTB_above_optthresh];
-    ml_analyzedTBall = [ ml_analyzedTBall; temp.ml_analyzedTB];
-    mdateTBall = [ mdateTBall; temp.mdateTB];
-    filelistTBall = [ filelistTBall; temp.filelistTB];
+for yr = 2006:2013,
+    temp = load(['summary_allTB' num2str(yr)]);
+    classcountTB = [ classcountTB; temp.classcountTB];
+    classcountTB_above_adhocthresh = [ classcountTB_above_adhocthresh; temp.classcountTB_above_adhocthresh];
+    classcountTB_above_optthresh = [ classcountTB_above_optthresh; temp.classcountTB_above_optthresh];
+    ml_analyzedTB = [ ml_analyzedTB; temp.ml_analyzedTB];
+    mdateTB = [ mdateTB; temp.mdateTB];
+    filelistTB = [ filelistTB; temp.filelistTB];
     class2useTB = temp.class2useTB;
     clear temp
 end;
@@ -21,7 +21,8 @@ clear yr
 return
 
 
-load \\raspberry\d_work\IFCB1\ifcb_data_mvco_jun06\Manual_fromClass\summary\count_biovol_manual_13Sep2012_day
+%load \\raspberry\d_work\IFCB1\ifcb_data_mvco_jun06\Manual_fromClass\summary\count_biovol_manual_13Sep2012_day
+load C:\work\IFCB\ifcb_data_MVCO_jun06\Manual_fromClass\summary\count_biovol_manual_19Mar2013_day
 %figure, set(gcf, 'paperposition', [.25 2.5 8 2.5], 'units', 'inches')
 %set(gcf, 'position', [5 4 8 2.5])
 figure, set(gcf, 'paperposition', [.25 2.5 11.5 2.45], 'units', 'inches')
@@ -30,19 +31,22 @@ set(gcf, 'position', [5 4 11.5 2.45])
 %ind = strmatch('Ditylum', class2useTB);
 ind = strmatch('Guinardia', class2useTB, 'exact');
 %[xmat, ymat ] = timeseries2ydmat(mdateTBall, classcountTB_above_adhocthreshall(:,ind)./ml_analyzedTBall);
-[xmat, ymat ] = timeseries2ydmat(mdateTBall, classcountTBall(:,ind)./ml_analyzedTBall);
+[xmat, ymat ] = timeseries2ydmat(mdateTB, classcountTB(:,ind)./ml_analyzedTB);
 hold on, 
 %ind2 = strmatch('Ditylum', class2use);
-ind2 = strmatch('Guinardia', class2use, 'exact');
 plot(matdate_bin, classcount_bin(:,ind2)./ml_analyzed_mat_bin(:,ind2), 'r*')
+%ind2 = strmatch('Guinardia', class2use, 'exact');
+gind = get_G_delicatula_ind(class2use);
+ii = find(~isnan(ml_analyzed_mat_bin(:,gind(1)))); %cases with main Guinardia delicatula annotated
+plot(matdate_bin(ii), nansum(classcount_bin(ii,gind)./ml_analyzed_mat_bin(ii,gind),2), 'r*')
 plot(xmat(:), ymat(:), 'linewidth', 1.5)
 %text(datenum(2007,1,1), 10, ['\it' class2useTB(ind)])
 datetick, set(gca, 'xgrid', 'on', 'box', 'on')
 %ylim([0 15])
 ylim([0 80])
 legend('manual', 'automated')
-%ylabel('Chains mL^{-1}', 'fontsize', 18)
-ylabel('Cells mL^{-1}', 'fontsize', 12)
+ylabel('Chains mL^{-1}', 'fontsize', 12)
+%ylabel('Cells mL^{-1}', 'fontsize', 12)
 set(gca, 'fontsize', 12)
 
 %img = imread('http://ifcb-data.whoi.edu/mvco/IFCB5_2011_017_134444_01637.png'); %Ditylum
