e48434e30957c6bd71aa3947958186cd813d3522
Heidi M. Sosik <hsosik@whoi.edu>
2015-11-02 16:42:07 -0500

    relocating project development code into new 'Development' subfolder (lab specific code, not ready / appropriate for outside users)

--- scripts/find_missing_blobs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# find blob bins that have yet to be computed and staged to demi
-# this script is designed to run on demi
-find /data/vol1/IFCB* -name '*.roi' -exec sh check_for_blobs.sh {} \;
-find /data/vol3/IFCB* -name '*.roi' -exec sh check_for_blobs.sh {} \;
--- scripts/install_rabbitmq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# for stock debian x64
-curl -O http://www.rabbitmq.com/releases/rabbitmq-server/v2.7.1/rabbitmq-server_2.7.1-1_all.deb
-apt-get install erlang-nox python-pip
-dpkg -i rabbitmq-server_2.7.1-1_all.deb
-pip install pika
-
