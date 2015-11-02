90e7073f86328c137db92fb318392c889d901d0e
Heidi M. Sosik <hsosik@whoi.edu>
2015-11-02 11:55:05 -0500

    deleting dead code

--- feature_extraction/image_HOG.m
+++
@@ -1,9 +0,0 @@
-function [ target ] = image_HOG( target )
-% takes gray scale image as input, returns vector of Histogram of Oriented Gradients (HOG) 
-% feature descriptors stored in target structure
-% Heidi M. Sosik, Woods Hole Oceanographic Institution, Oct 2011
-
-target.image_props.HOG = HOG(target.image);
-
-end
-
