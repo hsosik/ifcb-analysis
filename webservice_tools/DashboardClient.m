f979844706b753c2b048c962760fa9e42227279d
Joe Futrelle <joefutrelle@gmail.com>
2015-11-19 16:45:38 -0500

    added documentation

--- webservice_tools/DashboardClient.m
+++ webservice_tools/DashboardClient.m
@@ -9,8 +9,9 @@ classdef DashboardClient
     methods
         function this = DashboardClient(base_url, time_series, api_key)
             % DashboardClient create a dashboard client
-            % base_url = root of dashboard URL (e.g.,
-            % http://ifcb-data.whoi.edu)
+            % base_url = root of dashboard URL (e.g., http://ifcb-data.whoi.edu)
+            % time_series = label of the time series to access
+            % api_key = API key for privileged operations
             
             if nargin > 0
                 this.base_url = base_url;
@@ -26,6 +27,8 @@ classdef DashboardClient
         end
         % bin-level stuff
         function r = get_bin(~, pid)
+            % get_bin get all ROIS and ADC data for a bin as a struct array
+            
             r = get_bin(char(pid));
         end
         function r = bin_rois(~, pid)
@@ -37,10 +40,14 @@ classdef DashboardClient
             r = r.targets;
         end
         function r = bin_metadata(~, pid)
+            % bin_metadata get the bin-level metadata (not ROIs)
+            
             url = [char(pid) '_short.json'];
             r = webread(url);
         end
         function i = mosaic(this, pid, page, size, scale)
+            % mosaic return a mosaic image for a given bin
+            
             if nargin < 3
                 page=1;
             end
@@ -55,55 +62,96 @@ classdef DashboardClient
         end
         % roi-level stuff
         function r = roi_metadata(~, pid)
+            % roi_metadata get metadata for a single ROI
+            
             url = [char(pid) '.json'];
             r = webread(url);
         end
         function i = roi_image(~, pid)
+            % roi_image get the image for a single ROI
+            
             url = [char(pid) '.png'];
             i = webread(url);
         end
         function i = blob_image(~, pid)
+            % blob_image get the blob image for a single ROI (if available)
+            
             url = [char(pid) '_blob.png'];
             i = webread(url);
         end
         function i = outline_image(~, pid)
+            % outline_image get the blob outline overlay image for a ROI
+            
             url = [char(pid) '_blob_outline.png'];
             i = webread(url);
         end
         % feed and metrics
         function r = list_day(this, date)
+            % list_day list all bins in this time series for a given day
+            % date = a date like '2008-12-31'
+            
             r = list_day(date, this.namespace);
         end
         function r = latest(this)
+            % latest return the URL of the most recent bin in this time series
+            
             url = [this.namespace '/api/feed/nearest/3000-01-01'];
             r = webread(url);
         end
         function r = first(this)
+            % first return the URL of the earliest bin in this time series
+            
             url = [this.namespace '/api/feed/nearest/1970-01-01'];
             r = webread(url);
         end
         function r = nearest(this, date)
+            % nearest return the URL of the bin nearest the given date
+            
             url = [this.namespace '/api/feed/nearest/' date];
             r = webread(url);
         end
         function r = next(this, pid)
+            % next given a bin, return the next bin in the time series
+            
             url = [this.namespace '/api/feed/after/n/1/pid/' char(pid)];
             r = webread(url);
         end
         function r = previous(this, pid)
+            % previous given a bin, return the previous bin in the time series
+            
             url = [this.namespace '/api/feed/before/n/1/pid/' char(pid)];
             r = webread(url);
         end
         function r = get_metric(this, metric, start, ends)
+            % get_metric get the given metric between the given dates
+            % metrics include temperature, humidity, and trigger_rate
+            
             url = [this.namespace '/api/feed/' metric '/start/' start '/end/' ends];
             r = webread(url);
         end
         % tags
         function r = tags(this, pid)
+            % tags get the tags for a bin
+            
             url = [this.namespace '/api/tags/' char(pid)];
             r = webread(url);
         end
+        function r = has_tag(this, pid, tag)
+            % has_tag check if a bin has the given tag
+            
+            r = false;
+            tags = this.tags(pid);
+            for n=1:length(tags)
+                if strcmp(tags{n},tag)
+                    r = true;
+                    return
+                end
+            end
+        end
         function r = search_tags(this, query)
+            % search_tags return a list of bins with the given tag(s)
+            % you can separate tags with a comma
+            
             page = 1;
             r = [];
             while page > 0
@@ -118,28 +166,44 @@ classdef DashboardClient
             end
         end
         function r = add_tag(this, pid, tag)
+            % add_tag add a tag to a bin
+            
             url = [this.namespace '/api/add_tag/' tag '/' char(pid)];
             webread(url, this.authz);
             r = this.tags(pid);
         end
         function r = remove_tag(this, pid, tag)
+            % remove_tag remove a tag from a bin
+            
             url = [this.namespace '/api/remove_tag/' tag '/' char(pid)];
             webread(url, this.authz);
             r = this.tags(pid);
         end
         % products
         function r = product_exists(~, pid, product_type)
+            % product_exists check to see if a given product exists
+            % pid = bin pid
+            % product_type = includes 'binzip', 'blobs', 'features'
+            
             r = product_exists([char(pid) '_' product_type]);
         end
         function r = get_features(~, pid)
+            % get_features get the features for a given bin (if exists)
+            
             url = [char(pid) '_features'];
             r = get_fea_file(url);
         end
         function r = get_blobs(~, pid)
+            % get_blobs get the blobs for a given bin (if exists)
+            
             url = [char(pid) '_blob.zip'];
             r = get_blob_bin_file(url);
         end
         function r = deposit_product(this, pid, filepath)
+            % deposit_product upload a product
+            % pid = the full pid of the product
+            % filepath = local file containing product data
+            
             r = deposit_product(filepath, char(pid), this.api_key);
         end
     end
--- webservice_tools/WorkflowClient.m
+++ webservice_tools/WorkflowClient.m
@@ -6,6 +6,9 @@ classdef WorkflowClient
     end
     methods
         function this = WorkflowClient(base_url)
+            % create a workflow client
+            % base_url = base URL of workflow service (e.g., http://localhost:9270)
+            
             if nargin > 0
                 this.base_url = base_url;
             end
@@ -19,6 +22,10 @@ classdef WorkflowClient
             u = [this.base_url this.api_prefix endpoint pid_part];
         end
         function r = create(this, pid, state)
+            % create create a product
+            % pid = URL of product
+            % state = initial state (default: 'available')
+            
             if nargin < 3;
                 state = 'available';
             end
@@ -26,6 +33,10 @@ classdef WorkflowClient
             r = webwrap(@()webwrite(url,'state',state));
         end
         function r = get(this, pid, product_type)
+            % get get status information about a product
+            % pid = URL of bin
+            % product_type = one of 'binzip','blobs','features', etc.
+            
             if nargin < 3
                 product_type = 'raw';
             end
@@ -33,6 +44,12 @@ classdef WorkflowClient
             r = webwrap(@()webread(url));
         end
         function r = depend(this, pid, upstream, role, priority)
+            % depend establish a dependency between two products
+            % pid = the URL of the "downstream" product
+            % upstream = the URL of the "upstream" product
+            % role = the relationship between them (e.g., 'raw2binzip')
+            % priority = the priority of this job (lower is higher priority)
+            
             if nargin < 5
                 priority = 100;
             end
@@ -40,6 +57,13 @@ classdef WorkflowClient
             r = webwrap(@()webwrite(url,'upstream',upstream,'role',role,'priority',priority));
         end
         function r = update(this, pid, state, event, message, ttl)
+            % update update the status of a product
+            % pid = product URL
+            % state = new state (default "running")
+            % event = new event (default "heartbeat")
+            % message = new message
+            % ttl = time-to-live (default forever)
+            
             if nargin < 3
                 state = 'running';
             end;if nargin < 4
@@ -53,6 +77,12 @@ classdef WorkflowClient
             r = webwrap(@()webwrite(url,'state',state,'event',event,'message',message,'ttl',ttl));
         end
         function r = complete(this, pid, state, event, message)
+            % complete mark a product as completed
+            % pid = product URL
+            % state = final state (default "available")
+            % event = final event (default "completed")
+            % message = final message
+            
             if nargin < 3
                 state = 'available';
             end;if nargin < 4
@@ -64,6 +94,14 @@ classdef WorkflowClient
             r = webwrap(@()webwrite(url,'state',state,'event',event,'message',message,'ttl',-1));
         end
         function r = update_if(this, pid, state, new_state, event, message, ttl)
+            % update_if update only if in given state
+            % pid = product URL
+            % state = state the product must be in (default "waiting")
+            % new_state = state to change to if possible (default "running")
+            % event = event for this state change (default "started")
+            % message = message for this state change
+            % ttl = time-to-live for this state change (default forever)
+            
             if nargin < 3
                 state = 'waiting';
             end;if nargin < 4
@@ -79,9 +117,17 @@ classdef WorkflowClient
             r = webwrap(@()webwrite(url,'state',state,'new_state',new_state,'event',event,'message',message,'ttl',ttl));
         end
         function r = abort(this, pid)
+            % abort equivalent to update if running to waiting/aborted
+            
             r = this.update_if(pid,'running','waiting','aborted');
         end
         function r = expire(this, state, new_state, event, message, ttl)
+            % expire cause any expired objects to make the given state transition
+            % state = state the products must be in now (default "running")
+            % new_state = state after transition (default "waiting")
+            % event = state change event (default "expired")
+            % ttl = time to live for new state (default forever)
+            
             if nargin < 3
                 state = 'running';
             end;if nargin < 4
@@ -97,6 +143,11 @@ classdef WorkflowClient
             r = webwrap(@()webwrite(url,'state',state,'new_state',new_state,'event',event,'message',message,'ttl',ttl));
         end
         function r = heartbeat(this, pid, message, ttl)
+            % heartbeat keep a product from expiring
+            % pid = product URL
+            % message = message for this update
+            % ttl = new time-to-live
+            
             if nargin < 3
                 message = '';
             end;if nargin < 4
@@ -110,6 +161,11 @@ classdef WorkflowClient
             end
         end
         function r = start_next(this, role, ttl, state)
+            % start_next start the next product for the given role
+            % role = the role
+            % ttl = new time-to-live
+            % state = state the product must be in (default "waiting")
+            
             if nargin < 4
                 state = 'waiting';
             end;if nargin < 3;
