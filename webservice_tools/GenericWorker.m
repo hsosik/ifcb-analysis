d4e5b5afb063e02acfa62b90510298513b4c626c
Joe Futrelle <joefutrelle@gmail.com>
2015-12-04 16:24:23 -0500

    first draft of features task without multiblob still refactoring generic worker stuff bin_blobs now takes a log callback

--- webservice_tools/GenericWorker.m
+++ webservice_tools/GenericWorker.m
@@ -8,37 +8,54 @@ classdef GenericWorker
     end
     
     methods
-        function this = GenericWorker(workflow_client, dashboard_client)
-            this.wf = workflow_client;
-            this.db = dashboard_client;
+        function this = GenericWorker(host, port, api_key)
+            this.wf = WorkflowClient(['http://' char(host) ':9270']);
+            this.db = DashboardClient(['http://' char(host) ':' num2str(port)],api_key);
         end
-        function do_all_work(this, role, ttl, callback)
+        function log(this, pid, message, ttl)
+            disp(message);
+            this.wf.heartbeat(pid, message, ttl);
+        end
+        function r = deposit_product(this, pid, file)
+            r = this.db.deposit_product(pid, file, 'check');
+        end
+        function not_done = do_one_job(this, role, callback, ttl)
             not_done = true;
-            while not_done
-                disp('looking for next job');
-                this.wf.expire();
-                r = this.wf.start_next(role, ttl);
-                if r.http == 404
-                    disp('no more jobs');
-                    not_done = false;
-                else
-                    product_pid = r.pid;
-                    disp(['START ' product_pid]);
-                    try
-                        r = callback(this, product_pid);
-                        if r
-                            disp(['DONE ' product_pid]);
-                            this.wf.complete(product_pid);
-                        else
-                            disp(['NOT DONE ' product_pid]);
-                            this.wf.abort(product_pid);
-                        end
-                    catch exc
-                        disp(getReport(exc));
-                        disp(['ERROR ' product_pid]);
+            if ~exist('ttl','var')
+                ttl = 120;
+            end
+            disp('looking for next job');
+            this.wf.expire();
+            r = this.wf.start_next(role, ttl);
+            if r.http == 404
+                disp('no more jobs');
+                not_done = false;
+            else
+                product_pid = r.pid;
+                disp(['START ' product_pid]);
+                try
+                    r = callback(this, product_pid);
+                    if r
+                        disp(['DONE ' product_pid]);
+                        this.wf.complete(product_pid);
+                    else
+                        disp(['NOT DONE ' product_pid]);
                         this.wf.abort(product_pid);
                     end
+                catch exc
+                    disp(getReport(exc));
+                    disp(['ERROR ' product_pid]);
+                    this.wf.abort(product_pid);
                 end
+            end
+        end
+        function do_all_jobs(this, role, callback, ttl)
+            if ~exist('ttl','var')
+                ttl = 120;
+            end
+            not_done = true;
+            while not_done
+                not_done = this.do_one_job(role, callback, ttl);
             end
         end
     end
--- webservice_tools/binzip_task.m
+++ webservice_tools/binzip_task.m
@@ -2,6 +2,10 @@ function [ r ] = binzip_task( w, binzip_pid )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
+function log(msg)
+    w.log(binzip_pid, msg, 20);
+end
+
 zip_path = [tempname '.zip'];
 c = onCleanup(@() cleanup(zip_path));
 
@@ -9,7 +13,9 @@ bin_pid = strrep(binzip_pid,'_binzip','');
 
 binzip_create(bin_pid,zip_path);
 
-r = w.db.deposit_product(binzip_pid,zip_path,'check');
+log(['DEPOSITING ' binzip_pid]);
+r = w.deposit_product(binzip_pid,zip_path);
+log(['DEPOSITED ' binzip_pid]);
 
 end
 
