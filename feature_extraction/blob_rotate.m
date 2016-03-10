1b5630694d97b7683ff8affd258b6a4d00e6d3f2
Joe Futrelle <joefutrelle@gmail.com>
2016-03-10 14:30:52 -0500

    fixed bug where in the multi-blob case, the image might be rotated along the major axis of some blob other than the largest one.

--- feature_extraction/blob_rotate.m
+++ feature_extraction/blob_rotate.m
@@ -16,15 +16,15 @@ if isempty(se3)
 end;
 
 if target.blob_props.numBlobs > 0,
-    geomprops = regionprops(logical(target.blob_image), 'Orientation');
-    [~,ind] = sort([target.blob_props.Area], 'descend'); %if isempty(ind), pause, end; 
+    geomprops = regionprops(logical(target.blob_image), {'Area','Orientation'});
+    [~,ind] = sort([geomprops.Area], 'descend'); %if isempty(ind), pause, end; 
     ind = ind(1);
     theta = -1*geomprops(ind).Orientation;
     %    img3 = imrotate(img2, theta, 'bilinear', 'crop');
     img = imrotate(target.blob_image, theta, 'bilinear');
     img = imclose(img, se3);
     img = imdilate(img, se2);
-    img = bwmorph(img, 'thin', 1);
+    img = bwmorph(img, 'thin', 1); % FIXME should be 3 in v3
 %figure(1), subplot(2,2,1), imshow(target.blob_image)
 %subplot(2,2,2), imshow(img), subplot(2,2,4), imshow(img2); subplot(2,2,3), imshow(target.image)
 %pause
