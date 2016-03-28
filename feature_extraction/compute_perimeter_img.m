d19d93be1e67edbeb362f7847898ef30387a30a5
Joe Futrelle <joefutrelle@gmail.com>
2016-03-28 14:07:21 -0400

    fixed bug where conv2 expects a non-binary image as input

--- feature_extraction/compute_perimeter_img.m
+++ feature_extraction/compute_perimeter_img.m
@@ -2,7 +2,7 @@ function [ perimeter_img ] = compute_perimeter_img( img )
 %return 4-connected perimeter image for a B&W blob image
 %   Detailed explanation goes here
 
-perimeter_img = im2bw(conv2(img,[0 -1 0; -1 4 -1; 0 -1 0]));
+perimeter_img = im2bw(conv2(single(img),[0 -1 0; -1 4 -1; 0 -1 0]));
 
 end
 
