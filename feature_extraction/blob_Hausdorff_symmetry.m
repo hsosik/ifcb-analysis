1707eb5c42d28ad43ca0be6606aa1a4081f30a50
Joe Futrelle <joefutrelle@gmail.com>
2016-03-28 11:41:00 -0400

    MATLAB and Python Hausdorff symmetry now agree. MATLAB Hausdorff symmetry is now computed for each blob.

--- feature_extraction/blob_Hausdorff_symmetry.m
+++ feature_extraction/blob_Hausdorff_symmetry.m
@@ -7,30 +7,24 @@ function [ target ] = blob_Hausdorff_symmetry( target )
 %November 2011, IFCB processing
 
 if target.blob_props.numBlobs > 0,
-    %perimeter = bwboundaries(target.blob_image_rotated, 'noholes');
-    img = target.blob_image_rotated;
-    gprops = regionprops(img, 'Centroid', 'Area');
-%    if size(gprops) ~= target.blob_props.numBlobs, keyboard, end;
-    [~,ind] = sort([gprops.Area], 'descend'); ind = ind(1); %find largest blob
-    gprops = gprops(ind);
-    p = bwboundaries(img, 'noholes'); 
-    c =repmat(fliplr(gprops.Centroid),size(p{ind},1),1); p = p{ind}-c;
-    p90 = [p(:,2), p(:,1)];
-    p180 = -p;;
-    pfud = [-p(:,1) p(:,2)];
-    %figure(1), clf
-    %plot(p(:,1), p(:,2), '.')
-    %hold on
-    %plot(pfud(:,1), pfud(:,2), 'r.')
-    %plot(p90(:,1), p90(:,2), 'g.')
-    %plot(p180(:,1), p180(:,2), 'm.')
-    hdfud = ModHausdorffDistMex(p,pfud);
-    hd90 = ModHausdorffDistMex(p, p90);
-    hd180 = ModHausdorffDistMex(p,p180);
-    %%[hdfud hd90 hd180]
-	target.blob_props.Hflip = hdfud;
-    target.blob_props.H90 = hd90;
-    target.blob_props.H180 = hd180;
+    for i = 1:target.blob_props.numBlobs,
+        img = target.rotated_blob_images{i};
+        c = size(img) / 2; % assume center of image is centroid
+        % find points on the boundary and center them
+        perimeter_img = im2bw(conv2(img,[0 -1 0; -1 4 -1; 0 -1 0]));
+        [y, x] = find(perimeter_img);
+        % center and correct for 1-based indexing
+        p = horzcat(y-c(1)-1,x-c(2)-1);
+        p90 = [p(:,2), p(:,1)];
+        p180 = -p;
+        pfud = [-p(:,1) p(:,2)];
+        hdfud = ModHausdorffDistMex(p,pfud);
+        hd90 = ModHausdorffDistMex(p, p90);
+        hd180 = ModHausdorffDistMex(p,p180);
+        target.blob_props.H180(i) = hd180;
+        target.blob_props.H90(i) = hd90;
+        target.blob_props.Hflip(i) = hdfud;
+    end;
 else
     target.blob_props.Hflip = NaN;
     target.blob_props.H90 = NaN;
