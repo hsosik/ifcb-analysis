8ace18f715574de410c23d39d345cf797d3123fa
Heidi M. Sosik <hsosik@whoi.edu>
2016-03-28 20:21:05 -0400

    move computation of rotated_BoundingBox_solidity here (instead of in add_derived_features) so RotatedArea does not need to be stored

--- feature_extraction/blob_rotated_geomprop.m
+++ feature_extraction/blob_rotated_geomprop.m
@@ -4,14 +4,14 @@ function [ target ] = blob_rotated_geomprop( target )
 prop_list = {'BoundingBox' 'Area'};
 target.blob_props.RotatedBoundingBox_xwidth = zeros(1,max(1,target.blob_props.numBlobs));
 target.blob_props.RotatedBoundingBox_ywidth = target.blob_props.RotatedBoundingBox_xwidth;
-target.blob_props.RotatedArea = target.blob_props.RotatedBoundingBox_xwidth;
+target.blob_props.rotated_BoundingBox_solidity = target.blob_props.RotatedBoundingBox_xwidth;
 
 for idx = 1:target.blob_props.numBlobs,
     %input to region props as label image (single blob labeled as 1s), not input as logical (since possible to get split blobs after rotation)
     geomprops = regionprops(target.rotated_blob_images{idx}, prop_list);  
     target.blob_props.RotatedBoundingBox_xwidth(idx) = geomprops.BoundingBox(3);
-    target.blob_props.RotatedBoundingBox_ywidth(idx) = geomprops.BoundingBox(4);   
-    target.blob_props.RotatedArea(idx) = geomprops.Area;   
+    target.blob_props.RotatedBoundingBox_ywidth(idx) = geomprops.BoundingBox(4);
+    target.blob_props.rotated_BoundingBox_solidity(idx) = geomprops.Area./(geomprops.BoundingBox(4)*geomprops.BoundingBox(4));
 end;
 
 end
