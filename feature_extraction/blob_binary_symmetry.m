3db97355f663e3b3ddbfae7cbb06aca2f43b6831
Heidi M. Sosik <hsosik@whoi.edu>
2016-03-28 20:51:26 -0400

    initialize output to avoid problems when no blobs

--- feature_extraction/blob_binary_symmetry.m
+++ feature_extraction/blob_binary_symmetry.m
@@ -1,5 +1,9 @@
 function [ target ] = blob_binary_symmetry( target )
 
+target.blob_props.B180 = NaN(1,max(1,target.blob_props.numBlobs));
+target.blob_props.B900 = target.blob_props.B180;
+target.blob_props.Bflip = target.blob_props.B180;
+
 if target.blob_props.numBlobs > 0,
     for i = 1:target.blob_props.numBlobs,
         [b180, b90, bflip] = binary_symmetry(target.rotated_blob_images{i});
@@ -7,8 +11,6 @@ if target.blob_props.numBlobs > 0,
         target.blob_props.B90(i) = b90;
         target.blob_props.Bflip(i) = bflip;
     end;
-else %empty blob image
-    % not sure what to do here - jf
 end
 
 end
