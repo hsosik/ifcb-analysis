24174bd5b14fea63f18176d54a814257d5f12776
Joe Futrelle <joefutrelle@gmail.com>
2016-04-13 14:08:26 -0400

    added documentation

--- feature_extraction/compute_perimeter_img.m
+++ feature_extraction/compute_perimeter_img.m
@@ -1,6 +1,10 @@
 function [ perimeter_img ] = compute_perimeter_img( img )
 %return 4-connected perimeter image for a B&W blob image
-%   Detailed explanation goes here
+% done by convolving the image with this matrix
+% |  0 -1  0 |
+% | -1  4 -1 |
+% |  0 -1  0 |
+% and only keeping points for which the result is > 0
 
 perimeter_img = im2bw(conv2(single(img),[0 -1 0; -1 4 -1; 0 -1 0], 'same'));
 
