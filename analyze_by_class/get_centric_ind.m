2e5ff9182c6a9a0d363b113851093d8e73a54b5f
Emily Peacock <epeacock@whoi.edu>
2016-07-13 11:27:42 -0400

    new get_centric_ind.m and get_pennate_ind.m for comparing abundance of these diatom categories to genetic data.

---
+++ analyze_by_class/get_centric_ind.m
@@ -0,0 +1,19 @@
+
+function [ ind_out, class_label ] = get_centric_ind( class2use, class_label)
+%function [ ind_out, class_label ] = get_ciliate_ind( class2use, class_label )
+% MVCO class list specific to return of indices that correspond to all variants of ciliates
+%  Heidi M. Sosik, Woods Hole Oceanographic Institution, March 2013
+
+class2get = {'Chaetoceros' 'Corethron' 'DactFragCerataul' 'Dactyliosolen' 'Ditylum' 'Eucampia' 'Guinardia_delicatula'...
+    'Guinardia_flaccida' 'Leptocylindrus' 'Rhizosolenia' 'Skeletonema' 'Thalassiosira' 'Thalassiosira_dirty'...
+   'Lauderia' 'Stephanopyxis' 'Cerataulina' 'Coscinodiscus' 'Odontella' 'Guinardia_striata' 'Paralia' 'Chaetoceros_didymus'...
+  'Hemiaulus' 'Leptocylindrus_mediterraneus' 'Ditylum_parasite' 'Bidulphia'...
+  'G_delicatula_parasite' 'G_delicatula_external_parasite' 'Chaetoceros_other'...
+  %categories below here are questionable, and may not want to be included.
+ 'mix_elongated' 'Chaetoceros_didymus_flagellate'...
+   'Chaetoceros_flagellate' 'Chaetoceros_pennate' 'Cerataulina_flagellate' 'diatom_flagellate'};
+
+[~,ind_out] = intersect(class2use, class2get);
+ind_out = sort(ind_out);
+
+end
