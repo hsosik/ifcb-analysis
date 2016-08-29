35b3134579be98b2ba6b39ba3e3ace8e01fb95c4
Joe Futrelle <joefutrelle@gmail.com>
2016-08-29 16:54:46 +0000

    fixed #13

--- python/ifcb/features/segmentation.py
+++ python/ifcb/features/segmentation.py
@@ -3,7 +3,11 @@ import numpy as np
 from skimage import img_as_float
 from scipy.ndimage.morphology import binary_fill_holes
 from skimage.morphology import binary_closing, binary_dilation, remove_small_objects
-from skimage.filter import threshold_otsu
+
+try:
+    import skimage.filters as imfilters
+except ImportError:
+    import skimage.filter as imfilters
 
 from ifcb.features.phasecong import phasecong_Mm
 from ifcb.features.morphology import SE2, SE3, hysthresh, bwmorph_thin
@@ -14,7 +18,7 @@ BLOB_MIN = 150
 DARK_THRESHOLD_ADJUSTMENT=0.65
 
 def dark_threshold(roi,adj=DARK_THRESHOLD_ADJUSTMENT):
-    thresh = int(round(threshold_otsu(roi) * adj))
+    thresh = int(round(imfilters.threshold_otsu(roi) * adj))
     return roi < thresh
 
 def segment_roi(roi):
@@ -30,7 +34,7 @@ def segment_roi(roi):
     # step 4. threshold to find dark areas
     dark = dark_threshold(roi, DARK_THRESHOLD_ADJUSTMENT)
     # step 5. add dark areas back to blob
-    B = B | dark
+    B = np.logical_or(B,dark)
     # step 6. binary closing
     B = binary_closing(B,SE3)
     # step 7. binary dilation
