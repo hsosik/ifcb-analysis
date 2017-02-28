e0342d6113b63be5faf5d3e29780d466cfb81164
hsosik <hsosik@whoi.edu>
2017-02-28 18:49:19 -0500

    update to match mean case with smooth transition between Dec and Jan

--- analyze_by_class/smoothed_climatology_median.m
+++ analyze_by_class/smoothed_climatology_median.m
@@ -3,14 +3,19 @@ function [ xmedian, xstd ] = smoothed_climatology( x , win)
 %assume input vector or matrix with dimensions yearday x number of years
 %outputs running mean and std with span +/- value specified in win, pooling observations from all years
  
-for ii = 1:366, 
+xin = x; 
+xinlength = size(xin,1);
+x = [x; x; x]; %make sure consistency across Dec-Jan transition
+xlength = size(x,1);
+for ii = 1:xlength, 
     iii = ii-win:ii+win; 
-    iii = intersect(iii,1:366); 
-    t = x(iii,:); 
+    iii = intersect(iii,1:xlength); 
+    t = x(iii,:);
     t = t(~isnan(t(:)));
-    xmedian(ii) = median(t); 
-    xstd(ii) = nanstd(t); 
+    xmedian(ii) = median(t(:)); 
+    xstd(ii) = nanstd(t(:)); 
 end;
+xmedian = xmedian(xinlength+1:xinlength*2);
+xstd = xstd(xinlength+1:xinlength*2);
 
 end
-
