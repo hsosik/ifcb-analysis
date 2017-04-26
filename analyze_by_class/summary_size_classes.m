197e0dd95d3adbdd3654a165e2f7d2210af6380c
hsosik <hsosik@whoi.edu>
2017-04-26 11:53:20 -0400

    fix unit documentation error

--- analyze_by_class/summary_size_classes.m
+++ analyze_by_class/summary_size_classes.m
@@ -114,7 +114,7 @@ clear daycount classcount iii cellC
 %end;
 
 ml_day_mat = squeeze(cell2mat(struct2cell(ml_day)))'; 
-C_day_mat = squeeze(cell2mat(struct2cell(C_day)))'./ml_day_mat/1000; %microgC per mL
+C_day_mat = squeeze(cell2mat(struct2cell(C_day)))'./ml_day_mat/1000; %microgC per L
 C_diatom = sum(C_day_mat(:,ind_diatom),2); %sum to skip the ones that are incomplete for all diatoms
 
 C0_10_mat = squeeze(cell2mat(struct2cell(C0_10)))'./ml_day_mat/1000; C0_10sum = sum(C0_10_mat,2);
@@ -133,7 +133,7 @@ C20_infphyto = sum(C20_inf_mat(:,ind_phyto),2);
 [ Cmdate_mat, C20_diatom_mat, Cyearlist, yd ] = timeseries2ydmat( unqday, C_diatom );
 
 Notes1 = 'Output from summary_size_classes.m';
-Notes2 = 'Carbon values in micrograms per mL';
+Notes2 = 'Carbon values in micrograms per L';
 Cmdate_day = unqday;
 %save c:\work\mvco\carbon\IFCB_carbon_manual_Jan2014 Cmdate_mat C0_10phyto_mat C10_20phyto_mat C20_infphyto_mat C_diatom C_day_mat classes Cyearlist yd ind_diatom ind_phyto Notes1 Notes2 Cmdate_day
 datestr = date; datestr = regexprep(datestr,'-','');
