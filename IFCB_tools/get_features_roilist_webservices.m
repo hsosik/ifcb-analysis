102847ab34d6d193bc10fbd2e0fd5372528322ba
hsosik <hsosik@whoi.edu>
2017-05-01 19:58:07 -0400

    add verbose mode to command line

--- IFCB_tools/get_features_roilist_webservices.m
+++ IFCB_tools/get_features_roilist_webservices.m
@@ -4,8 +4,11 @@ function [ feature_mat , fea_titles ] = get_features_roilist_webservices( roilis
 %input a cell array of ROI ids with full URL specified
 %Heidi M. Sosik, Woods Hole Oceanographic Institution, April 2017
 %
+disp('Accessing features...please be patient')
 feature_mat = NaN(length(roilist_fullpath), length(fea2use));
-for count = 1:length(roilist_fullpath)
+totaln = length(roilist_fullpath);
+for count = 1:totaln
+    if ~rem(count,100), disp([num2str(count) ' of ' num2str(totaln)]), end
     [ fea, titles ] = roi_features(roilist_fullpath{count}); %assume all have same fea_titles
     [~,ia] = intersect(titles, fea2use);
     feature_mat(count,:) = fea(ia);
