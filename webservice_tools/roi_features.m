c0313a6717b2f84bbe74ab84fb6c84850a85f2fc
Joe Futrelle <joefutrelle@gmail.com>
2017-05-01 13:57:01 -0400

    uses new web service, is faster

--- webservice_tools/roi_features.m
+++ webservice_tools/roi_features.m
@@ -1,28 +1,12 @@
 function [ feavec, feanames ] = roi_features ( roi_url )
 
-[dir, file, ~] = fileparts(roi_url);
-roi_url = [dir '/' file];
+[namespace, roi_lid, ~] = fileparts(roi_url);
+roi_url = [namespace '/' roi_lid];
+features_url = [roi_url '_features.json'];
 
-roi_metadata = webread([roi_url '.json']);
-bin_url = roi_metadata.binID;
-roi_number = roi_metadata.targetNumber;
-features_url = [bin_url '_features.csv'];
+result = webread(features_url);
 
-tmp = tempname;
-fout = fopen(tmp,'w');
-path = websave(tmp, features_url);
-fclose(fout);
-data = importdata(path);
-feavecs = data.data;
-feanames = data.colheaders;
-[n_rois, ~] = size(feavecs);
-
-for i=1:n_rois
-    rn = feavecs(i,1);
-    if rn == roi_number
-        feavec = feavecs(i,:);
-        return;
-    end
-end
+feanames = result.names;
+feavec = result.values;
 
 end
