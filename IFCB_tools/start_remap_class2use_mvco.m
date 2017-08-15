2152d0338a929ec21f2802350d3e9f576bb9f9ed
hsosik <hsosik@whoi.edu>
2017-08-15 15:56:57 -0400

    revise to run on old SEA cruise

--- IFCB_tools/start_remap_class2use_mvco.m
+++ IFCB_tools/start_remap_class2use_mvco.m
@@ -3,8 +3,8 @@
 %   Heidi M. Sosik, Woods Hole Oceanographic Institution, March 2013
 
 config = load('class2use_MVCOmanual6', 'class2use'); %USER load YOUR class2use list from a file; format class2use = {'classstr1', 'classstr2', ...};
-config.remappath = '\\SOSIKNAS1\IFCB_products\IFCB14_Dock\Manual_fromClass_test\'; %USER indicate path that contains all manual annotation files to be remapped
-config.remapfunc = 'MVCO_fix1_class_map'; %USER specify YOUR function that defines how classes are mapped from old to new
+config.remappath = '\\SOSIKNAS1\IFCB_products\IFCB2_C211A_SEA2007\Manual_fromClass\'; %USER indicate path that contains all manual annotation files to be remapped
+config.remapfunc = 'MVCO_fix1_class_map_modified_for_old_cruises'; %USER specify YOUR function that defines how classes are mapped from old to new
 remap_class2use_manual_remove_ciliate_subdivide(config) %first to remove ciliate subdiv and remap manual
 remap_class2use_manual_overwrite_class2use(config)
 
