1f7b036456622dfefb52b9ea1e67964e2a7b8675
Heidi Sosik <hsosik@whoi.edu>
2017-10-05 10:36:08 -0400

    some basic queries to the manual-classify postgres database for one class and one time series

---
+++ analyze_by_class/query_classcount_timeseries.sql
@@ -0,0 +1,20 @@
+
+WITH
+    CI AS (
+        SELECT id FROM classify_classlabel WHERE name = '%s'
+    ),
+ 
+    CA AS (
+        SELECT DISTINCT ON (c.bin, c.roi) c.*, p.power
+            FROM classify_classification c, auth_user_groups g, auth_group p
+            WHERE c.user_id = g.user_id
+            AND p.id = g.group_id
+        	AND cast(timeseries_id as uuid)=(select id from classify_timeseries where url like '%%%s/')
+            ORDER BY c.bin, c.roi, p.power DESC, c.verification_time DESC NULLS LAST, c.time DESC
+    ),
+    CF AS (
+        SELECT * FROM CA,CI WHERE CA.classification_id = CI.id
+    )
+
+SELECT bin, COUNT(*) FROM CF
+    GROUP BY bin ORDER BY bin;
