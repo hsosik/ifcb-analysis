361e624eb0cd7bbfdb6b72fd702eaf34659154d4
hsosik <hsosik@whoi.edu>
2017-10-29 15:30:10 -0400

    just fiddling with graph size

--- analyze_by_class/plot_ditylum_guinardia.m
+++ analyze_by_class/plot_ditylum_guinardia.m
@@ -25,7 +25,7 @@ load \\sosiknas1\IFCB_products\MVCO\Manual_fromClass\summary\count_manual_curren
 figure, set(gcf, 'paperposition', [.25 2.5 11.5 2.45], 'units', 'inches')
 %set(gcf, 'position', [5 4 11.5 2.45])
 set(gcf, 'position', [5 4 11.5 2])
-set(gcf, 'position', [5 4 11.5 1.5])
+%set(gcf, 'position', [5 4 11.5 1.5])
 
 ind = strmatch('Guinardia', class2useTB, 'exact');
 %[xmat, ymat ] = timeseries2ydmat(mdateTB, classcountTB_above_adhocthresh(:,ind)./ml_analyzedTB);
@@ -44,7 +44,7 @@ set(gca, 'xlim', datenum([2006,6,1; 2018,1,1]), 'fontsize', 14, 'xgrid', 'on', '
 img = imread('http://ifcb-data.whoi.edu/mvco/IFCB5_2012_030_223923_03472.png'); %Guinardia
 t = size(img,1)/1000*1.0; s = size(img,2)/1000*1.0;
 %i3 = axes('position', [-.09 .68 s t]);
-i3 = axes('position', [.06 .7 s t]);
+i3 = axes('position', [.06 .65 s t]);
 imshow(img)
 title('\itGuinardia delicatula', 'fontsize', 12)
 
