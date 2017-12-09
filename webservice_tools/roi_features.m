20992340f85e03f21afe418a7d8c3ef951b33426
hsosik <hsosik@whoi.edu>
2017-12-09 13:51:28 -0500

    add weboptions configuration to increase timeout value for webread

--- webservice_tools/roi_features.m
+++ webservice_tools/roi_features.m
@@ -1,10 +1,12 @@
 function [ feavec, feanames ] = roi_features ( roi_url )
 
+options = weboptions('Timeout',30);
+
 [namespace, roi_lid, ~] = fileparts(roi_url);
 roi_url = [namespace '/' roi_lid];
 features_url = [roi_url '_features.json'];
 
-result = webread(features_url);
+result = webread(features_url, options);
 
 feanames = result.names;
 feavec = result.values;
