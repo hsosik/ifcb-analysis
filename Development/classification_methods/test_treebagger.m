f436fb6b52c4f1a65250d48de00680e095ccb7c4
hsosik <hsosik@whoi.edu>
2018-01-07 11:49:31 -0500

    messing around with some classifier tests for more classes

--- Development/classification_methods/test_treebagger.m
+++ Development/classification_methods/test_treebagger.m
@@ -1,13 +1,44 @@
-
 result_path = '\\sosiknas1\IFCB_products\MVCO\Manual_fromClass\train_fromcsv_Sep2017\splits\';
+result_path = '\\sosiknas1\IFCB_products\MVCO\Manual_fromClass\train_fromcsv_Sep2017_new\splits\';
+%result_path = 'C:\work\IFCB\Manual_fromClass\train_fromcsv_Sep2017\splits\';
+%result_path2 = 'C:\work\IFCB\\Manual_fromClass\train_fromcsv_Sep2017_new\splits\';
+
 train_filename = 'train_features_v2';
-result_str = 'trees_features_v2_'
-nTrees = 50;
+result_str = 'trees_features_v2_';
+
+load([result_path train_filename])
+temp = load([result_path2 train_filename]);
+class2use = [class2use temp.class2use];
+class_vector = [class_vector; temp.class_vector];
+features = [features; temp.features];
+targets = [targets; temp.targets];
+clear temp
+
+%FUDGE for now to fix mistake with more than 4000 detritus examples
+ii = strmatch('detritus', class_vector);
+ii = ii(4001:end);
+class_vector(ii) = [];
+features(ii,:) = [];
+targets(ii) = [];
+
+class2skip = {'roundCell' 'other' 'crypto' 'other_interaction'};
+[,ii] = ismember(class_vector, class2skip);
+class_vector(ii) = [];
+features(ii,:) = [];
+targets(ii) = [];
+
+[~,ia] = ismember(class_vector, class2use);
+n_class = histc(ia', 1:length(class2use)); clear ia
+
+ic = find(n_class > 1000);
+[~,ia] = ismember(class_vector, class2use(ic));
+ia = find(ia); %non-zero values
+
+nTrees = 100;
 paroptions = statset('UseParallel','always');
 
-load([result_path train_filename]);
 %make_TreeBaggerClassifier_user_training( result_path, train_filename, result_str, nTrees)
-b = TreeBagger(nTrees,features,class_vector,'Method','c','OOBVarImp','on','MinLeaf',1,'Options',paroptions);
+b = TreeBagger(nTrees,features(ia,:),class_vector(ia),'Method','c','OOBVarImp','on','MinLeaf',1,'Options',paroptions);
 
 figure, hold on
 plot(oobError(b), 'b-');
@@ -16,12 +47,13 @@ ylabel('Out-of-Bag Classification Error');
 datestring = datestr(now, 'ddmmmyyyy');
 
 classes = class2use;
+
 save([result_path result_str datestring],'b', 'targets', 'featitles', 'classes', '-v7.3')
 
 classifier_oob_analysis( [result_path result_str datestring] )
 
 validation = load([result_path 'validation_features_v2']);
-trees = load([result_path result_str '29Dec2017']);
+trees = load([result_path result_str datestring]);
 classes = trees.classes;
 [ TBclass, TBscores] = predict(trees.b, validation.features);
 [c1, gord1] = confusionmat(validation.class_vector,TBclass); %transposed from mine
