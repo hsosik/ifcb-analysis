3e8bc99640b53cd6cd6cf50a08542c483661fba9
hsosik <hsosik@whoi.edu>
2020-04-26 13:23:53 -0400

    update to work with new IFCB meta data compilation scripts

--- IFCB_tools/IFCB_match_uw.m
+++ IFCB_tools/IFCB_match_uw.m
@@ -1,4 +1,4 @@
-function [IFCB_match] =  IFCB_match_uw(IFCB_files, uw)
+function [IFCB_match] =  IFCB_match_uw(IFCB_files, IFCB_mdate, uw)
 % function IFCB_match = IFCB_match_uw(IFCB_files, uw);
 % input a list of IFCB filenames and a table of underway ship's info
 % (mdate, lat, lon)
@@ -6,27 +6,53 @@ function [IFCB_match] =  IFCB_match_uw(IFCB_files, uw)
 % if larger than 5 minute gap in the underway data)
 % Heidi M. Sosik, Woods Hole Oceanographic Instition, Decemeber 2020
 %
-IFCB_match.pid = IFCB_files;
-IFCB_mdate = IFCB_file2date(cellstr(IFCB_files));
-for count = 21:40%length(IFCB_mdate)
-    [m,ia] = min(abs(IFCB_mdate(count)-uw.mdate));
-    if m < 5/60/24 %5 minutes as days
-        IFCB_match.Lat(count) = uw.lat(ia);
-        IFCB_match.Lon(count) = uw.lon(ia);
+warning off
+
+%IFCB_mdate = IFCB_file2date(cellstr(IFCB_files));
+iso8601format = 'yyyy-mm-dd hh:MM:ss';
+if strmatch('matdate', uw.Properties.VariableNames)
+    uw_mdate = uw.matdate;
+else %NES-LTER api case
+    uw_mdate = datenum(uw.date, iso8601format);
+end
+t = uw.Properties.VariableNames;
+ilat = find(contains(lower(t), 'lat'));
+ilon = find(contains(lower(t), 'lon'));
+uw_lat = uw.(t{ilat(1)});
+uw_lon = uw.(t{ilon(1)});
+IFCB_match = uw(1,:);
+IFCB_match.lat(1) = NaN; 
+IFCB_match.lon(1) = NaN;
+
+for count = 1:length(IFCB_mdate)
+    [m,ia] = min(abs(IFCB_mdate(count)-uw_mdate));
+    if m <= 5/60/24 %5 minutes as days
+        IFCB_match(count,1:end-2) = uw(ia,:);
+        IFCB_match.lat(count) = uw_lat(ia);
+        IFCB_match.lon(count) = uw_lon(ia);        
     else
-        if IFCB_mdate(count) > uw.mdate(ia) %closest to end of gap
-            it = ia;
-        else %closest to start of gap
-            it = ia-1;
+        if ia >= length(uw_lat)
+            IFCB_match.lat(count) = NaN;
+            IFCB_match.lon(count) = NaN;
+        else
+            if IFCB_mdate(count) > uw_mdate(ia) %closest to end of gap
+                it = ia;
+            else %closest to start of gap
+                it = ia-1;
+            end
+            step = floor((uw_mdate(it+1)- uw_mdate(it))*24*60); %one minute interpolation
+            [lat,lon] = track2(uw_lat(it), uw_lon(it),uw_lat(it+1), uw_lon(it+1),[],[], step);
+            it2 = round((IFCB_mdate(count)-uw_mdate(it))/(uw_mdate(it+1)-uw_mdate(it))*step); %index of closest interpolated minute
+            IFCB_match.lat(count) = lat(it2);
+            IFCB_match.lon(count) = lon(it2);
+            IFCB_match(count,1:end-2) = array2table(interp1(uw_mdate, uw{:,:}, IFCB_mdate(count)),'VariableNames', uw.Properties.VariableNames);            
+            disp('CHECK interpolation')
+            %keyboard
+            disp('Consider keyboard here')
         end
-        step = floor((uw.mdate(it+1)- uw.mdate(it))*24*60); %one minute interpolation
-   
-        [lat,lon] = track2(uw.lat(it), uw.lon(it),uw.lat(it+1), uw.lon(it+1),[],[], step);
-        it2 = round((IFCB_mdate(count)-uw.mdate(it))/(uw.mdate(it+1)-uw.mdate(it))*step); %index of closest interpolated minute
-        IFCB_match.Lat(count) = lat(it2);
-        IFCB_match.Lon(count) = lon(it2);
     end
 end
+IFCB_match = addvars(IFCB_match, IFCB_files, 'before', 1, 'NewVariableNames',{'pid'});
+IFCB_match = addvars(IFCB_match, IFCB_mdate, 'NewVariableNames',{'mdate'});
 
 end
-
