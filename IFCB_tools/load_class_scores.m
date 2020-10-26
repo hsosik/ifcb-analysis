022db30c2ff82e10e838ca4e7e9b0d3346e781db
Heidi M. Sosik <hsosik@whoi.edu>
2020-10-26 16:09:16 -0400

    update for v3 format of HDF5 output from CNN runs

--- IFCB_tools/load_class_scores.m
+++ IFCB_tools/load_class_scores.m
@@ -1,9 +1,15 @@
 function [bin_id, scores, roi_numbers, class_labels] = load_class_scores(path)
 
-scores = h5read(path,'/scores')';
+%scores = h5read(path,'/scores')';
 
-bin_id = h5readatt(path,'/scores','bin_id');
+%bin_id = h5readatt(path,'/scores','bin_id');
+%roi_numbers = h5read(path,'/roi_numbers');
+%class_labels = h5readatt(path,'/scores','class_labels');
+
+%Heidi, new format Oct 2020
+scores = h5read(path,'/output_scores')';
 roi_numbers = h5read(path,'/roi_numbers');
-class_labels = h5readatt(path,'/scores','class_labels');
+bin_id = h5readatt(path,'/metadata','bin_id');
+class_labels = cellstr(h5read(path,'/class_labels'));
 
 end
