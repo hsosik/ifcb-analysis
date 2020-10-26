6d033d067e96416935d03756c2fc6d8cc0e04233
hsosik <hsosik@whoi.edu>
2020-10-26 18:12:31 -0400

    revise to output structure

--- IFCB_tools/load_class_scores.m
+++ IFCB_tools/load_class_scores.m
@@ -1,4 +1,5 @@
-function [bin_id, scores, roi_numbers, class_labels] = load_class_scores(path)
+function classTable = load_class_scores(path)
+%function [bin_id, scores, roi_numbers, class_labels] = load_class_scores(path)
 
 %scores = h5read(path,'/scores')';
 
@@ -7,9 +8,12 @@ function [bin_id, scores, roi_numbers, class_labels] = load_class_scores(path)
 %class_labels = h5readatt(path,'/scores','class_labels');
 
 %Heidi, new format Oct 2020
-scores = h5read(path,'/output_scores')';
-roi_numbers = h5read(path,'/roi_numbers');
-bin_id = h5readatt(path,'/metadata','bin_id');
-class_labels = cellstr(h5read(path,'/class_labels'));
-
+classTable.scores = h5read(path,'/output_scores')';
+classTable.roi_numbers = h5read(path,'/roi_numbers');
+classTable.metadata.bin_id = h5readatt(path,'/metadata','bin_id');
+classTable.metadata.version = h5readatt(path,'/metadata','version');
+classTable.metadata.model_id = h5readatt(path,'/metadata','model_id');
+classTable.metadata.run_timestamp = h5readatt(path,'/metadata','timestamp');
+classTable.class_labels = cellstr(h5read(path,'/class_labels'));
+classTable.top_class = h5read(path,'/output_classes')';
 end
