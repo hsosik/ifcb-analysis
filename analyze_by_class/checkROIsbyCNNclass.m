403bfbb4949525feda16ca0ca420e9741f3b71a6
hsosik <hsosik@whoi.edu>
2021-12-08 21:04:19 -0500

    update to handle new pages of ROIs and saving ROIs in workspace for repeat plotting

--- analyze_by_class/checkROIsbyCNNclass.m
+++ analyze_by_class/checkROIsbyCNNclass.m
@@ -3,17 +3,37 @@ load \\sosiknas1\IFCB_products\EXPORTS\summary\summary_biovol_allHDF_min20_2018l
 load('\\sosiknas1\IFCB_products\EXPORTS\summary\summary_biovol_allHDF_min20_2018', 'meta_data')
 ii = find(strcmp(meta_data.cruise, 'SR1812')& strcmp(meta_data.sample_type, 'underway'));
 %%
-cc = find(strcmp(class2use, 'Guinardia_flaccida'))
+classStr = 'Guinardia_flaccida';
+cc = find(strcmp(class2use, classStr))
 roiset = cat(1,classPidList{ii,cc});
 whos roiset
 %%
-figure
-t = tiledlayout(10,5)
-title(t, 'CNN G. flaccida: ')
+%get the images 
+disp('Reading images...')
+clear img
 for ind = 1:length(roiset)
-    nexttile
-    imshow(imread(['https://ifcb-data.whoi.edu/EXPORTS/' roiset{ind} '.png']))
-    %title(['CNN G. flaccida: ' set2{ind}])
-    %title(set2(ind), 'interpreter', 'none')
-    %pause
+   if ~rem(ind,20)
+       disp([num2str(ind) ' of ' num2str(length(roiset))]);
+   end
+   img{ind} = imread(['https://ifcb-data.whoi.edu/EXPORTS/' roiset{ind} '.png']);
+end
+disp('Ready to plot')
+%%
+figure
+t = tiledlayout(4,3);
+title(t, ['CNN ' classStr], 'interpreter', 'none')
+tilecount = 0;
+for ind = 1:length(roiset)
+    if tilecount<prod(t.GridSize)
+        nexttile
+        tilecount = tilecount+1;
+        imshow(img{ind})
+        title(roiset(ind), 'interpreter', 'none')
+    else
+        pause
+        tilecount = 0;
+        clf
+        t = tiledlayout(4,3);
+        title(t, ['CNN ' classStr], 'interpreter', 'none')
+    end
 end
