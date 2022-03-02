02861bec05597e457f6aeefbd56f255e874b7a47
Joe Futrelle <joefutrelle@gmail.com>
2022-03-02 13:16:57 -0500

    replace deprecated operation

--- python/ifcb_features/morphology.py
+++ python/ifcb_features/morphology.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from skimage.morphology import binary_dilation, binary_erosion, disk, reconstruction
-from scipy.ndimage import measurements, correlate
+from skimage.morphology import binary_dilation, disk, reconstruction
+from scipy.ndimage import correlate
 
 EIGHT = np.ones((3,3)).astype(np.bool)
 FOUR = disk(1).astype(np.bool)
@@ -30,7 +30,7 @@ def hysthresh(img,T1,T2):
     edges = img > T1
     sum = 1
     while sum > 0:
-        bd = (binary_dilation(edges,EIGHT) & (img > T2)) - edges
+        bd = (binary_dilation(edges,EIGHT) & (img > T2)) ^ edges
         edges = np.logical_or(bd, edges)
         sum = np.sum(bd)
     return edges
