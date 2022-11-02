f4ba75db186c28799893b07f42bf7ccf75ec4406
hsosik <hsosik@whoi.edu>
2022-11-01 20:03:48 -0400

    update to address new cases with bad data in hdr and/or adc files (see github issue https://github.com/hsosik/ifcb-analysis/issues/33)

--- IFCB_tools/IFCB_volume_analyzed.m
+++ IFCB_tools/IFCB_volume_analyzed.m
@@ -1,26 +1,49 @@
-function [ ml_analyzed ] = IFCB_volume_analyzed( hdrfilename )
-%function [ ml_analyzed ] = IFCB_volume_analyzed( hdrfilename )
+function [ ml_analyzed ] = IFCB_volume_analyzed( hdrfilename , hdrOnly_flag)
+%function [ ml_analyzed ] = IFCB_volume_analyzed( hdrfilename, hdrOnly_flag )
 % hdrfilename input as char or cell complete with path (or url)
+% hdrOnly_flag, optional 0 (default) or 1 (case to skip adc file estimate)
+%
 % reads sample run time and "inhibit" time from IFCB hdr file 
 % returns associated estimate of sample volume analyzed (in milliliters)
 % assuming standard IFCB configuration with sample syringe operating 
 % at 0.25 mL per minute
 % Applies only to IFCB instruments after 007 and higher (except 008)
 % Heidi M. Sosik, Woods Hole Oceanographic Institution, September 2012
-
+%
+% update for default to always compute from hdr file info AND estimate from
+% adc file info (with call to IFCB_volume_analyzed_fromADC.m); then use
+% adhoc criteria to select cases when hdr info is not good and adc should
+% be used as next best estimate (derived from analysis of thousands of
+% files over NES-LTER transect cruises and OTZ cruises and numerous IFCB 
+% instrument numbers and revisions of acquisition software from McLane)
+% Set optional hdrOnly_flag = 1 for quicker performance but *ONLY* if you are sure 
+% that your hdr file info is all robust (not recommended for most users
+% as of 2022
+% Heidi M. Sosik, Woods Hole Oceanographic Institution, November 2022
+if ~exist('hdrOnly_flag', 'var')
+    hdrOnly_flag = 0; %default
+end
 flowrate = 0.25; %milliliters per minute for syringe pump
 if ischar(hdrfilename), hdrfilename = cellstr(hdrfilename); end
 ml_analyzed = NaN(size(hdrfilename));
 for count = 1:length(hdrfilename)
     hdr = IFCBxxx_readhdr(hdrfilename{count});
-    if ~isempty(hdr)
-        looktime = hdr.runtime - hdr.inhibittime; %seconds
-        if looktime > 0
-            ml_analyzed(count) = flowrate.*looktime/60;
-        else
-            ml_analyzed(count) = IFCB_volume_analyzed_fromADC (regexprep(hdrfilename{count}, '.hdr', '.adc'));           
+    %if ~isempty(hdr)
+    %    [~, inhibittime_adc, runtime_adc] = IFCB_volume_analyzed_fromADC (regexprep(hdrfilename{count}, '.hdr', '.adc'));
+    %end
+    runtime = hdr.runtime;
+    inhibittime = hdr.inhibittime;
+    if ~hdrOnly_flag
+        [~, inhibittime_adc, runtime_adc] = IFCB_volume_analyzed_fromADC (regexprep(hdrfilename{count}, '.hdr', '.adc'));
+        if (hdr.runtime./runtime_adc < 0.98 || hdr.runtime./runtime_adc > 1.02)
+            runtime = runtime_adc;
+        end
+        if (hdr.inhibittime./inhibittime_adc < 0.98 || hdr.inhibittime./inhibittime_adc > 1.02)
+            inhibittime = inhibittime_adc;
         end
     end
+    looktime = runtime-inhibittime; %seconds
+    ml_analyzed(count) = flowrate.*looktime/60;
 end
 
 end
--- IFCB_tools/IFCB_volume_analyzed_fromADC.m
+++ IFCB_tools/IFCB_volume_analyzed_fromADC.m
@@ -1,5 +1,5 @@
-function [ ml_analyzed ] = IFCB_volume_analyzed_fromADC( adcfilename )
-%function [ ml_analyzed ] = IFCB_volume_analyzed( hdrfilename )
+function [ ml_analyzed, inhibittime, runtime ] = IFCB_volume_analyzed_fromADC( adcfilename )
+%function [ ml_analyzed ] = IFCB_volume_analyzed_fromADC( hdrfilename )
 % hdrfilename input as char or cell complete with path (or url)
 % reads sample run time and "inhibit" time from IFCB hdr file 
 % returns associated estimate of sample volume analyzed (in milliliters)
@@ -7,41 +7,59 @@ function [ ml_analyzed ] = IFCB_volume_analyzed_fromADC( adcfilename )
 % at 0.25 mL per minute
 % Applies only to IFCB instruments after 007 and higher (except 008)
 % Heidi M. Sosik, Woods Hole Oceanographic Institution, September 2012
+% updated to address newly discovered cases in failure to log good values
+% in adc files (see https://github.com/hsosik/ifcb-analysis/issues/33)
+% Heidi M. Sosik, Woods Hole Oceanographic Institution, November 2022
 
 flowrate = 0.25; %milliliters per minute for syringe pump
-if ischar(adcfilename), adcfilename = cellstr(adcfilename); end;
+if ischar(adcfilename), adcfilename = cellstr(adcfilename); end
 ml_analyzed = NaN(size(adcfilename));
+inhibittime = ml_analyzed;
+runtime = ml_analyzed;
 for count = 1:length(adcfilename)
-    %hdr = IFCBxxx_readhdr(hdrfilename{count});
     if startsWith(adcfilename{count}, 'http')
         adc = webread(adcfilename{count}, 'FileType', 'csv');
     else
         adc = readtable(adcfilename{count}, 'FileType', 'text');       
     end
     if ~isempty(adc)
-        % try using the info in adc file columns 23 and 24 for run and inhibit time
-        runtime = adc.Var23(end);
-        inhibittime = adc.Var24(end);
-        looktime = runtime - inhibittime; %seconds
-        ml_analyzed(count) = flowrate.*looktime/60;
-        %if that doesn't work (e.g., 0 or negative ml_analyzed), trying skipping the last line
-        if ml_analyzed(count) <= 0 || abs(adc.Var23(end)-adc.Var2(end)) >= 0.3 %minor cases for some files with bad last adc line
-            runtime = adc.Var23(end-1);
-            inhibittime = adc.Var24(end-1);
-            looktime = runtime - inhibittime; %seconds
-            ml_analyzed(count) = flowrate.*looktime/60;
-        end
-        %if that still doesn't work use the inhibit time stats to estimate best quess value
-        if ml_analyzed(count) <= 0 %minor case for some files with 0 runtime and inhibit time in numerous rows at file end
-           runtime = adc.Var2(end-1); %next best info after runtime
-           ii = find(adc.Var23);
-           modeinhibittime = mode(diff(adc.Var24(ii)));
-           inhibittime = adc.Var24(ii(end)) + (size(adc,1)-length(ii)) * modeinhibittime;
-           looktime = runtime - inhibittime;
-           ml_analyzed(count) = flowrate.*looktime/60;
+        %find indices of rows where inhibitime is not 0 and not less than the previous value (within 0.1 second)
+        diffinh = diff(adc.Var24);
+        iii = [1; find(adc.Var24(2:end)>0 & diffinh>-.1 & diffinh<5) + 1];
+        %calculate the mode differential inhibittime from the good records, round to nearest 4 digits before finding mode
+        modeinhibittime = mode(round(diff(adc.Var24(iii)),4)); %this will be used for the "second best" estimate
+        %now see if there should be any offsets applied for adc files with strange non-zero starts to the time counting
+        runtime_offset = 0;
+        inhibittime_offset = 0;
+        if size(adc,1)>1 % only do this if there is more than one line in the adc file
+        %now check if the inhibit and runtime columns have a large offset
+            runtime_offset_test = adc.Var23(2)-adc.Var2(2);
+            if runtime_offset_test > 10 %when the difference is small there is no problem keep at 0, if big do this
+                runtime_offset = runtime_offset_test;
+                %large runtime and inhibit time offsets seem to come and go together
+                inhibittime_offset = adc.Var24(2) + modeinhibittime*2; %use the second row since the first one is bad occasionally, add two mode increments to account for that
+            end
+               
+            % if ~isempty(setdiff(1:size(adc,1), iii)) %check if there are bad rows
+            if size(adc,1)==length(iii)
+                %this is the best value--if it's not bad
+                inhibittime(count) = adc.Var24(end)-inhibittime_offset;
+            else
+                %second best estimate, last good row, plus mode as best guess for each bad row
+                inhibittime(count) = adc.Var24(iii(end)) + (size(adc,1)-length(iii)) * modeinhibittime-inhibittime_offset;
+            end
+            %this is the best value--if it's not bad
+            runtime(count) = adc.Var23(end)-runtime_offset;
+            %grabstart values in col2 are not corrupted when the runtime values in later columns sometimes are towards the file end
+            %but col2 starts off a bit after col23, so adjust with the median difference estimated from the top 50 rows
+            n = min(size(adc,1),50); % use 50 rows or all that exist if fewer than 50
+            runtime2 = adc.Var2(end)+median(adc.Var23(1:n)-adc.Var2(1:n))-runtime_offset;
+            if abs(runtime-runtime2)>0.2 %if the estimates are not close, use the second best
+                runtime(count) = runtime2;
+            end
         end
     end
 end
-
+looktime = runtime-inhibittime;
+ml_analyzed(count) = flowrate.*looktime/60;
 end
-
